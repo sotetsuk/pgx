bd6a5a3959890ec94bc96b57e1fd43991a3f52c5
youyou-ku <youyou5016ku@gmail.com>
2022-08-23 20:40:23 +0900

    add legal_move

--- pgx/animal_shogi.py
+++ pgx/animal_shogi.py
@@ -36,3 +36,104 @@ class Board:
         self.board[point] = np.roll(self.board[point], piece + 5*self.turn)
         return
 
+    def owner_piece(self, point):
+        ind = np.where(self.board[point] == 1)[0]
+        # 駒がない位置
+        if ind == 0:
+            return 2, 0
+        # 駒がある位置
+        else:
+            return (ind-1)/5, (ind-1) % 5 + 1
+
+    # 上下左右の編に接しているかどうか
+    def is_side(self, point):
+        is_up = point % 4 == 0
+        is_down = point % 4 == 2
+        is_left = point >= 8
+        is_right = point <= 3
+        return is_up, is_down, is_left, is_right
+
+    def hiyoko_move(self, point):
+        if self.turn == 0:
+            return [point-1]
+        else:
+            return [point+1]
+
+    def kirin_move(self, point):
+        u, d, l, r = self.is_side(point)
+        moves = []
+        if not u:
+            moves.append(point-1)
+        if not d:
+            moves.append(point+1)
+        if not l:
+            moves.append(point+4)
+        if not r:
+            moves.append(point-4)
+        return moves
+
+    def zou_move(self, point):
+        u, d, l, r = self.is_side(point)
+        moves = []
+        if not u:
+            if not l:
+                moves.append(point+3)
+            if not r:
+                moves.append(point-5)
+        if not d:
+            if not l:
+                moves.append(point+5)
+            if not r:
+                moves.append(point-3)
+        return moves
+
+    def lion_move(self, point):
+        m1 = self.kirin_move(point)
+        m2 = self.zou_move(point)
+        return m1.extend(m2)
+
+    def niwatori_move(self, point):
+        moves = self.kirin_move(point)
+        u, d, l, r = self.is_side(point)
+        if self.turn == 0:
+            if not u:
+                if not l:
+                    moves.append(point+3)
+                if not r:
+                    moves.append(point-5)
+        else:
+            if not d:
+                if not l:
+                    moves.append(point+5)
+                if not r:
+                    moves.append(point-3)
+        return moves
+
+    def point_moves(self, point, piece):
+        if piece == 1:
+            return self.hiyoko_move(point)
+        if piece == 2:
+            return self.kirin_move(point)
+        if piece == 3:
+            return self.zou_move(point)
+        if piece == 4:
+            return self.lion_move(point)
+        if piece == 5:
+            return self.niwatori_move(point)
+
+    def legal_moves(self):
+        moves = []
+        for i in range(12):
+            owner, piece = self.owner_piece(i)
+            if owner == self.turn:
+                points = self.point_moves(i, piece)
+                for p in points:
+                    owner2, piece2 = self.owner_piece(p)
+                    # 自分の駒がある場所には動けない
+                    if owner2 == self.turn:
+                        continue
+                    # ひよこが最奥までいった場合、強制的に成る
+                    if piece == 1 and p % 4 == 0:
+                        moves.append([i, p, piece, piece2, 1])
+                    else:
+                        moves.append([i, p, piece, piece2, 0])
