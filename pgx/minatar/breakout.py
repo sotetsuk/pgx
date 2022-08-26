a81e8656461322d1982d6de3ab43ef82d5c19c0e
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2022-08-26 16:00:05 +0900

    copy

--- pgx/minatar/breakout.py
+++ pgx/minatar/breakout.py
@@ -9,7 +9,6 @@ The original MinAtar implementation is distributed under GNU General Public Lice
 
 import numpy as np
 
-
 #####################################################################################################################
 # Env
 #
@@ -20,6 +19,75 @@ import numpy as np
 # the ball hits the bottom of the screen. The balls direction is indicated by a trail channel.
 #
 #####################################################################################################################
+
+
+def step(state, action):
+    r = 0
+    if self.terminal:
+        return r, self.terminal
+
+    a = self.action_map[a]
+
+    # Resolve player action
+    if a == "l":
+        self.pos = max(0, self.pos - 1)
+    elif a == "r":
+        self.pos = min(9, self.pos + 1)
+
+    # Update ball position
+    self.last_x = self.ball_x
+    self.last_y = self.ball_y
+    if self.ball_dir == 0:
+        new_x = self.ball_x - 1
+        new_y = self.ball_y - 1
+    elif self.ball_dir == 1:
+        new_x = self.ball_x + 1
+        new_y = self.ball_y - 1
+    elif self.ball_dir == 2:
+        new_x = self.ball_x + 1
+        new_y = self.ball_y + 1
+    elif self.ball_dir == 3:
+        new_x = self.ball_x - 1
+        new_y = self.ball_y + 1
+
+    strike_toggle = False
+    if new_x < 0 or new_x > 9:
+        if new_x < 0:
+            new_x = 0
+        if new_x > 9:
+            new_x = 9
+        self.ball_dir = [1, 0, 3, 2][self.ball_dir]
+    if new_y < 0:
+        new_y = 0
+        self.ball_dir = [3, 2, 1, 0][self.ball_dir]
+    elif self.brick_map[new_y, new_x] == 1:
+        strike_toggle = True
+        if not self.strike:
+            r += 1
+            self.strike = True
+            self.brick_map[new_y, new_x] = 0
+            new_y = self.last_y
+            self.ball_dir = [3, 2, 1, 0][self.ball_dir]
+    elif new_y == 9:
+        if np.count_nonzero(self.brick_map) == 0:
+            self.brick_map[1:4, :] = 1
+        if self.ball_x == self.pos:
+            self.ball_dir = [3, 2, 1, 0][self.ball_dir]
+            new_y = self.last_y
+        elif new_x == self.pos:
+            self.ball_dir = [2, 3, 0, 1][self.ball_dir]
+            new_y = self.last_y
+        else:
+            self.terminal = True
+
+    if not strike_toggle:
+        self.strike = False
+
+    self.ball_x = new_x
+    self.ball_y = new_y
+    return r, self.terminal
+
+
 class Env:
     def __init__(self, ramping=None, random_state=None):
         self.channels = {
