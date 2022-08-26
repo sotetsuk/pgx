ac62ba7f96e0cc6e9e0fe80fe393589d20bc0da4
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2022-08-26 16:50:25 +0900

    implement base

--- pgx/minatar/breakout.py
+++ pgx/minatar/breakout.py
@@ -6,8 +6,13 @@ The authors of original MinAtar implementation are:
 The original MinAtar implementation is distributed under GNU General Public License v3.0
     * https://github.com/kenjyoung/MinAtar/blob/master/License.txt
 """
+import copy
+from dataclasses import dataclass
+from typing import Tuple
 
 import numpy as np
+from jax import numpy as jnp
+
 
 #####################################################################################################################
 # Env
@@ -19,36 +24,53 @@ import numpy as np
 # the ball hits the bottom of the screen. The balls direction is indicated by a trail channel.
 #
 #####################################################################################################################
+@dataclass
+class MinAtarBreakoutState:
+    ball_y: int = 3
+    ball_x: int = 0
+    ball_dir: int = 2
+    pos: int = 4
+    brick_map: jnp.ndarray = jnp.zeros((10, 10))
+    strike: bool = False
+    last_x: int = 0
+    last_y: int = 3
+    terminal: bool = False
+    last_action: int = 0
 
 
-def step(state, action):
+# TODO: sticky action prob
+def step(
+    state: MinAtarBreakoutState, action: int
+) -> Tuple[MinAtarBreakoutState, int, bool]:
+
     r = 0
-    if self.terminal:
-        return r, self.terminal
+    if state.terminal:
+        return state, r, state.terminal
 
-    a = self.action_map[a]
+    next_state = copy.deepcopy(state)
 
     # Resolve player action
-    if a == "l":
-        self.pos = max(0, self.pos - 1)
-    elif a == "r":
-        self.pos = min(9, self.pos + 1)
+    if action == 1:  # "l"
+        next_state.pos = max(0, next_state.pos - 1)
+    elif action == 3:  # "r"
+        next_state.pos = min(9, next_state.pos + 1)
 
     # Update ball position
-    self.last_x = self.ball_x
-    self.last_y = self.ball_y
-    if self.ball_dir == 0:
-        new_x = self.ball_x - 1
-        new_y = self.ball_y - 1
-    elif self.ball_dir == 1:
-        new_x = self.ball_x + 1
-        new_y = self.ball_y - 1
-    elif self.ball_dir == 2:
-        new_x = self.ball_x + 1
-        new_y = self.ball_y + 1
-    elif self.ball_dir == 3:
-        new_x = self.ball_x - 1
-        new_y = self.ball_y + 1
+    next_state.last_x = next_state.ball_x
+    next_state.last_y = next_state.ball_y
+    assert next_state.ball_dir in [0, 1, 2, 3]
+    if next_state.ball_dir == 0:
+        new_x = next_state.ball_x - 1
+        new_y = next_state.ball_y - 1
+    elif next_state.ball_dir == 1:
+        new_x = next_state.ball_x + 1
+        new_y = next_state.ball_y - 1
+    elif next_state.ball_dir == 2:
+        new_x = next_state.ball_x + 1
+        new_y = next_state.ball_y + 1
+    elif next_state.ball_dir == 3:
+        new_x = next_state.ball_x - 1
+        new_y = next_state.ball_y + 1
 
     strike_toggle = False
     if new_x < 0 or new_x > 9:
@@ -56,152 +78,35 @@ def step(state, action):
             new_x = 0
         if new_x > 9:
             new_x = 9
-        self.ball_dir = [1, 0, 3, 2][self.ball_dir]
+        next_state.ball_dir = [1, 0, 3, 2][next_state.ball_dir]
     if new_y < 0:
         new_y = 0
-        self.ball_dir = [3, 2, 1, 0][self.ball_dir]
-    elif self.brick_map[new_y, new_x] == 1:
+        next_state.ball_dir = [3, 2, 1, 0][next_state.ball_dir]
+    elif next_state.brick_map[new_y, new_x] == 1:
         strike_toggle = True
-        if not self.strike:
+        if not next_state.strike:
             r += 1
-            self.strike = True
-            self.brick_map[new_y, new_x] = 0
-            new_y = self.last_y
-            self.ball_dir = [3, 2, 1, 0][self.ball_dir]
+            next_state.strike = True
+            next_state.brick_map[new_y, new_x] = 0
+            new_y = next_state.last_y
+            next_state.ball_dir = [3, 2, 1, 0][next_state.ball_dir]
     elif new_y == 9:
-        if np.count_nonzero(self.brick_map) == 0:
-            self.brick_map[1:4, :] = 1
-        if self.ball_x == self.pos:
-            self.ball_dir = [3, 2, 1, 0][self.ball_dir]
-            new_y = self.last_y
-        elif new_x == self.pos:
-            self.ball_dir = [2, 3, 0, 1][self.ball_dir]
-            new_y = self.last_y
+        if np.count_nonzero(next_state.brick_map) == 0:
+            next_state.brick_map[1:4, :] = 1
+        if next_state.ball_x == next_state.pos:
+            next_state.ball_dir = [3, 2, 1, 0][next_state.ball_dir]
+            new_y = next_state.last_y
+        elif new_x == next_state.pos:
+            next_state.ball_dir = [2, 3, 0, 1][next_state.ball_dir]
+            new_y = next_state.last_y
         else:
-            self.terminal = True
+            next_state.terminal = True
 
     if not strike_toggle:
-        self.strike = False
+        next_state.strike = False
 
-    self.ball_x = new_x
-    self.ball_y = new_y
-    return r, self.terminal
+    next_state.ball_x = new_x
+    next_state.ball_y = new_y
+    next_state.last_action = action
 
-
-class Env:
-    def __init__(self, ramping=None, random_state=None):
-        self.channels = {
-            "paddle": 0,
-            "ball": 1,
-            "trail": 2,
-            "brick": 3,
-        }
-        self.action_map = ["n", "l", "u", "r", "d", "f"]
-        if random_state is None:
-            self.random = np.random.RandomState()
-        else:
-            self.random = random_state
-        self.reset()
-
-    # Update environment according to agent action
-    def act(self, a):
-        r = 0
-        if self.terminal:
-            return r, self.terminal
-
-        a = self.action_map[a]
-
-        # Resolve player action
-        if a == "l":
-            self.pos = max(0, self.pos - 1)
-        elif a == "r":
-            self.pos = min(9, self.pos + 1)
-
-        # Update ball position
-        self.last_x = self.ball_x
-        self.last_y = self.ball_y
-        if self.ball_dir == 0:
-            new_x = self.ball_x - 1
-            new_y = self.ball_y - 1
-        elif self.ball_dir == 1:
-            new_x = self.ball_x + 1
-            new_y = self.ball_y - 1
-        elif self.ball_dir == 2:
-            new_x = self.ball_x + 1
-            new_y = self.ball_y + 1
-        elif self.ball_dir == 3:
-            new_x = self.ball_x - 1
-            new_y = self.ball_y + 1
-
-        strike_toggle = False
-        if new_x < 0 or new_x > 9:
-            if new_x < 0:
-                new_x = 0
-            if new_x > 9:
-                new_x = 9
-            self.ball_dir = [1, 0, 3, 2][self.ball_dir]
-        if new_y < 0:
-            new_y = 0
-            self.ball_dir = [3, 2, 1, 0][self.ball_dir]
-        elif self.brick_map[new_y, new_x] == 1:
-            strike_toggle = True
-            if not self.strike:
-                r += 1
-                self.strike = True
-                self.brick_map[new_y, new_x] = 0
-                new_y = self.last_y
-                self.ball_dir = [3, 2, 1, 0][self.ball_dir]
-        elif new_y == 9:
-            if np.count_nonzero(self.brick_map) == 0:
-                self.brick_map[1:4, :] = 1
-            if self.ball_x == self.pos:
-                self.ball_dir = [3, 2, 1, 0][self.ball_dir]
-                new_y = self.last_y
-            elif new_x == self.pos:
-                self.ball_dir = [2, 3, 0, 1][self.ball_dir]
-                new_y = self.last_y
-            else:
-                self.terminal = True
-
-        if not strike_toggle:
-            self.strike = False
-
-        self.ball_x = new_x
-        self.ball_y = new_y
-        return r, self.terminal
-
-    # Query the current level of the difficulty ramp, difficulty does not ramp in this game, so return None
-    def difficulty_ramp(self):
-        return None
-
-        # Process the game-state into the 10x10xn state provided to the agent and return
-
-    def state(self):
-        state = np.zeros((10, 10, len(self.channels)), dtype=bool)
-        state[self.ball_y, self.ball_x, self.channels["ball"]] = 1
-        state[9, self.pos, self.channels["paddle"]] = 1
-        state[self.last_y, self.last_x, self.channels["trail"]] = 1
-        state[:, :, self.channels["brick"]] = self.brick_map
-        return state
-
-    # Reset to start state for new episode
-    def reset(self):
-        self.ball_y = 3
-        ball_start = self.random.choice(2)
-        self.ball_x, self.ball_dir = [(0, 2), (9, 3)][ball_start]
-        self.pos = 4
-        self.brick_map = np.zeros((10, 10))
-        self.brick_map[1:4, :] = 1
-        self.strike = False
-        self.last_x = self.ball_x
-        self.last_y = self.ball_y
-        self.terminal = False
-
-    # Dimensionality of the game-state (10x10xn)
-    def state_shape(self):
-        return [10, 10, len(self.channels)]
-
-    # Subset of actions that actually have a unique impact in this environment
-    def minimal_action_set(self):
-        minimal_actions = ["n", "l", "r"]
-        return [self.action_map.index(x) for x in minimal_actions]
+    return next_state, r, next_state.terminal
--- tests/test_breakout.py
+++ tests/test_breakout.py
@@ -1,8 +1,12 @@
 import copy
 import random
+from typing import Any, Dict
 
 import numpy as np
+from jax import numpy as jnp
 from minatar import Environment
+
+from pgx.minatar import breakout
 
 breakout_state_keys = {
     "ball_y",
@@ -14,6 +18,7 @@ breakout_state_keys = {
     "last_x",
     "last_y",
     "terminal",
+    "last_action",
 }
 
 
@@ -21,10 +26,10 @@ def extract_state(env, state_keys):
     state_dict = {}
     # task-dependent attribute
     for k in state_keys:
-        state_dict[k] = copy.deepcopy(getattr(env.env, k))
-    # last_action and random state
-    for k in ("last_action",):
-        state_dict[k] = copy.deepcopy(getattr(env, k))
+        if k in ("last_action",):
+            state_dict[k] = copy.deepcopy(getattr(env, k))
+        else:
+            state_dict[k] = copy.deepcopy(getattr(env.env, k))
     return state_dict
 
 
@@ -37,6 +42,25 @@ def assert_states(state1, state2):
         ), f"{key}, {state1[key]}, {state2[key]}"
 
 
+def pgx2minatar(state: breakout.MinAtarBreakoutState) -> Dict[str, Any]:
+    d = {}
+    for key in breakout_state_keys:
+        d[key] = copy.deepcopy(getattr(state, key))
+        if isinstance(d[key], jnp.ndarray):
+            d[key] = np.array(d[key])
+    return d
+
+
+def minatar2pgx(state_dict: Dict[str, Any]) -> breakout.MinAtarBreakoutState:
+    state = breakout.MinAtarBreakoutState()
+    for key in breakout_state_keys:
+        val = copy.deepcopy(state_dict[key])
+        if isinstance(val, np.ndarray):
+            val = jnp.array(val)
+        setattr(state, key, val)
+    return state
+
+
 def test_step():
     env = Environment("breakout", sticky_action_prob=0.0)
     num_actions = env.num_actions()
@@ -44,7 +68,9 @@ def test_step():
     env.reset()
     s = extract_state(env, breakout_state_keys)
     a = random.randrange(num_actions)
-    env.act(a)
+    r, done = env.act(a)
     s_next = extract_state(env, breakout_state_keys)
 
-    assert_states(s, s_next)
+    s_next_pgx, r, done = breakout.step(minatar2pgx(s), a)
+
+    assert_states(s_next, pgx2minatar(s_next_pgx))
