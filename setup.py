a749d32c4bc216b25daaeba717f5be0757649943
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2022-08-29 19:55:32 +0900

    [feat] Implement MinAtar Breakout (#4)
    * init
    
    * rm submodule
    
    * add test
    
    * update tests
    
    * copy minatar/breakout.py
    
    * doc
    
    * copy
    
    * fix req
    
    * implement base
    
    * pass test
    
    * use lax.cond
    
    * use cond
    
    * enhance test
    
    * update
    
    * tidy
    
    * fix
    
    * update
    
    * fix
    
    * replace fn
    
    * extract
    
    * update
    
    * replace
    
    * replace
    
    * finish
    
    * finish determinsitic step
    
    * add _reset_det
    
    * use jit
    
    * fix docs
    
    * add to_obs
    
    * fix
    
    * update requirements
    
    * update requirements
    
    * fix check

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 from setuptools import setup
 
-
 setup(
-    name='pgx',
-    version='0.0.1',
-    description='',
-    url='',
-    author='Sotetsu KOYAMADA',
-    author_email='sotetsu.koyamada@gmail.com',
-    keywords='',
+    name="pgx",
+    version="0.0.1",
+    description="",
+    url="",
+    author="Sotetsu KOYAMADA",
+    author_email="sotetsu.koyamada@gmail.com",
+    keywords="",
     packages=["pgx"],
-    install_requires=[
-        "jax",
-        ],
+    install_requires=["jax", "flax"],
     classifiers=[
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3.8",
     ],
 )
