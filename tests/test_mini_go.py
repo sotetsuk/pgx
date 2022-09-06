af7ebd812df3697c117f68d9b3cde03e8e0d25ef
OkanoShinri <n.salt2000@gmail.com>
2022-09-06 13:55:04 +0900

    add test

---
+++ tests/test_mini_go.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from pgx.mini_go import BLACK, WHITE, _is_surrounded, init, step
+
+
+def test_sorrounded():
+    state = init()
+    state, _, _ = step(state=state, action=np.array([0, 1, BLACK]))
+    state, _, _ = step(state=state, action=np.array([1, 0, BLACK]))
+    state, _, _ = step(state=state, action=np.array([1, 2, BLACK]))
+    state, _, _ = step(state=state, action=np.array([2, 1, BLACK]))
+    state, _, _ = step(state=state, action=np.array([1, 1, WHITE]))
+    state, _, _ = step(state=state, action=np.array([4, 0, BLACK]))
+    """
+      [ 0 1 2 3 4 ]
+    [0] + @ + + +
+    [1] @ O @ + +
+    [2] + @ + + +
+    [3] + + + + +
+    [4] @ + + + +
+    """
+
+    b, _ = _is_surrounded(
+        state.board, 1, 1, WHITE, np.zeros_like(state.board, dtype=bool)
+    )
+    assert b
+
+    b, _ = _is_surrounded(
+        state.board, 4, 0, BLACK, np.zeros_like(state.board, dtype=bool)
+    )
+    assert not b
