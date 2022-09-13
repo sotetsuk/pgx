6409bb958e312b2c69940b4633e6c2b419d840d9
youyou-ku <73025744+youyou-ku@users.noreply.github.com>
2022-09-13 20:54:22 +0900

    駒の利きをnp.arrayで管理 (#24)
    * add moves array
    
    * add white test
    
    * add white test2
    
    * add effects
    
    * add effects test
    
    * add prev_move
    
    * add checked
    
    * revise legal_actions
    
    * actions change, add is_suicide, leave_check

--- pgx/animal_shogi.py
+++ pgx/animal_shogi.py
@@ -1,6 +1,26 @@
+import copy
 from dataclasses import dataclass
 
 import numpy as np
+
+
+# 指し手のdataclass
+@dataclass
+class AnimalShogiAction:
+    # 上の3つは移動と駒打ちで共用
+    # 下の3つは移動でのみ使用
+    # 駒打ちかどうか
+    is_drop: int
+    # piece: 動かした(打った)駒の種類
+    piece: int
+    # final: 移動後の座標
+    final: int
+    # 移動前の座標
+    first: int = 0
+    # captured: 取られた駒の種類。駒が取られていない場合は0
+    captured: int = 0
+    # is_promote: 駒を成るかどうかの判定
+    is_promote: int = 0
 
 
 # 盤面のdataclass
@@ -15,57 +35,69 @@ class AnimalShogiState:
     board: np.ndarray = np.zeros((11, 12), dtype=np.int32)
     # hand 持ち駒。先手ヒヨコ,先手キリン,先手ゾウ,後手ヒヨコ,後手キリン,後手ゾウの6種の値を増減させる
     hand: np.ndarray = np.zeros(6, dtype=np.int32)
+    # checked: ターンプレイヤーの王に王手がかかっているかどうか
+    checked: int = 0
+    # checking_piece: ターンプレイヤーに王手をかけている駒の座標
+    checking_piece: np.ndarray = np.zeros(12, dtype=np.int32)
 
 
-# 指し手のdataclass
-@dataclass
-class AnimalShogiAction:
-    # 上の3つは移動と駒打ちで共用
-    # 下の3つは移動でのみ使用
-    # 駒打ちかどうか
-    is_drop: bool
-    # piece: 動かした(打った)駒の種類
-    piece: int
-    # final: 移動後の座標
-    final: int
-    # 移動前の座標
-    first: int = 0
-    # captured: 取られた駒の種類。駒が取られていない場合は0
-    captured: int = 0
-    # is_promote: 駒を成るかどうかの判定
-    is_promote: int = 0
+# BLACK/WHITE/(NONE)_○○_MOVEは22にいるときの各駒の動き
+# 端にいる場合は対応するところに0をかけていけないようにする
+BLACK_PAWN_MOVE = np.array([[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]])
+WHITE_PAWN_MOVE = np.array([[0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]])
+BLACK_GOLD_MOVE = np.array([[1, 1, 0, 0], [1, 0, 1, 0], [1, 1, 0, 0]])
+WHITE_GOLD_MOVE = np.array([[0, 1, 1, 0], [1, 0, 1, 0], [0, 1, 1, 0]])
+ROOK_MOVE = np.array([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 0]])
+BISHOP_MOVE = np.array([[1, 0, 1, 0], [0, 0, 0, 0], [1, 0, 1, 0]])
+KING_MOVE = np.array([[1, 1, 1, 0], [1, 0, 1, 0], [1, 1, 1, 0]])
 
 
-# 手番を変更する
-def turn_change(state: AnimalShogiState):
-    state.turn = (state.turn + 1) % 2
-    return state
+# 手番側でない色を返す
+def another_color(state: AnimalShogiState):
+    return (state.turn + 1) % 2
 
 
-#  駒打ちでない移動の処理
+#  駒打ちでない移動の処理 手番変更、盤面書き換えなし
 def move(
     state: AnimalShogiState,
     act: AnimalShogiAction,
 ):
-    state.board[act.piece][act.first] = 0
-    state.board[0][act.first] = 1
-    state.board[act.captured][act.final] = 0
-    state.board[act.piece + 4 * act.is_promote][act.final] = 1
+    s = copy.deepcopy(state)
+    s.board[act.piece][act.first] = 0
+    s.board[0][act.first] = 1
+    s.board[act.captured][act.final] = 0
+    s.board[act.piece + 4 * act.is_promote][act.final] = 1
     if act.captured != 0:
-        if state.turn == 0:
-            state.hand[(act.captured - 6) % 4] += 1
+        if s.turn == 0:
+            s.hand[(act.captured - 6) % 4] += 1
         else:
-            state.hand[act.captured % 4 + 2] += 1
-    state = turn_change(state)
-    return state
+            s.hand[act.captured % 4 + 2] += 1
+    return s
 
 
-#  駒打ちの処理
+#  駒打ちの処理 手番変更、盤面書き換えなし
 def drop(state: AnimalShogiState, act: AnimalShogiAction):
-    state.hand[act.piece - 1 - 2 * state.turn] -= 1
-    state.board[act.piece][act.final] = 1
-    state.board[0][act.final] = 0
-    state = turn_change(state)
+    s = copy.deepcopy(state)
+    s.hand[act.piece - 1 - 2 * state.turn] -= 1
+    s.board[act.piece][act.final] = 1
+    s.board[0][act.final] = 0
+    return s
+
+
+# stateとactを受け取りis_dropによって操作を分ける
+# 手番、王手判定も更新。引数の盤面も書き換える
+def action(state: AnimalShogiState, act: AnimalShogiAction):
+    if act.is_drop == 1:
+        state = drop(state, act)
+    else:
+        state = move(state, act)
+    state.turn = another_color(state)
+    state.checked = is_check(state)
+    # 王手をかけている駒は直前に動かした駒
+    if state.checked:
+        state.checking_piece[act.final] = 1
+    else:
+        state.checking_piece = np.zeros(12, dtype=np.int32)
     return state
 
 
@@ -75,11 +107,23 @@ def piece_type(state: AnimalShogiState, point: int):
 
 
 # 盤面のどこに何の駒があるかをnp.arrayに移したもの
-# 同じ座標に複数回poece_typeを使用する場合はこちらを使った方が良い
+# 同じ座標に複数回piece_typeを使用する場合はこちらを使った方が良い
 def board_status(state: AnimalShogiState):
-    board = np.zeros(12)
+    board = np.zeros(12, dtype=np.int32)
     for i in range(12):
         board[i] = piece_type(state, i)
+    return board
+
+
+# 駒の持ち主の判定
+def pieces_owner(state: AnimalShogiState):
+    board = np.zeros(12, dtype=np.int32)
+    for i in range(12):
+        piece = piece_type(state, i)
+        if piece == 0:
+            board[i] = 2
+        else:
+            board[i] = (piece - 1) // 5
     return board
 
 
@@ -93,131 +137,152 @@ def is_side(point):
     return is_up, is_down, is_left, is_right
 
 
-#  各駒の動き
-def pawn_move(turn, point):
-    #  最奥にいてはいけない
-    if turn == 0:
-        assert point % 4 != 0
-        return [point - 1]
-    else:
-        assert point % 4 != 3
-        return [point + 1]
+# point(0~11)を座標(00~23)に変換
+def convert_point(point):
+    return point // 4, point % 4
+
+
+# はみ出す部分をカットする
+def cut_outside(array, point):
+    u, d, l, r = is_side(point)
+    if u:
+        array[:, 0] *= 0
+    if d:
+        array[:, 2] *= 0
+    if r:
+        array[0, :] *= 0
+    if l:
+        array[2, :] *= 0
+
+
+def return_board(array, point):
+    y, t = convert_point(point)
+    cut_outside(array, point)
+    return np.roll(array, (y - 1, t - 1), axis=(0, 1))
+
+
+# 各駒の動き
+def black_pawn_move(point):
+    return return_board(np.copy(BLACK_PAWN_MOVE), point)
+
+
+def white_pawn_move(point):
+    return return_board(np.copy(WHITE_PAWN_MOVE), point)
+
+
+def black_gold_move(point):
+    return return_board(np.copy(BLACK_GOLD_MOVE), point)
+
+
+def white_gold_move(point):
+    return return_board(np.copy(WHITE_GOLD_MOVE), point)
 
 
 def rook_move(point):
-    u, d, l, r = is_side(point)
-    moves = []
-    if not r:
-        moves.append(point - 4)
-    if not u:
-        moves.append(point - 1)
-    if not d:
-        moves.append(point + 1)
-    if not l:
-        moves.append(point + 4)
-    return moves
+    return return_board(np.copy(ROOK_MOVE), point)
 
 
 def bishop_move(point):
-    u, d, l, r = is_side(point)
-    moves = []
-    if not r:
-        if not u:
-            moves.append(point - 5)
-        if not d:
-            moves.append(point - 3)
-    if not l:
-        if not u:
-            moves.append(point + 3)
-        if not d:
-            moves.append(point + 5)
-    return moves
+    return return_board(np.copy(BISHOP_MOVE), point)
 
 
 def king_move(point):
-    #  座標が小さい順に並ぶようにする
-    u, d, l, r = is_side(point)
-    moves = []
-    if not r:
-        if not u:
-            moves.append(point - 5)
-        moves.append(point - 4)
-        if not d:
-            moves.append(point - 3)
-    if not u:
-        moves.append(point - 1)
-    if not d:
-        moves.append(point + 1)
-    if not l:
-        if not u:
-            moves.append(point + 3)
-        moves.append(point + 4)
-        if not d:
-            moves.append(point + 5)
-    return moves
-
-
-def gold_move(turn, point):
-    #  座標が小さい順に並ぶようにする
-    u, d, l, r = is_side(point)
-    moves = []
-    if not r:
-        if not u and turn == 0:
-            moves.append(point - 5)
-        moves.append(point - 4)
-        if not d and turn == 1:
-            moves.append(point - 3)
-    if not u:
-        moves.append(point - 1)
-    if not d:
-        moves.append(point + 1)
-    if not l:
-        if not u and turn == 0:
-            moves.append(point + 3)
-        moves.append(point + 4)
-        if not d and turn == 1:
-            moves.append(point + 5)
-    return moves
+    return return_board(np.copy(KING_MOVE), point)
 
 
 #  座標と駒の種類から到達できる座標を列挙する関数
 def point_moves(piece, point):
-    turn = (piece - 1) // 5
-    if piece % 5 == 1:
-        return pawn_move(turn, point)
+    if piece == 1:
+        return black_pawn_move(point)
+    if piece == 6:
+        return white_pawn_move(point)
     if piece % 5 == 2:
         return rook_move(point)
     if piece % 5 == 3:
         return bishop_move(point)
     if piece % 5 == 4:
         return king_move(point)
-    if piece % 5 == 0:
-        return gold_move(turn, point)
+    if piece == 5:
+        return black_gold_move(point)
+    if piece == 10:
+        return white_gold_move(point)
+
+
+# 利きの判定
+def effected(state: AnimalShogiState, turn: int):
+    all_effect = np.zeros(12)
+    board = board_status(state)
+    piece_owner = pieces_owner(state)
+    for i in range(12):
+        own = piece_owner[i]
+        if own != turn:
+            continue
+        piece = board[i]
+        effect = point_moves(piece, i).reshape(12)
+        all_effect += effect
+    return all_effect
+
+
+# 自殺手判定
+def is_suicide(piece, position, effects):
+    # ライオン以外は関係ない
+    if piece % 5 != 4:
+        return False
+    # 行先に相手の駒の利きがあるかどうか
+    return effects[position] != 0
+
+
+# 王手放置判定
+def leave_check(piece, position, check, cp):
+    if not check:
+        return False
+    # 玉が動いていればとりあえず放置ではない（自殺手の可能性はある）
+    if piece % 5 == 4:
+        return False
+    # 両王手などについてはどうぶつ将棋では考えない
+    # cp[position] が1のところに動いていれば、王手を回避できている
+    return cp[position] == 0
+
+
+# 王手の判定(turn側の王に王手がかかっているかを判定)
+def is_check(state: AnimalShogiState):
+    effects = effected(state, another_color(state))
+    king_location = state.board[4 + 5 * state.turn, :].argmax()
+    return effects[king_location] != 0
 
 
 #  駒打ち以外の合法手を列挙する
 def legal_moves(state: AnimalShogiState):
     board = board_status(state)
+    piece_owner = pieces_owner(state)
+    # 相手の駒の利き
+    effects = effected(state, another_color(state))
     moves = []
     for i in range(12):
-        piece = board[i]
-        # 自分の駒の時のみ動かせる
-        if (piece - 1) // 5 != state.turn:
+        if piece_owner[i] != state.turn:
             continue
-        points = point_moves(piece, i)
-        # 可変長なので後に修正
-        for p in points:
-            piece2 = board[p]
-            # 自分の駒がある場所には動けない
-            if (piece2 - 1) // 5 == state.turn:
+        piece = board[i]
+        points = point_moves(piece, i).reshape(12)
+        for p in range(12):
+            if points[p] == 0:
                 continue
+            if piece_owner[p] == state.turn:
+                continue
+            piece2 = board[p]
             # ひよこが最奥までいった場合、強制的に成る
+            # なぜかpieceが小数に変換されてしまうのでとりあえずintに変換しておく
             if piece == 1 and p % 4 == 0:
-                moves.append(AnimalShogiAction(False, piece, p, i, piece2, 1))
+                m = AnimalShogiAction(0, piece, p, i, piece2, 1)
             elif piece == 6 and p % 4 == 3:
-                moves.append(AnimalShogiAction(False, piece, p, i, piece2, 1))
+                m = AnimalShogiAction(0, piece, p, i, piece2, 1)
             else:
-                moves.append(AnimalShogiAction(False, piece, p, i, piece2, 0))
+                m = AnimalShogiAction(0, piece, p, i, piece2, 0)
+            # mを行った後の盤面（手番はそのまま）
+            after = move(state, m)
+            # mを行った後も自分の玉に王手がかかっていてはいけない
+            if is_check(after):
+                continue
+            moves.append(m)
     return moves
 
 
@@ -239,5 +304,10 @@ def legal_drop(state: AnimalShogiState):
                 continue
             if piece == 6 and j % 4 == 3:
                 continue
-            moves.append(AnimalShogiAction(True, piece, j))
+            d = AnimalShogiAction(1, piece, j)
+            s = drop(state, d)
+            # 自玉が取られるような手は打てない
+            if is_check(s):
+                continue
+            moves.append(d)
     return moves
--- tests/test_animal_shogi.py
+++ tests/test_animal_shogi.py
@@ -37,12 +37,30 @@ TEST_BOARD = AnimalShogiState(
     ]),
     hand=np.array([1, 2, 1, 0, 0, 0])
 )
+TEST_BOARD2 = AnimalShogiState(
+    turn=1,
+    board=np.array([
+        [0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]),
+    hand=np.array([0, 0, 0, 1, 1, 1])
+)
 
 
-def test_turn_change():
+def test_another_color():
     b = copy.deepcopy(INIT_BOARD)
-    s = turn_change(b)
-    assert s.turn == 1
+    assert another_color(b) == 1
+    b2 = copy.deepcopy(TEST_BOARD2)
+    assert another_color(b2) == 0
 
 
 def test_move():
@@ -54,7 +72,6 @@ def test_move():
     assert s.board[1][5] == 1
     assert s.board[6][5] == 0
     assert s.hand[0] == 1
-    assert s.turn == 1
     b2 = copy.deepcopy(TEST_BOARD)
     m2 = AnimalShogiAction(False, 1, 0, 1, 8, 1)
     s2 = move(b2, m2)
@@ -63,7 +80,14 @@ def test_move():
     assert s2.board[5][0] == 1
     assert s2.board[8][0] == 0
     assert s2.hand[2] == 2
-    assert s2.turn == 1
+    b3 = copy.deepcopy(TEST_BOARD2)
+    m3 = AnimalShogiAction(False, 6, 7, 6, 2, 1)
+    s3 = move(b3, m3)
+    assert s3.board[0][6] == 1
+    assert s3.board[6][6] == 0
+    assert s3.board[10][7] == 1
+    assert s3.board[2][7] == 0
+    assert s3.hand[4] == 2
 
 
 def test_drop():
@@ -72,11 +96,19 @@ def test_drop():
     s = drop(b, d)
     assert s.hand[2] == 0
     assert s.board[3][2] == 1
+    assert s.board[0][2] == 0
     b2 = copy.deepcopy(TEST_BOARD)
     d2 = AnimalShogiAction(True, 1, 5)
     s2 = drop(b2, d2)
     assert s2.hand[0] == 0
     assert s2.board[1][5] == 1
+    assert s2.board[0][5] == 0
+    b3 = copy.deepcopy(TEST_BOARD2)
+    d3 = AnimalShogiAction(True, 7, 2)
+    s3 = drop(b3, d3)
+    assert s3.hand[4] == 0
+    assert s3.board[7][2] == 1
+    assert s3.board[0][2] == 0
 
 
 def test_piece_type():
@@ -85,39 +117,57 @@ def test_piece_type():
     assert piece_type(INIT_BOARD, 9) == 0
 
 
+def test_effected():
+    assert np.all(effected(INIT_BOARD, 1) == np.array([1, 1, 0, 0, 1, 2, 1, 0, 1, 2, 0, 0]))
+    assert np.all(effected(TEST_BOARD, 0) == np.array([1, 0, 2, 0, 0, 1, 2, 3, 0, 0, 2, 0]))
+    assert np.all(effected(TEST_BOARD2, 1) == np.array([3, 1, 2, 0, 0, 3, 1, 1, 2, 1, 1, 0]))
+
+
+def test_is_check():
+    assert not is_check(INIT_BOARD)
+    assert is_check(TEST_BOARD)
+    assert not is_check(TEST_BOARD2)
+
+
 def test_legal_move():
     assert legal_moves(INIT_BOARD) == \
-           [AnimalShogiAction(is_drop=False, piece=2, final=2, first=3, captured=0, is_promote=0),
-            AnimalShogiAction(is_drop=False, piece=1, final=5, first=6, captured=6, is_promote=0),
-            AnimalShogiAction(is_drop=False, piece=4, final=2, first=7, captured=0, is_promote=0),
-            AnimalShogiAction(is_drop=False, piece=4, final=10, first=7, captured=0, is_promote=0)]
+           [AnimalShogiAction(is_drop=0, piece=2, final=2, first=3, captured=0, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=1, final=5, first=6, captured=6, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=4, final=2, first=7, captured=0, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=4, final=10, first=7, captured=0, is_promote=0)]
+    # 王手を受けている状態の挙動
     assert legal_moves(TEST_BOARD) == \
-           [AnimalShogiAction(is_drop=False, piece=1, final=0, first=1, captured=8, is_promote=1),
-            AnimalShogiAction(is_drop=False, piece=5, final=2, first=3, captured=0, is_promote=0),
-            AnimalShogiAction(is_drop=False, piece=5, final=7, first=3, captured=7, is_promote=0),
-            AnimalShogiAction(is_drop=False, piece=2, final=2, first=6, captured=0, is_promote=0),
-            AnimalShogiAction(is_drop=False, piece=2, final=5, first=6, captured=0, is_promote=0),
-            AnimalShogiAction(is_drop=False, piece=2, final=7, first=6, captured=7, is_promote=0),
-            AnimalShogiAction(is_drop=False, piece=2, final=10, first=6, captured=0, is_promote=0),
-            AnimalShogiAction(is_drop=False, piece=4, final=7, first=11, captured=7, is_promote=0),
-            AnimalShogiAction(is_drop=False, piece=4, final=10, first=11, captured=0, is_promote=0)]
+           [AnimalShogiAction(is_drop=0, piece=5, final=7, first=3, captured=7, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=2, final=7, first=6, captured=7, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=4, final=7, first=11, captured=7, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=4, final=10, first=11, captured=0, is_promote=0)]
+    assert legal_moves(TEST_BOARD2) == \
+           [AnimalShogiAction(is_drop=0, piece=10, final=2, first=1, captured=0, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=9, final=8, first=4, captured=0, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=9, final=9, first=4, captured=0, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=8, final=2, first=5, captured=0, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=8, final=8, first=5, captured=0, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=8, final=10, first=5, captured=0, is_promote=0),
+            AnimalShogiAction(is_drop=0, piece=6, final=7, first=6, captured=2, is_promote=1)]
 
 
 def test_legal_drop():
-    assert legal_drop(TEST_BOARD) == \
-           [AnimalShogiAction(is_drop=True, piece=1, final=2), AnimalShogiAction(is_drop=True, piece=1, final=5),
-            AnimalShogiAction(is_drop=True, piece=1, final=9), AnimalShogiAction(is_drop=True, piece=1, final=10),
-            AnimalShogiAction(is_drop=True, piece=2, final=2), AnimalShogiAction(is_drop=True, piece=2, final=5),
-            AnimalShogiAction(is_drop=True, piece=2, final=8), AnimalShogiAction(is_drop=True, piece=2, final=9),
-            AnimalShogiAction(is_drop=True, piece=2, final=10), AnimalShogiAction(is_drop=True, piece=3, final=2),
-            AnimalShogiAction(is_drop=True, piece=3, final=5), AnimalShogiAction(is_drop=True, piece=3, final=8),
-            AnimalShogiAction(is_drop=True, piece=3, final=9), AnimalShogiAction(is_drop=True, piece=3, final=10)]
+    assert legal_drop(TEST_BOARD) == []
+    assert legal_drop(TEST_BOARD2) == \
+           [AnimalShogiAction(is_drop=1, piece=6, final=2), AnimalShogiAction(is_drop=True, piece=6, final=8),
+            AnimalShogiAction(is_drop=1, piece=6, final=9), AnimalShogiAction(is_drop=True, piece=6, final=10),
+            AnimalShogiAction(is_drop=1, piece=7, final=2), AnimalShogiAction(is_drop=True, piece=7, final=8),
+            AnimalShogiAction(is_drop=1, piece=7, final=9), AnimalShogiAction(is_drop=True, piece=7, final=10),
+            AnimalShogiAction(is_drop=1, piece=8, final=2), AnimalShogiAction(is_drop=True, piece=8, final=8),
+            AnimalShogiAction(is_drop=1, piece=8, final=9), AnimalShogiAction(is_drop=True, piece=8, final=10)]
 
 
 if __name__ == '__main__':
-    test_turn_change()
+    test_another_color()
     test_move()
     test_drop()
     test_piece_type()
+    test_effected()
+    test_is_check()
     test_legal_move()
     test_legal_drop()
