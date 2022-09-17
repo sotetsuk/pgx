21c8bfa9cbc480026ea9e03d990e0e12ded4bc5b
keigo habara <k.habara.aa@gmail.com>
2022-09-17 22:58:36 +0900

    use jax.numpy

--- pgx/mini_mahjong.py
+++ pgx/mini_mahjong.py
@@ -1,9 +1,8 @@
 from typing import Dict, List, Tuple
 
-import numpy as np
+import jax
+import jax.numpy as jnp
 from shanten_tools import shanten  # type: ignore
-
-np.random.seed(0)
 
 # TODO: 赤牌
 # TODO: リーチ
@@ -14,10 +13,11 @@ np.random.seed(0)
 
 
 class Deck:
-    def __init__(self):
+    def __init__(self, key: jax.random.PRNGKey):
         # TODO: 赤牌
-        self.deck = np.array([i // 4 for i in range(136)])
-        np.random.shuffle(self.deck)
+        self.deck = jax.random.permutation(
+            key, jnp.array([i // 4 for i in range(136)])
+        )
         self.idx = 0
         self.end = 136 - 14
 
@@ -33,27 +33,25 @@ class Deck:
 
 class Hand:
     def __init__(self):
-        self.hand = np.zeros(34, dtype=np.uint8)
+        self.hand = jnp.zeros(34, dtype=jnp.uint8)
 
     def size(self):
-        return np.sum(self.hand)
+        return jnp.sum(self.hand)
 
-    def add(self, tile: int):
-        assert self.hand[tile] < 4
-        self.hand[tile] += 1
+    def unique(self):
+        return jnp.where(self.hand > 0)[0].tolist()
 
-    def sub(self, tile: int):
-        assert self.hand[tile] > 0
-        self.hand[tile] -= 1
+    def add(self, tile: int, x: int = 1):
+        self.hand = self.hand.at[tile].set(self.hand[tile] + x)
 
-    def to_indices(self):
-        return np.where(self.hand > 0)[0].tolist()
+    def sub(self, tile: int, x: int = 1):
+        self.add(tile, -x)
 
     def can_ron(self, tile: int):
         assert self.hand[tile] < 4
-        self.hand[tile] += 1
+        self.add(tile)
         can_ron = shanten(self.hand) == -1
-        self.hand[tile] -= 1
+        self.sub(tile)
         return can_ron
 
     def can_tsumo(self):
@@ -91,29 +89,22 @@ class Hand:
         return chis
 
     def pon(self, tile: int):
-        assert self.hand[tile] >= 2
-        self.hand[tile] -= 2
+        self.sub(tile, 2)
 
     def chi(self, tile: int, chi: int):
         assert tile < 27
         if chi == CHI_R:
             assert tile % 9 > 1
-            assert self.hand[tile - 2] > 0
-            assert self.hand[tile - 1] > 0
-            self.hand[tile - 2] -= 1
-            self.hand[tile - 1] -= 1
+            self.sub(tile - 2)
+            self.sub(tile - 1)
         if chi == CHI_M:
             assert tile % 9 > 0 and tile % 9 < 8
-            assert self.hand[tile - 1] > 0
-            assert self.hand[tile + 1] > 0
-            self.hand[tile - 1] -= 1
-            self.hand[tile + 1] -= 1
+            self.sub(tile - 1)
+            self.sub(tile + 1)
         if chi == CHI_L:
             assert tile % 9 < 7
-            assert self.hand[tile + 1] > 0
-            assert self.hand[tile + 2] > 0
-            self.hand[tile + 1] -= 1
-            self.hand[tile + 2] -= 1
+            self.sub(tile + 1)
+            self.sub(tile + 2)
 
 
 # Event/Action
@@ -134,29 +125,32 @@ TSUMO = 40
 
 
 class MiniMahjong:
-    def __init__(self):
-        self.deck = Deck()
+    def __init__(self, key: jax.random.PRNGKey):
+        self.deck = Deck(key)
         self.hand = [Hand() for _ in range(4)]
         for i in range(4):
-            for j in range(13):
+            for _ in range(13):
                 self.hand[i].add(self.deck.draw())
 
         self.turn = 0
         # 手牌が3n+2枚のplayer.
         # 存在しなければ直前にdiscardしたplayer.
 
-        self.target = None
+        self.target = -1
         # 直前に捨てられた牌.
         # TODO: 加槓も対象にする.
+
+        self.is_terminal = False
 
         self._draw()
 
     def legal_actions(self) -> Dict[int, List[int]]:
-        if self.hand[self.turn].size() % 3 == 2:
+        assert not self.is_terminal
+
+        if self.target == -1:
             # 手牌が3n+2
             return {
-                self.turn: self.hand[self.turn].to_indices()
-                + self._legal_tsumo()
+                self.turn: self.hand[self.turn].unique() + self._legal_tsumo()
             }
             # TODO: 暗/加槓の追加
             # TODO: 喰い変えNGによる捨て牌制限
@@ -181,14 +175,20 @@ class MiniMahjong:
 
         return ret
 
-    def step(self, actions: Dict[int, int]) -> Tuple[np.ndarray, bool]:
+    def step(self, actions: Dict[int, int]) -> Tuple[jnp.ndarray, bool]:
+        assert not self.is_terminal
+
         assert len(actions) > 0
 
         legal_actions = self.legal_actions()
         assert len(legal_actions) > 0
 
         for player in actions:
-            assert actions[player] in legal_actions[player]
+            # assert actions[player] in legal_actions[player]
+            if actions[player] not in legal_actions[player]:
+                print(actions[player])
+                print(legal_actions[player])
+                assert False
 
         # action(int) の小さいものが優先される.
         player, action = min(
@@ -197,7 +197,7 @@ class MiniMahjong:
         )
         return self._step(player, action)
 
-    def _step(self, player: int, action: int) -> Tuple[np.ndarray, bool]:
+    def _step(self, player: int, action: int) -> Tuple[jnp.ndarray, bool]:
         if action < 34:
             # discard
             assert player == self.turn
@@ -205,7 +205,7 @@ class MiniMahjong:
             # 割り込みがなければ次の人のdrawまで進む
             if len(self.legal_actions()) == 0:
                 return self._draw()
-            return np.zeros(4), False
+            return jnp.zeros(4), False
 
         elif action == 34:
             # ron
@@ -214,12 +214,12 @@ class MiniMahjong:
         elif action == 35:
             # pon
             self._pon(player)
-            return np.zeros(4), False
+            return jnp.zeros(4), False
 
         elif action < 39:
             # chi
             self._chi(player, action)
-            return np.zeros(4), False
+            return jnp.zeros(4), False
 
         elif action == 39:
             # pass
@@ -231,82 +231,90 @@ class MiniMahjong:
             assert player == self.turn
             return self._tsumo(), True
 
-    def _ryukyoku(self) -> np.ndarray:
+        assert False
+
+    def _ryukyoku(self) -> jnp.ndarray:
         print("流局")
-        is_tenpai = np.array(
+        is_tenpai = jnp.array(
             [shanten(self.hand[i].hand) == 0 for i in range(4)]
         )
-        is_not_tenpai = np.array(
+        is_not_tenpai = jnp.array(
             [shanten(self.hand[i].hand) > 0 for i in range(4)]
         )
 
-        tens = np.zeros(4)
+        tens = jnp.zeros(4)
 
         # 点棒移動
         if sum(is_tenpai) == 1:
-            tens[is_tenpai] += 3000
-            tens[is_not_tenpai] -= 1000
+            tens = tens.at[is_tenpai].set(3000)
+            tens = tens.at[is_not_tenpai].set(-1000)
 
         if sum(is_tenpai) == 2:
-            tens[is_tenpai] += 1500
-            tens[is_not_tenpai] -= 1500
+            tens = tens.at[is_tenpai].set(1500)
+            tens = tens.at[is_not_tenpai].set(-1500)
 
         if sum(is_tenpai) == 3:
-            tens[is_tenpai] += 1000
-            tens[is_not_tenpai] -= 3000
+            tens = tens.at[is_tenpai].set(1000)
+            tens = tens.at[is_not_tenpai].set(-3000)
+
+        self.is_terminal = True
 
         return tens
 
-    def _draw(self) -> Tuple[np.ndarray, bool]:
-        self.target = None
+    def _draw(self) -> Tuple[jnp.ndarray, bool]:
+        self.target = -1
         if self.deck.is_empty():
-            return [self._ryukyoku(), True]
+            return self._ryukyoku(), True
         else:
             self.hand[self.turn].add(self.deck.draw())
-            return [np.zeros(4), False]
+            return jnp.zeros(4), False
 
     def _discard(self, tile: int):
         self.hand[self.turn].sub(tile)
         self.target = tile
 
-    def _tsumo(self) -> np.ndarray:
+    def _tsumo(self) -> jnp.ndarray:
         print("自摸")
         print("self.hand[self.turn].hand:", self.hand[self.turn].hand)
 
-        tens = np.zeros(4)
-        # 一律30符1翻
+        tens = jnp.zeros(4)
+        # 一律満貫
         if player == 0:
-            tens = np.full(4, -500)
-            tens[self.turn] = 1500
+            tens = jnp.full(4, -4000)
+            tens = tens.at[self.turn].set(12000)
         else:
-            tens = np.full(4, -300)
-            tens[0] = -500
-            tens[self.turn] = 1100
+            tens = jnp.full(4, -2000)
+            tens = tens.at[0].set(-4000)
+            tens = tens.at[self.turn].set(8000)
+
+        self.is_terminal = True
 
         return tens
 
-    def _ron(self, player: int) -> np.ndarray:
+    def _ron(self, player: int) -> jnp.ndarray:
         print("ロン")
         print("self.hand[player].hand:", self.hand[player].hand)
         print("self.target:", self.target)
 
-        # 一律30符1翻
-        ten = 1500 if player == 0 else 1000
+        # 一律満貫
+        ten = 12000 if player == 0 else 8000
 
-        tens = np.zeros(4)
-        tens[self.turn] = -ten
-        tens[player] = ten
+        tens = jnp.zeros(4)
+        tens = tens.at[self.turn].set(-ten)
+        tens = tens.at[player].set(ten)
+
+        self.is_terminal = True
 
         return tens
 
     def _pon(self, player: int):
         self.hand[player].pon(self.target)
-        self.target = None
+        self.target = -1
         self.turn = player
 
     def _chi(self, player: int, chi: int):
         self.hand[player].chi(self.target, chi)
-        self.target = None
+        self.target = -1
         self.turn = player
 
     def _legal_tsumo(self):
@@ -315,27 +323,27 @@ class MiniMahjong:
         return []
 
     def _legal_ron(self, player: int):
-        if self.target is None:
+        if self.target == -1:
             return []
         if self.hand[player].can_ron(self.target):
             return [RON]
         return []
 
     def _legal_pon(self, player: int):
-        if self.target is None:
+        if self.target == -1:
             return []
         if self.hand[player].can_pon(self.target):
             return [PON]
         return []
 
     def _legal_chis(self, player: int):
-        if self.target is None:
+        if self.target == -1:
             return []
         return self.hand[player].legal_chis(self.target)
 
     def observation(self, player: int):
         # TODO: 自分のhand以外の情報
-        return {"hand": self.hand[player].hand, "target": self.target}
+        return {"hand": self.hand[player], "target": self.target}
 
 
 class BasicAgent:
@@ -345,70 +353,70 @@ class BasicAgent:
         if RON in actions:
             return RON
 
-        if np.sum(obs["hand"]) % 3 == 2:
+        if obs["hand"].size() % 3 == 2:
             # discard
             # shanten数が小さくなるように選択
             min_shanten = 9999
             discard = -1
             for tile in actions:
-                obs["hand"][tile] -= 1
-                s = shanten(obs["hand"])
+                obs["hand"].sub(tile)
+                s = shanten(obs["hand"].hand)
                 if s < min_shanten:
                     s = min_shanten
                     discard = tile
-                obs["hand"][tile] += 1
+                obs["hand"].add(tile)
             return discard
 
         target = obs["target"]
 
         if PON in actions:
-            obs["hand"][target] -= 2
-            s = shanten(obs["hand"])
-            obs["hand"][target] += 2
-            if s < shanten(obs["hand"]):
+            obs["hand"].sub(target, 2)
+            s = shanten(obs["hand"].hand)
+            obs["hand"].add(target, 2)
+            if s < shanten(obs["hand"].hand):
                 return PON
 
         if CHI_R in actions:
-            obs["hand"][target - 2] -= 1
-            obs["hand"][target - 1] -= 1
-            s = shanten(obs["hand"])
-            obs["hand"][target - 2] += 1
-            obs["hand"][target - 1] += 1
-            if s < shanten(obs["hand"]):
+            obs["hand"].sub(target - 2)
+            obs["hand"].sub(target - 1)
+            s = shanten(obs["hand"].hand)
+            obs["hand"].add(target - 2)
+            obs["hand"].add(target - 1)
+            if s < shanten(obs["hand"].hand):
                 return CHI_R
 
         if CHI_M in actions:
-            obs["hand"][target - 1] -= 1
-            obs["hand"][target + 1] -= 1
-            s = shanten(obs["hand"])
-            obs["hand"][target - 1] += 1
-            obs["hand"][target + 1] += 1
-            if s < shanten(obs["hand"]):
+            obs["hand"].sub(target - 1)
+            obs["hand"].sub(target + 1)
+            s = shanten(obs["hand"].hand)
+            obs["hand"].add(target - 1)
+            obs["hand"].add(target + 1)
+            if s < shanten(obs["hand"].hand):
                 return CHI_M
 
         if CHI_L in actions:
-            obs["hand"][target + 1] -= 1
-            obs["hand"][target + 2] -= 1
-            s = shanten(obs["hand"])
-            obs["hand"][target + 1] += 1
-            obs["hand"][target + 2] += 1
-            if s < shanten(obs["hand"]):
+            obs["hand"].sub(target + 1)
+            obs["hand"].sub(target + 2)
+            s = shanten(obs["hand"].hand)
+            obs["hand"].add(target + 1)
+            obs["hand"].add(target + 2)
+            if s < shanten(obs["hand"].hand):
                 return CHI_L
 
         return PASS
 
 
 if __name__ == "__main__":
-    env = MiniMahjong()
     agent = BasicAgent()
 
-    while True:
-        selected = {}
-        for player, actions in env.legal_actions().items():
-            selected[player] = agent.act(actions, env.observation(player))
+    for i in range(5):
+        env = MiniMahjong(jax.random.PRNGKey(seed=i))
+        while True:
+            selected = {}
+            for player, actions in env.legal_actions().items():
+                selected[player] = agent.act(actions, env.observation(player))
 
-        rewards, done = env.step(selected)
-        if done:
-            print('rewards:', rewards)
-            break
-
+            rewards, done = env.step(selected)
+            if done:
+                print("rewards:", rewards)
+                break
