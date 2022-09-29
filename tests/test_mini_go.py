d62dc82d52cc083d459001a30b81acf41d5c0746
OkanoShinri <n.salt2000@gmail.com>
2022-09-30 04:00:49 +0900

    update test

--- tests/test_mini_go.py
+++ tests/test_mini_go.py
@@ -3,20 +3,22 @@ import jax.numpy as jnp
 
 from pgx.mini_go import get_board, init, legal_actions, step
 
+BOARD_SIZE = 5
+
 
 def test_end_by_pass():
-    state = init()
+    state = init(BOARD_SIZE)
 
-    state, _, done = step(state=state, action=-1)
+    state, _, done = step(state=state, action=-1, size=BOARD_SIZE)
     assert state.passed[0]
     assert not done
-    state, _, done = step(state=state, action=0)
+    state, _, done = step(state=state, action=0, size=BOARD_SIZE)
     assert not state.passed[0]
     assert not done
-    state, _, done = step(state=state, action=-1)
+    state, _, done = step(state=state, action=-1, size=BOARD_SIZE)
     assert state.passed[0]
     assert not done
-    state, _, done = step(state=state, action=-1)
+    state, _, done = step(state=state, action=-1, size=BOARD_SIZE)
     assert state.passed[0]
     assert done
 
@@ -25,29 +27,29 @@ def test_step():
     """
     https://www.cosumi.net/replay/?b=You&w=COSUMI&k=0&r=0&bs=5&gr=ccbccdcbdbbadabdbecaacabecaddeaettceedbetttt
     """
-    state = init()
-    state, _, _ = step(state=state, action=12)  # BLACK
-    state, _, _ = step(state=state, action=11)  # WHITE
-    state, _, _ = step(state=state, action=17)
-    state, _, _ = step(state=state, action=7)
-    state, _, _ = step(state=state, action=8)
-    state, _, _ = step(state=state, action=1)
-    state, _, _ = step(state=state, action=3)
-    state, _, _ = step(state=state, action=16)
-    state, _, _ = step(state=state, action=21)
-    state, _, _ = step(state=state, action=2)
-    state, _, _ = step(state=state, action=10)
-    state, _, _ = step(state=state, action=5)
-    state, _, _ = step(state=state, action=14)
-    state, _, _ = step(state=state, action=15)
-    state, _, _ = step(state=state, action=23)
-    state, _, _ = step(state=state, action=20)
-    state, _, _ = step(state=state, action=-1)  # pass
-    state, _, _ = step(state=state, action=22)
-    state, _, _ = step(state=state, action=19)
-    state, _, _ = step(state=state, action=21)
-    state, _, _ = step(state=state, action=-1)  # pass
-    state, r, done = step(state=state, action=-1)  # pass
+    state = init(size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=12, size=BOARD_SIZE)  # BLACK
+    state, _, _ = step(state=state, action=11, size=BOARD_SIZE)  # WHITE
+    state, _, _ = step(state=state, action=17, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=7, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=8, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=1, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=3, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=16, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=21, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=2, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=10, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=5, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=14, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=15, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=23, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=20, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=-1, size=BOARD_SIZE)  # pass
+    state, _, _ = step(state=state, action=22, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=19, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=21, size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=-1, size=BOARD_SIZE)  # pass
+    state, r, done = step(state=state, action=-1, size=BOARD_SIZE)  # pass
 
     expected_board: jnp.ndarray = jnp.array(
         [
@@ -72,15 +74,15 @@ def test_step():
 
 
 def test_kou():
-    state = init()
-    state, _, _ = step(state=state, action=2)  # BLACK
-    state, _, _ = step(state=state, action=17)  # WHITE
-    state, _, _ = step(state=state, action=6)  # BLACK
-    state, _, _ = step(state=state, action=13)  # WHITE
-    state, _, _ = step(state=state, action=8)  # BLACK
-    state, _, _ = step(state=state, action=11)  # WHITE
-    state, _, _ = step(state=state, action=12)  # BLACK
-    state, _, _ = step(state=state, action=7)  # WHITE
+    state = init(size=BOARD_SIZE)
+    state, _, _ = step(state=state, action=2, size=BOARD_SIZE)  # BLACK
+    state, _, _ = step(state=state, action=17, size=BOARD_SIZE)  # WHITE
+    state, _, _ = step(state=state, action=6, size=BOARD_SIZE)  # BLACK
+    state, _, _ = step(state=state, action=13, size=BOARD_SIZE)  # WHITE
+    state, _, _ = step(state=state, action=8, size=BOARD_SIZE)  # BLACK
+    state, _, _ = step(state=state, action=11, size=BOARD_SIZE)  # WHITE
+    state, _, _ = step(state=state, action=12, size=BOARD_SIZE)  # BLACK
+    state, _, _ = step(state=state, action=7, size=BOARD_SIZE)  # WHITE
 
     """
     ===========
@@ -98,12 +100,12 @@ def test_kou():
     """
     assert state.kou[0] == 12
 
-    _, r, done = step(state=state, action=12)  # BLACK
+    _, r, done = step(state=state, action=12, size=BOARD_SIZE)  # BLACK
     # ルール違反により黒の負け
     assert done
     assert (r == jnp.array([-1, 1])).all()
 
-    state, _, done = step(state=state, action=0)  # BLACK
+    state, _, done = step(state=state, action=0, size=BOARD_SIZE)  # BLACK
     # 回避した場合
     assert not done
     assert state.kou[0] == -1
@@ -112,10 +114,10 @@ def test_kou():
 def test_random_play():
     import numpy as np
 
-    state = init()
+    state = init(size=BOARD_SIZE)
     done = False
     while not done:
-        actions = np.where(legal_actions(state))
+        actions = np.where(legal_actions(state, BOARD_SIZE))
         if len(actions[0]) == 0:
             a = -1
         else:
@@ -123,7 +125,7 @@ def test_random_play():
             key, subkey = jax.random.split(key)
             a = jax.random.choice(subkey, actions[0])
 
-        state, _, done = step(state=state, action=a)
+        state, _, done = step(state=state, action=a, size=BOARD_SIZE)
 
         if state.turn[0] > 100:
             break
