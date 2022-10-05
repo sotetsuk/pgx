ab338a6568e38923a7354966d3e8a16243c51a73
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2022-10-05 20:19:55 +0900

    apply format

--- pgx/minatar/breakout.py
+++ pgx/minatar/breakout.py
@@ -25,18 +25,21 @@ THREE = jnp.array(3, dtype=jnp.int8)
 FOUR = jnp.array(4, dtype=jnp.int8)
 NINE = jnp.array(9, dtype=jnp.int8)
 
+
 @struct.dataclass
 class MinAtarBreakoutState:
     ball_y: jnp.ndarray = THREE
-    ball_x:jnp.ndarray  = ZERO
-    ball_dir:jnp.ndarray  = TWO
+    ball_x: jnp.ndarray = ZERO
+    ball_dir: jnp.ndarray = TWO
     pos: jnp.ndarray = FOUR
-    brick_map: jnp.ndarray = jnp.zeros((10, 10), dtype=jnp.bool_).at[1:4, :].set(True)
-    strike:jnp.ndarray  = jnp.array(False, dtype=jnp.bool_)
-    last_x:jnp.ndarray  = ZERO
-    last_y:jnp.ndarray  = THREE
-    terminal:jnp.ndarray  = jnp.array(False, dtype=jnp.bool_)
-    last_action:jnp.ndarray  = ZERO
+    brick_map: jnp.ndarray = (
+        jnp.zeros((10, 10), dtype=jnp.bool_).at[1:4, :].set(True)
+    )
+    strike: jnp.ndarray = jnp.array(False, dtype=jnp.bool_)
+    last_x: jnp.ndarray = ZERO
+    last_y: jnp.ndarray = THREE
+    terminal: jnp.ndarray = jnp.array(False, dtype=jnp.bool_)
+    last_action: jnp.ndarray = ZERO
 
 
 @jax.jit
@@ -71,64 +74,64 @@ def _step_det(
     state: MinAtarBreakoutState, action: jnp.ndarray
 ) -> Tuple[MinAtarBreakoutState, int, bool]:
     r = 0
-    if (self.terminal):
+    if self.terminal:
         return r, self.terminal
 
     a = self.action_map[a]
 
     # Resolve player action
-    if (a == 'l'):
+    if a == "l":
         self.pos = max(0, self.pos - 1)
-    elif (a == 'r'):
+    elif a == "r":
         self.pos = min(9, self.pos + 1)
 
     # Update ball position
     self.last_x = self.ball_x
     self.last_y = self.ball_y
-    if (self.ball_dir == 0):
+    if self.ball_dir == 0:
         new_x = self.ball_x - 1
         new_y = self.ball_y - 1
-    elif (self.ball_dir == 1):
+    elif self.ball_dir == 1:
         new_x = self.ball_x + 1
         new_y = self.ball_y - 1
-    elif (self.ball_dir == 2):
+    elif self.ball_dir == 2:
         new_x = self.ball_x + 1
         new_y = self.ball_y + 1
-    elif (self.ball_dir == 3):
+    elif self.ball_dir == 3:
         new_x = self.ball_x - 1
         new_y = self.ball_y + 1
 
     strike_toggle = False
-    if (new_x < 0 or new_x > 9):
-        if (new_x < 0):
+    if new_x < 0 or new_x > 9:
+        if new_x < 0:
             new_x = 0
-        if (new_x > 9):
+        if new_x > 9:
             new_x = 9
         self.ball_dir = [1, 0, 3, 2][self.ball_dir]
-    if (new_y < 0):
+    if new_y < 0:
         new_y = 0
         self.ball_dir = [3, 2, 1, 0][self.ball_dir]
-    elif (self.brick_map[new_y, new_x] == 1):
+    elif self.brick_map[new_y, new_x] == 1:
         strike_toggle = True
-        if (not self.strike):
+        if not self.strike:
             r += 1
             self.strike = True
             self.brick_map[new_y, new_x] = 0
             new_y = self.last_y
             self.ball_dir = [3, 2, 1, 0][self.ball_dir]
-    elif (new_y == 9):
-        if (np.count_nonzero(self.brick_map) == 0):
+    elif new_y == 9:
+        if np.count_nonzero(self.brick_map) == 0:
             self.brick_map[1:4, :] = 1
-        if (self.ball_x == self.pos):
+        if self.ball_x == self.pos:
             self.ball_dir = [3, 2, 1, 0][self.ball_dir]
             new_y = self.last_y
-        elif (new_x == self.pos):
+        elif new_x == self.pos:
             self.ball_dir = [2, 3, 0, 1][self.ball_dir]
             new_y = self.last_y
         else:
             self.terminal = True
 
-    if (not strike_toggle):
+    if not strike_toggle:
         self.strike = False
 
     self.ball_x = new_x
@@ -144,9 +147,7 @@ def _reset_det(ball_start: jnp.ndarray) -> MinAtarBreakoutState:
     )
     last_x = ball_x
     return MinAtarBreakoutState(
-        ball_x=ball_x,
-        ball_dir=ball_dir,
-        last_x=last_x
+        ball_x=ball_x, ball_dir=ball_dir, last_x=last_x
     )  # type: ignore
 
 
