fb9efd33c90a8d0e762e8dd903f7ffa16a4fafe7
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2022-10-05 23:56:57 +0900

    fix

--- pgx/minatar/asterix.py
+++ pgx/minatar/asterix.py
@@ -447,33 +447,20 @@ def _reset_det() -> MinAtarAsterixState:
 
 @jax.jit
 def _to_obs(state: MinAtarAsterixState) -> jnp.ndarray:
-    obs = jnp.zeros((10, 10, 4), dtype=bool)
+    obs = jnp.zeros((10, 10, 4), dtype=jnp.bool_)
     obs = obs.at[state.player_y, state.player_x, 0].set(True)
-    # state[self.player_y, self.player_x, self.channels["player"]] = 1
-    for i in range(8):
-        obs = jax.lax.cond(
+    obs = jax.lax.fori_loop(
+        0, 8, lambda i, _obs: jax.lax.cond(
             state.entities[i, 0] != INF,
-            __to_obs,
-            lambda _obs, _state, _i: _obs,
-            obs,
-            state,
-            i,
-        )
-        # if state.entities[i, 0] != INF:
-        #     obs = __to_obs(obs, state, i)
-
-    # for x in self.entities:
-    #     if x is not None:
-    #         c = self.channels["gold"] if x[3] else self.channels["enemy"]
-    #         state[x[1], x[0], c] = 1
-    #         back_x = x[0] - 1 if x[2] else x[0] + 1
-    #         if back_x >= 0 and back_x <= 9:
-    #             state[x[1], back_x, self.channels["trail"]] = 1
+            lambda: _update_obs_by_entity(_obs, state, i),
+            lambda: _obs,
+        ), obs
+    )
     return obs
 
 
 @jax.jit
-def __to_obs(obs, state, i):
+def _update_obs_by_entity(obs, state, i):
     c = jax.lax.cond(state.entities[i, 3], lambda: 3, lambda: 1)
     obs = obs.at[state.entities[i, 1], state.entities[i, 0], c].set(True)
     back_x = jax.lax.cond(
