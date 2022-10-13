b9ba73d03eedf405587d4c558f2ede6144fd7233
keigo habara <k.habara.aa@gmail.com>
2022-10-13 14:23:05 +0900

    time convertion

--- pgx/mahjong/full_mahjong_agent.py
+++ pgx/mahjong/full_mahjong_agent.py
@@ -81,6 +81,7 @@ def act(
 
 if __name__ == "__main__":
     legal_action_time = 0.0
+    obs_time = 0.0
     select_time = 0.0
     step_time = 0.0
 
@@ -91,21 +92,24 @@ if __name__ == "__main__":
         done = False
         while not done:
             tmp = time.time()
-            legal_actions = state.legal_actions()
+            legal_actions = np.array(state.legal_actions())
             if i != 0:
                 legal_action_time += time.time() - tmp
 
             tmp = time.time()
+            obs = [to_np(state.observe(i)) for i in range(4)]
+            if i != 0:
+                obs_time += time.time() - tmp
+
+            tmp = time.time()
             selected = np.array(
-                [
-                    act(i, np.array(legal_actions[i]), to_np(state.observe(i)))
-                    for i in range(4)
-                ]
+                [act(i, legal_actions[i], obs[i]) for i in range(4)]
             )
             if i != 0:
                 select_time += time.time() - tmp
 
             tmp = time.time()
+            # state, reward, done = State.step(state, selected)  # type: ignore
             state, reward, done = step(state, selected)  # type: ignore
             if i != 0:
                 step_time += time.time() - tmp
@@ -122,17 +126,18 @@ if __name__ == "__main__":
                 "      ",
                 list(map(Meld.to_str, state.melds[i][: state.n_meld[i]])),
             )
-        print("riichi:", state.riichi)
-        print("is_menzen:", state.is_menzen)
-        print("doras:", state.deck.doras)
-        print("end:", state.deck.end)
+        print(f"{state.riichi=}")
+        print(f"{state.is_menzen=}")
+        print(f"{state.deck.doras=}")
+        print(f"{state.deck.end=}")
         if state.target != -1:
             print("target:", Tile.to_str(state.target))
         if state.last_draw != -1:
             print("last_draw:", Tile.to_str(state.last_draw))
-        print("reward:", reward)
+        print(f"{reward=}")
         print("-" * 30)
 
-    print("legal_action:", legal_action_time)
-    print("select:", select_time)
-    print("step:", step_time)
+    print(f"{legal_action_time=}")
+    print(f"{obs_time=}")
+    print(f"{select_time=}")
+    print(f"{step_time=}")
