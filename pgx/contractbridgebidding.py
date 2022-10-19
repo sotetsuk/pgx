b2d262d61aa72ec0d08a059d75a58658224e932e
Kita-Haruka <hrkkt1213@gmail.com>
2022-10-19 17:38:37 +0900

    fix legal_actions

--- pgx/contractbridgebidding.py
+++ pgx/contractbridgebidding.py
@@ -34,6 +34,8 @@ class ContractBridgeBiddingState:
     call_xx: np.ndarray = np.zeros(1, dtype=np.bool8)
     # legal_actions プレイヤーの可能なbidの一覧
     legal_actions: np.ndarray = np.ones(38, dtype=np.bool8)
+    # 最初はdable, redoubleできない
+    legal_actions[-2:] = 0
     # first_denominaton_NS NSチームにおいて、各denominationをどのプレイヤー
     # が最初にbidしたかを表す
     first_denomination_NS: np.ndarray = np.zeros(5, dtype=np.int8)
