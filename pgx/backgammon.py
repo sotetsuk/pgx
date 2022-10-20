97f6bfe38821c413952392df6888262a5f0f3cba
nissymori <kyoudai.630@gmail.com>
2022-10-20 14:35:13 +0900

    nissymori/feat/backgammon_state

--- pgx/backgammon.py
+++ pgx/backgammon.py
@@ -7,9 +7,23 @@ import numpy as np
 class BackgammonState:
     # 白なら0黒なら1
     turn: int = 0
-    # 各point(24)にあるcheckerの数, 白と黒で二次元.
+
+    # 各point(24)にあるcheckerの数 [0]: 白, [1]: 黒
     borad: np.ndarray = np.zeros((2, 24), dtype=int)
+
     # barの上にあるcheckerの数
     bar: np.ndarray = np.zeros(2, dtype=int)
+
     # offにあるcheckerの数
     off: np.ndarray = np.zeros(2, dtype=int)
+
+    # ゲームの報酬. doublingが起こると変わる.
+    game_reward: int = 0
+
+    # doubligをしたかどうか.
+    has_doubled: np.ndarray = np.zeros(2, dtype=bool)
+
+    # bear offできるかどうか.
+    is_bearable: np.ndarray = np.zeros(2, dtype=bool)
+
+    legal_actions: np.ndarray = np.zeros()
