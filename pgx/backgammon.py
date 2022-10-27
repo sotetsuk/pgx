e8cce69de0ad4af7afdd1643c4513fbdc286bebc
nissymori <kyoudai.630@gmail.com>
2022-10-27 20:06:45 +0900

    pass test

--- pgx/backgammon.py
+++ pgx/backgammon.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from random import randint
-from typing import List
+from typing import Tuple
 
 import numpy as np
 
@@ -10,12 +10,6 @@ class BackgammonState:
 
     # 各point(24) bar(2) off(2)にあるcheckerの数 負の値は白, 正の値は黒
     board: np.ndarray = np.zeros(28, dtype=np.int8)
-
-    # ゲームの報酬. doublingが起こると変わる.
-    game_reward: np.ndarray = np.zeros(0, dtype=np.int8)
-
-    # doubligをしたかどうか.
-    has_doubled: np.ndarray = np.zeros(2, dtype=np.int8)
 
     # bear offできるかどうか.
     is_bearable: np.ndarray = np.zeros(2, dtype=np.int8)
@@ -34,8 +28,6 @@ class BackgammonState:
 
 def init() -> BackgammonState:
     board: np.ndarray = _make_init_board()
-    game_reward: np.ndarray = np.array([1])
-    has_doubled: np.ndarray = np.zeros(2, dtype=np.int8)
     is_bearable: np.ndarray = np.zeros(2, dtype=np.int8)
     dice: np.ndarray = _roll_init_dice()
     turn: np.ndarray = _init_turn(dice)
@@ -44,8 +36,6 @@ def init() -> BackgammonState:
     )
     state = BackgammonState(
         board=board,
-        game_reward=game_reward,
-        has_doubled=has_doubled,
         is_bearable=is_bearable,
         dice=dice,
         turn=turn,
@@ -76,6 +66,10 @@ def _roll_init_dice() -> np.ndarray:
 
 
 def _init_turn(dice: np.ndarray) -> np.ndarray:
+    """
+    ゲーム開始時のターン決め.
+    サイコロの目が大きい方が手番.
+    """
     diff = dice[1] - dice[0]
     turn = np.sign(diff)  # diff > 0 : turn=1, diff < 0: turn=-1
     return np.array([turn], dtype=np.int8)
@@ -92,29 +86,46 @@ def _home_board(turn: int) -> np.ndarray:
 
 
 def _off(board: np.ndarray, turn: np.ndarray) -> np.ndarray:
+    """
+    白: 26, 黒: 27
+    """
     t: int = turn[0]
     off: int = board[26 + np.clip(t, a_min=0, a_max=1)]
     return np.array([off], dtype=np.int8)
 
 
 def _bar(board: np.ndarray, turn: np.ndarray) -> np.ndarray:
+    """
+    白: 24, 黒 25
+    """
     t: int = turn[0]
     bar: int = board[24 + np.clip(t, a_min=0, a_max=1)]
     return np.array([bar], dtype=np.int8)
 
 
-def _can_bear_off(board: np.ndarray, turn: np.ndarray) -> bool:
+def _rear_distance(board: np.ndarray, turn: np.ndarray) -> int:
+    """
+    board上にあるcheckerについて, goal地点とcheckerの距離の最大値
+    """
+    t: int = turn[0]
+    b = board[:24]
+    exists: np.ndarray = np.where((b * t > 0))[0]
+    if turn == 1:
+        return int(np.max(exists)) + 1
+    else:
+        return 24 - int(np.min(exists))
+
+
+def _is_all_on_homeboad(board: np.ndarray, turn: np.ndarray) -> bool:
     """
     全てのcheckerがhome boardにあれば bear offできる.
     """
     t: int = turn[0]
     home_board: np.ndarray = _home_board(t)
     on_home_boad: int = np.array(
-        [board[i] * t for i in home_board if board[i] * t >= 0]
+        [board[i] * t for i in home_board if board[i] * t >= 0], dtype=np.int8
     ).sum()
-    print(on_home_boad)
     off: int = _off(board, turn)[0] * t
-    print(off)
     return (15 - off) == on_home_boad
 
 
@@ -126,3 +137,74 @@ def _is_open(board: np.ndarray, turn: np.ndarray, point: int) -> bool:
     t: int = turn[0]
     checkers: int = board[point]
     return t * checkers >= -1  # 黒と白のcheckerは異符号
+
+
+def _exists(board: np.ndarray, turn: np.ndarray, point: int) -> bool:
+    """
+    指定pointに手番のchckerが存在するか.
+    """
+    t: int = turn[0]
+    checkers: int = board[point]
+    return t * checkers >= 1
+
+
+def _calc_src(src: int, turn: np.ndarray) -> int:
+    """
+    boardとindexを合わせる.
+    """
+    t = turn[0]
+    if src == 1:
+        return 24 + np.clip(t, a_min=0, a_max=1)
+    else:
+        return src - 2
+
+
+def _calc_tgt(idx: int, turn: np.ndarray, die) -> int:
+    """
+    srcがbarだった際の対応
+    """
+    t = turn[0]
+    if idx >= 24:
+        return np.clip(24 * t, a_min=-1, a_max=24) + die * -turn[0]
+    else:
+        return idx + die * -turn[0]
+
+
+def _decompose_micro_action(micro_action: int, turn: np.ndarray) -> Tuple:
+    """
+    micro_action(int)をsource, die, tagetに分解する.
+    """
+    src = _calc_src(micro_action // 6, turn)
+    die = micro_action % 6 + 1  # 0~5 -> 1~6
+    tgt = _calc_tgt(src, turn, die)
+    return src, die, tgt
+
+
+def _is_no_op(micro_action):
+    """
+    no operationかどうか判定する.
+    """
+    return micro_action // 6 == 0
+
+
+def _is_micro_action_legal(board: np.ndarray, turn, micro_action: int) -> bool:
+    """
+    micro actionの合法判定
+    micro_action = src * 6 + die
+    src = [no op., from bar, 0, .., 23]
+    """
+    src, die, tgt = _decompose_micro_action(micro_action, turn)
+    if src >= 24:  # barからpointへの移動
+        return (_exists(board, turn, src)) and (_is_open(board, turn, tgt))
+    elif 0 <= tgt <= 23:  # pointからpointへの移動
+        return (
+            (_exists(board, turn, src))
+            and (_is_open(board, turn, tgt))
+            and (_bar(board, turn)[0] == 0)
+        )
+    else:  # bear off
+        return (
+            _exists(board, turn, src)
+            and _is_all_on_homeboad(board, turn)
+            and (_rear_distance(board, turn) <= die)
+        )
--- tests/test_backgammon.py
+++ tests/test_backgammon.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-from pgx.backgammon import _can_bear_off, _home_board, _is_open, init
+from pgx.backgammon import (
+    _calc_src,
+    _is_all_on_homeboad,
+    _is_micro_action_legal,
+    _is_open,
+    _rear_distance,
+    init,
+)
 
 
 def make_test_boad():
@@ -11,10 +18,10 @@ def make_test_boad():
     board[21] = -3
     board[26] = -2
     # 黒
-    board[8] = 2
-    board[10] = 5
+    board[3] = 2
     board[4] = 1
-    board[11] = 3
+    board[10] = 5
+    board[22] = 3
     board[25] = 4
     return board
 
@@ -41,13 +48,55 @@ def test_is_open():
     assert not _is_open(board, turn, 21)
 
 
-def test_can_bear_off():
+def test_is_all_on_home_boad():
     board = make_test_boad()
     # 白
     turn = np.array([-1])
-    print(_can_bear_off(board, turn))
-    assert _can_bear_off(board, turn)
-
+    print(_is_all_on_homeboad(board, turn))
+    assert _is_all_on_homeboad(board, turn)
     # 黒
     turn = np.array([1])
-    assert not _can_bear_off(board, turn)
+    assert not _is_all_on_homeboad(board, turn)
+
+
+def test_rear_distance():
+    board = make_test_boad()
+    turn = np.array([-1])
+    # 白
+    assert _rear_distance(board, turn) == 5
+    # 黒
+    turn = np.array([1])
+    assert _rear_distance(board, turn) == 23
+
+
+def test_calc_src():
+    assert _calc_src(1, np.array([-1])) == 24
+    assert _calc_src(1, np.array([1])) == 25
+    assert _calc_src(2, np.array([1])) == 0
+
+
+def test_is_micro_action_legal():
+    board = make_test_boad()
+    # 白
+    turn = np.array([-1])
+    assert _is_micro_action_legal(board, turn, (19 + 2) * 6 + 1)  # 19->21
+    assert not _is_micro_action_legal(
+        board, turn, (19 + 2) * 6 + 2
+    )  # 19 -> 22
+    assert not _is_micro_action_legal(
+        board, turn, (19 + 2) * 6 + 2
+    )  # 19 -> 22: 22に黒が複数ある.
+    assert not _is_micro_action_legal(
+        board, turn, (22 + 2) * 6 + 2
+    )  # 22 -> 25: 22に白がない
+    assert _is_micro_action_legal(board, turn, (19 + 2) * 6 + 6)  # bear off
+    assert not _is_micro_action_legal(
+        board, turn, (20 + 2) * 6 + 6
+    )  # 後ろにまだ白があるためbear offできない.
+    turn = np.array([1])
+    # 黒
+    assert not _is_micro_action_legal(
+        board, turn, (3 + 2) * 6 + 0
+    )  # 3->2: barにcheckerが残っているので動かせない.
+    assert _is_micro_action_legal(board, turn, (1) * 6 + 0)  # bar -> 23
+    assert not _is_micro_action_legal(board, turn, (1) * 6 + 2)  # bar -> 21
