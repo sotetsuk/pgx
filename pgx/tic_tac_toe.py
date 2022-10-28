1b663316c90b9b713512384440fdc680079e244b
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2022-10-28 16:19:00 +0900

    fix

--- pgx/tic_tac_toe.py
+++ pgx/tic_tac_toe.py
@@ -4,6 +4,8 @@ import jax
 import jax.numpy as jnp
 from flax import struct
 
+FALSE = jnp.bool_(False)
+TRUE = jnp.bool_(True)
 
 @struct.dataclass
 class State:
@@ -32,48 +34,47 @@ def step(
     #     ...
     board = state.board.at[action].set(state.turn)
     won = _win_check(board, state.turn)
-
     rewards = jax.lax.cond(
         won,
         lambda: jnp.int16([-1, -1]).at[state.curr_player].set(1),
         lambda: jnp.zeros(2, jnp.int16),
     )
-
+    terminated = won | jnp.all(board != -1)
     curr_player = (state.curr_player + 1) % 2
     state = State(
         curr_player=curr_player,
         legal_action_mask=board < 0,
-        terminated=jnp.bool_(won),
+        terminated=terminated,
         turn=(state.turn + 1) % 2,
         board=board,
     )  # type: ignore
     return curr_player, state, rewards
 
 
-def _win_check(board, turn) -> bool:
+def _win_check(board, turn) -> jnp.ndarray:
     # board:
     #   0 1 2
     #   3 4 5
     #   6 7 8
-    won = False
+    won = FALSE
     for i in range(0, 9, 3):
         # e.g., [0, 1, 2]
         won = jax.lax.cond(
-            jnp.all(board[i : i + 3] == turn), lambda: True, lambda: won
+            jnp.all(board[i : i + 3] == turn), lambda: TRUE, lambda: won
         )
     for i in range(3):
         # e.g., [0, 3, 6]
         won = jax.lax.cond(
-            jnp.all(board[i:9:3] == turn), lambda: True, lambda: won
+            jnp.all(board[i:9:3] == turn), lambda: TRUE, lambda: won
         )
     won = jax.lax.cond(
         jnp.all(board[jnp.int8([0, 4, 8])] == turn),
-        lambda: True,
+        lambda: TRUE,
         lambda: won,
     )
     won = jax.lax.cond(
         jnp.all(board[jnp.int8([2, 4, 6])] == turn),
-        lambda: True,
+        lambda: TRUE,
         lambda: won,
     )
     return won
