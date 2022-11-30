707d6e9796c3f0f16fdd8417a148b98750045422
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2022-12-01 04:01:46 +0900

    update tests

--- tests/test_suzume_jong.py
+++ tests/test_suzume_jong.py
@@ -31,38 +31,42 @@ def test_is_completed():
 def test_init():
     curr_player, state = init(jax.random.PRNGKey(1))
     _validate(state)
+    print(_to_str(state))
     assert _to_str(state) == """ dora: r
-*[2] 23358g, xxxxxxxxxx 
- [1] 34459 , xxxxxxxxxx 
- [0] 5789r , xxxxxxxxxx 
+*[2] 2*3 3 5 8*g : _ _ _ _ _ _ _ _ _ _  
+ [1] 3 4 4 5*9   : _ _ _ _ _ _ _ _ _ _  
+ [0] 5 7 8 9 r*  : _ _ _ _ _ _ _ _ _ _  
 """
 
 
 def test_step():
     curr_player, state = init(jax.random.PRNGKey(1))
     _validate(state)
+    print(_to_str(state))
     assert _to_str(state) == """ dora: r
-*[2] 23358g, xxxxxxxxxx 
- [1] 34459 , xxxxxxxxxx 
- [0] 5789r , xxxxxxxxxx 
+*[2] 2*3 3 5 8*g : _ _ _ _ _ _ _ _ _ _  
+ [1] 3 4 4 5*9   : _ _ _ _ _ _ _ _ _ _  
+ [0] 5 7 8 9 r*  : _ _ _ _ _ _ _ _ _ _  
 """
+
     curr_player, state, r = step(state, jnp.int8(1))
-    print(state.legal_action_mask)
     assert not state.terminated
     _validate(state)
+    print(_to_str(state))
     assert _to_str(state) == """ dora: r
- [2] 3358g , 2xxxxxxxxx 
-*[1] 344599, xxxxxxxxxx 
- [0] 5789r , xxxxxxxxxx 
+ [2] 3 3 5 8*g   : 2*_ _ _ _ _ _ _ _ _  
+*[1] 3 4 4 5*9*9 : _ _ _ _ _ _ _ _ _ _  
+ [0] 5 7 8 9 r*  : _ _ _ _ _ _ _ _ _ _  
 """
 
 
 def test_random_play():
     results = ""
+    print()
     for seed in range(10):
-        # print("=================================")
-        # print(seed)
-        # print("=================================")
+        print("=================================")
+        print(seed)
+        print("=================================")
         key = jax.random.PRNGKey(seed)
         key, subkey = jax.random.split(key)
         curr_player, state = init(subkey)
@@ -75,48 +79,48 @@ def test_random_play():
             curr_player, state, r = step(state, action)
             _validate(state)
         print(_to_str(state))
-        # print(r)
+        print(r)
         results += _to_str(state)
 
     expected = """[terminated] dora: 5
- [1] 1468g , 7xxxxxxxxx 
- [0] 5899g , 5xxxxxxxxx 
- [2] 23467 , xxxxxxxxxx 
+ [1] 1 4 6 8*g   : 7*_ _ _ _ _ _ _ _ _  
+ [0] 5 8 9*9 g   : 5 _ _ _ _ _ _ _ _ _  
+ [2] 2*3 4 6 7   : _ _ _ _ _ _ _ _ _ _  
 [terminated] dora: 4
- [2] 389gr , 1551812494 
- [0] 4grrr , 862g75927x 
- [1] 36668 , 715723g39x 
+ [2] 3 8*9*g*r*  : 1 5 5 1 8 1*2*4*9 4  
+ [0] 4 g r*r*r*  : 8 6 2 g 7 5 9 2 7*_  
+ [1] 3*6*6 6 8   : 7 1 5*7 2 3 g 3 9 _  
 [terminated] dora: 3
- [2] 124gr , 7rr8xxxxxx 
- [1] 56789 , 1732xxxxxx 
- [0] 168gg , 5954xxxxxx 
+ [2] 1 2 4 g r*  : 7 r*r*8*_ _ _ _ _ _  
+ [1] 5 6*7 8 9   : 1 7*3 2 _ _ _ _ _ _  
+ [0] 1*6 8 g*g   : 5 9*5 4 _ _ _ _ _ _  
 [terminated] dora: 3
- [1] 479rr , 3857xxxxxx 
- [2] 12357 , 82g7xxxxxx 
- [0] 259rr , 4986xxxxxx 
+ [1] 4 7 9 r*r*  : 3*8 5 7 _ _ _ _ _ _  
+ [2] 1 2 3 5 7*  : 8*2 g 7 _ _ _ _ _ _  
+ [0] 2 5 9*r*r*  : 4 9 8 6*_ _ _ _ _ _  
 [terminated] dora: 3
- [1] 379gr , g5979r36g3 
- [0] 11568 , 184g28245x 
- [2] 24789 , r52r67641x 
+ [1] 3*7 9 g*r*  : g 5*9 7 9 r*3 6 g 3  
+ [0] 1*1 5 6 8*  : 1 8 4 g 2 8 2*4*5 _  
+ [2] 2 4 7 8 9*  : r*5 2 r*6 7*6*4 1 _  
 [terminated] dora: 3
- [0] 14568 , 7rxxxxxxxx 
- [2] 46899 , 33xxxxxxxx 
- [1] 12345 , rxxxxxxxxx 
+ [0] 1 4 5 6 8   : 7 r*_ _ _ _ _ _ _ _  
+ [2] 4 6*8 9*9   : 3 3 _ _ _ _ _ _ _ _  
+ [1] 1 2*3 4 5   : r*_ _ _ _ _ _ _ _ _  
 [terminated] dora: 6
- [1] 45588 , 268497gr12 
- [2] 1356r , 24291539rx 
- [0] 179gg , g37346r87x 
+ [1] 4*5*5 8*8   : 2 6 8 4 9 7*g r*1*2  
+ [2] 1 3 5 6 r*  : 2 4 2*9 1 5 3*9*r*_  
+ [0] 1 7 9 g*g   : g 3 7 3 4 6 r*8 7 _  
 [terminated] dora: 5
- [2] 3557g , 92682g1894 
- [1] 67789 , 584497r62x 
- [0] 1233r , 4r136rg1gx 
+ [2] 3 5*5 7*g   : 9*2*6 8 2 g 1*8 9 4  
+ [1] 6*7 7 8*9   : 5 8 4*4 9 7 r*6 2 _  
+ [0] 1 2 3*3 r*  : 4 r*1 3 6 r*g 1 g _  
 [terminated] dora: 3
- [0] 2279g , 1rrr27xxxx 
- [2] 448gg , 79158xxxxx 
- [1] 23489 , 966r1xxxxx 
+ [0] 2*2 7 9 g   : 1 r r*r*2 7 _ _ _ _  
+ [2] 4 4 8 g*g   : 7 9 1 5 8 _ _ _ _ _  
+ [1] 2 3 4*8 9*  : 9 6 6*r*1 _ _ _ _ _  
 [terminated] dora: r
- [2] 126gr , 4xxxxxxxxx 
- [0] 234666, xxxxxxxxxx 
- [1] 11479 , xxxxxxxxxx 
+ [2] 1 2 6 g r*  : 4*_ _ _ _ _ _ _ _ _  
+ [0] 2 3*4 6*6 6 : _ _ _ _ _ _ _ _ _ _  
+ [1] 1*1 4 7 9   : _ _ _ _ _ _ _ _ _ _  
 """
-    # assert results == expected
+    assert results == expected
