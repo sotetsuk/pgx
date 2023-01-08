f899bcf8701ced2c3d6017af2a1993f64130810d
OkanoShinri <n.salt2000@gmail.com>
2023-01-08 21:32:39 +0900

    backgammon

--- pgx/visualizer.py
+++ pgx/visualizer.py
@@ -7,6 +7,7 @@ from typing import Optional, Union
 import numpy as np
 import svgwrite  # type: ignore
 
+from .backgammon import BackgammonState
 from .chess import ChessState
 from .contractbridgebidding import ContractBridgeBiddingState
 from .go import GoState
@@ -171,7 +172,34 @@ class Visualizer:
         return dwg
 
     def _set_config_by_state(self, _state, _color_mode):
-        if isinstance(_state, ChessState):
+        if isinstance(_state, BackgammonState):
+            self.GRID_SIZE = 50
+            self.BOARD_WIDTH = 17
+            self.BOARD_HEIGHT = 14
+            self._make_dwg_group = self._make_backgammon_dwg
+            if (
+                _color_mode is None and self.color_mode == "dark"
+            ) or _color_mode == "dark":
+                self.color_set = VisualizerConfig(
+                    "gray",
+                    "black",
+                    "black",
+                    "dimgray",
+                    "#202020",
+                    "gainsboro",
+                    "",
+                )
+            else:
+                self.color_set = VisualizerConfig(
+                    "white",
+                    "black",
+                    "lightgray",
+                    "white",
+                    "white",
+                    "black",
+                    "",
+                )
+        elif isinstance(_state, ChessState):
             self.GRID_SIZE = 50
             self.BOARD_WIDTH = 8
             self.BOARD_HEIGHT = 8
@@ -320,7 +348,12 @@ class Visualizer:
                 )
 
     def _get_nth_state(self, _states, _i):
-        if isinstance(_states, ChessState):
+        if isinstance(_states, BackgammonState):
+            return BackgammonState(
+                turn=_states.turn[_i],  # type:ignore
+                board=_states.board[_i],
+            )
+        elif isinstance(_states, ChessState):
             return ChessState(
                 turn=_states.turn[_i],  # type:ignore
                 board=_states.board[_i],
@@ -345,17 +378,12 @@ class Visualizer:
                 turn=_states.turn[_i],  # type:ignore
                 board=_states.board[_i],
                 hand=_states.hand[_i],
-                legal_actions_black=_states.legal_actions_black[_i],
-                legal_actions_white=_states.legal_actions_white[_i],
             )
         elif isinstance(_states, SuzumeJongState):
             return SuzumeJongState(
                 curr_player=_states.curr_player[_i],
-                legal_action_mask=_states.legal_action_mask[_i],
-                terminated=_states.terminated[_i],
                 turn=_states.turn[_i],
                 rivers=_states.rivers[_i],
-                last_discard=_states.last_discard[_i],
                 hands=_states.hands[_i],
                 n_red_in_hands=_states.n_red_in_hands[_i],
                 is_red_in_river=_states.is_red_in_river[_i],
@@ -363,7 +391,6 @@ class Visualizer:
                 draw_ix=_states.draw_ix[_i],
                 shuffled_players=_states.shuffled_players[_i],
                 dora=_states.dora[_i],
-                scores=_states.scores[_i],
             )
         elif isinstance(_states, TictactoeState):
             return TictactoeState(
@@ -376,6 +403,203 @@ class Visualizer:
 
         else:
             assert False
+
+    def _make_backgammon_dwg(
+        self,
+        dwg,
+        state: BackgammonState,
+        color_set: VisualizerConfig,
+    ) -> svgwrite.Drawing:
+        BOARD_WIDTH = self.BOARD_WIDTH
+        BOARD_HEIGHT = self.BOARD_HEIGHT
+        GRID_SIZE = self.GRID_SIZE
+        # background
+        dwg.add(
+            dwg.rect(
+                (0, 0),
+                (
+                    (BOARD_WIDTH + 6) * GRID_SIZE,
+                    (BOARD_HEIGHT + 3) * GRID_SIZE,
+                ),
+                fill=color_set.background_color,
+            )
+        )
+
+        # board
+        # grid
+        board_g = dwg.g()
+        board_g.add(
+            dwg.rect(
+                (0, 0),
+                (
+                    13 * GRID_SIZE,
+                    14 * GRID_SIZE,
+                ),
+                stroke=color_set.grid_color,
+                fill=color_set.background_color,
+            )
+        )
+        board_g.add(
+            dwg.rect(
+                (6 * GRID_SIZE, 0),
+                (
+                    1 * GRID_SIZE,
+                    14 * GRID_SIZE,
+                ),
+                stroke=color_set.grid_color,
+                fill=color_set.background_color,
+            )
+        )
+
+        for i in range(24):
+            p1 = (i * GRID_SIZE, 0)
+            p2 = ((i + 1) * GRID_SIZE, 0)
+            p3 = ((i + 0.5) * GRID_SIZE, 6 * GRID_SIZE)
+            if 6 <= i < 12:
+                p1 = ((i + 1) * GRID_SIZE, 0)
+                p2 = ((i + 2) * GRID_SIZE, 0)
+                p3 = ((i + 1.5) * GRID_SIZE, 6 * GRID_SIZE)
+            elif 12 <= i < 18:
+                p1 = ((i - 12) * GRID_SIZE, 14 * GRID_SIZE)
+                p2 = ((i + 1 - 12) * GRID_SIZE, 14 * GRID_SIZE)
+                p3 = ((i + 0.5 - 12) * GRID_SIZE, 8 * GRID_SIZE)
+            elif 18 <= i:
+                p1 = ((i + 1 - 12) * GRID_SIZE, 14 * GRID_SIZE)
+                p2 = ((i + 2 - 12) * GRID_SIZE, 14 * GRID_SIZE)
+                p3 = ((i + 1.5 - 12) * GRID_SIZE, 8 * GRID_SIZE)
+
+            fill_color = (
+                color_set.p1_outline if i % 2 == 0 else color_set.p2_outline
+            )
+
+            board_g.add(
+                dwg.polygon(
+                    points=[p1, p2, p3],
+                    stroke=color_set.grid_color,
+                    fill=fill_color,
+                )
+            )
+
+        # pieces
+        for i, piece in enumerate(state.board[:24]):
+            fill_color = color_set.p2_color
+            if piece < 0:  # 白
+                piece = -piece
+                fill_color = color_set.p1_color
+
+            x = (12 - i) + 0.5
+            y = 13.5
+            diff = -1
+            if 6 <= i < 12:
+                x = (12 - i) - 0.5
+                y = 13.5
+            elif 12 <= i < 18:
+                x = i - 12 + 0.5
+                y = 0.5
+                diff = 1
+            elif 18 <= i:
+                x = i - 12 + 1.5
+                y = 0.5
+                diff = 1
+            for n in range(piece):
+                board_g.add(
+                    dwg.circle(
+                        center=(x * GRID_SIZE, (y + n * diff) * GRID_SIZE),
+                        r=0.5 * GRID_SIZE,
+                        stroke=color_set.grid_color,
+                        fill=fill_color,
+                        # stroke_width=GRID_SIZE,
+                    )
+                )
+
+        # bar
+        for i, piece in enumerate(state.board[24:26]):  # 24:白,25:黒
+            fill_color = color_set.p2_color
+            diff = 1
+            if i == 0:
+                piece = -piece
+                fill_color = color_set.p1_color
+                diff = -1
+            for n in range(piece):
+                board_g.add(
+                    dwg.circle(
+                        center=(
+                            6.5 * GRID_SIZE,
+                            (6 + i * 2 + n * diff) * GRID_SIZE,
+                        ),
+                        r=0.5 * GRID_SIZE,
+                        stroke=color_set.grid_color,
+                        fill=fill_color,
+                        # stroke_width=GRID_SIZE,
+                    )
+                )
+
+        # off
+        board_g.add(
+            dwg.rect(
+                (13 * GRID_SIZE, 12 * GRID_SIZE),
+                (
+                    4 * GRID_SIZE,
+                    2 * GRID_SIZE,
+                ),
+                stroke=color_set.grid_color,
+                fill=color_set.background_color,
+            )
+        )
+        board_g.add(
+            dwg.circle(
+                center=(14 * GRID_SIZE, 13 * GRID_SIZE),
+                r=0.5 * GRID_SIZE,
+                stroke=color_set.grid_color,
+                fill=color_set.p1_color,
+            )
+        )
+        board_g.add(
+            dwg.text(
+                text=f"× {-state.board[26]}",  # 26:白
+                insert=(
+                    14.8 * GRID_SIZE,
+                    13.3 * GRID_SIZE,
+                ),
+                fill=color_set.grid_color,
+                font_size="34px",
+                font_family="serif",
+            )
+        )
+
+        board_g.add(
+            dwg.rect(
+                (13 * GRID_SIZE, 0),
+                (
+                    4 * GRID_SIZE,
+                    2 * GRID_SIZE,
+                ),
+                stroke=color_set.grid_color,
+                fill=color_set.background_color,
+            )
+        )
+        board_g.add(
+            dwg.circle(
+                center=(14 * GRID_SIZE, 1 * GRID_SIZE),
+                r=0.5 * GRID_SIZE,
+                stroke=color_set.grid_color,
+                fill=color_set.p2_color,
+            )
+        )
+        board_g.add(
+            dwg.text(
+                text=f"× {state.board[27]}",  # 27:黒
+                insert=(
+                    14.8 * GRID_SIZE,
+                    1.3 * GRID_SIZE,
+                ),
+                fill=color_set.grid_color,
+                font_size="34px",
+                font_family="serif",
+            )
+        )
+
+        return board_g
 
     def _make_chess_dwg(
         self,
