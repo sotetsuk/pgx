deef33ef3776a15677d9fac0247a5a0af360dc12
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-01-26 07:48:32 +0900

    Add RandomOpponentEnv

--- pgx/__init__.py
+++ pgx/__init__.py
@@ -1,2 +1,27 @@
-def hello():
-    return "hello world"
+from typing import Literal
+
+EnvId = Literal[
+    "tic_tac_toe/v0",
+    "minatar/breakout/v0",
+    "suzume_jong/v0",
+    "go/v0",
+]
+
+
+def make(env_id: EnvId):
+    if env_id == "tic_tac_toe/v0":
+        from pgx.tic_tac_toe import init, observe, step
+
+        return init, step, observe
+    elif env_id == "minatar/breakout/v0":
+        from pgx.minatar.breakout import init, observe, step
+
+        return init, step, observe
+    elif env_id == "suzume_jong/v0":
+        from pgx.suzume_jong import init, observe, step
+
+        return init, step, observe
+    elif env_id == "go/v0":
+        from pgx.go import init, observe, step
+
+        return init, step, observe
