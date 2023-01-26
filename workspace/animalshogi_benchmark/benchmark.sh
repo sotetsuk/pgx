4c988e28feb5b1fb09532dc1b875ebfa27d69456
youyou-ku <youyou5016ku@gmail.com>
2023-01-26 21:35:59 +0900

    kaigyou henkou

--- workspace/animalshogi_benchmark/benchmark.sh
+++ workspace/animalshogi_benchmark/benchmark.sh
@@ -1,6 +1,6 @@
 echo "| function name | compile time |"
 echo "| :--- | ---: |"
-for funcname in _move _update_legal_move_actions _effected_positions _is_check _is_try init _legal_actions step _init_legal_actions _board_status _dlaction_to_action
+for funcname in step _update_state_wo_legal_action _pass_move _not_pass_move _merge_ren _set_stone_next_to_oppo_ren _remove_stones legal_actions _get_reward _count_ji _get_alphazero_features
 do
     python3 benchmark.py $funcname
 done
