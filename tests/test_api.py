43fbc54930a59c135bc7d57ef28b1efd710df2ce
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-01-27 14:48:47 +0900

    use jit

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,14 @@
+import jax
+
 import pgx
 from pgx.validator import validate_init, validate_step
 
 
 def test_init():
     init, _, _ = pgx.make("tic_tac_toe/v0")
-    validate_init(init)
+    validate_init(jax.jit(init))
 
 
 def test_step():
     init, step, _ = pgx.make("tic_tac_toe/v0")
-    validate_step(init, step)
+    validate_step(jax.jit(init), jax.jit(step))
