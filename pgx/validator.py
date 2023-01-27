2f0add209ca327ea3a58e794c472994b155928ea
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-01-27 14:44:41 +0900

    add validate_step

--- pgx/validator.py
+++ pgx/validator.py
@@ -1,11 +1,43 @@
 import jax
 
+import pgx
+from pgx.utils import act_randomly
+
 
 def validate_init(init_fn):
-
-    # rng must be set to state.rng
     rng = jax.random.PRNGKey(38292)
     state = init_fn(rng)
     assert not (
         state.rng == jax.random.PRNGKey(0)  # default
     ).all(), f"Non-default rng must be set to state.rng: got {state.rng}"
+
+
+def validate_step(init_fn, step_fn, N=100):
+    rng = jax.random.PRNGKey(38201)
+    for _ in range(N):
+        rng, subkey = jax.random.split(rng)
+        state = init_fn(rng)
+        while True:
+            rng, subkey = jax.random.split(rng)
+            action = act_randomly(subkey, state)
+            state = step_fn(state, action)
+
+            validate_curr_player(state)
+
+            if state.terminated:
+                break
+
+
+def validate_illegal_actions(state: pgx.State):
+    ...
+
+
+def validate_curr_player(state: pgx.State):
+    if state.terminated:
+        assert (
+            state.curr_player == -1
+        ), f"curr_player must be -1 when terminated but got {state.curr_player}"
+    else:
+        assert (
+            state.curr_player >= 0
+        ), f"curr_player must be positivie before terminated but got {state.curr_player}"
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,12 @@
 import pgx
-from pgx.validator import validate_init
+from pgx.validator import validate_init, validate_step
 
 
 def test_init():
     init, _, _ = pgx.make("tic_tac_toe/v0")
     validate_init(init)
+
+
+def test_step():
+    init, step, _ = pgx.make("tic_tac_toe/v0")
+    validate_step(init, step)
