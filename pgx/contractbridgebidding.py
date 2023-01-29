ae36f3a9e3666f75ec1f464f9b20149542bd72bb
Kita-Haruka <hrkkt1213@gmail.com>
2023-01-29 19:30:05 +0900

    fix fmt

--- pgx/contractbridgebidding.py
+++ pgx/contractbridgebidding.py
@@ -349,7 +349,9 @@ def _card_str_to_int(card: str) -> int:
 
 def _key_to_hand(key: np.ndarray) -> np.ndarray:
     """Convert key to hand
-    >>> key = np.array([22369621, 22369621, 22369621, 22369621], dtype=np.int32)
+    >>> key = np.array(
+    ...     [22369621, 22369621, 22369621, 22369621], dtype=np.int32
+    ... )
     >>> _key_to_hand(key)
     array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
