9fbb8c81b9b31356faee76e49976f564671e0c46
nissymori <kyoudai.630@gmail.com>
2023-01-30 00:54:24 +0900

    multi processing

--- workspace/compariosn_to_other_benchmarks/go_comparison.py
+++ workspace/compariosn_to_other_benchmarks/go_comparison.py
@@ -6,29 +6,28 @@ import random
 import pyspiel
 import numpy as np
 import time
+import argparse
+from multiprocessing import Pool
+from pgx.go import init, step, observe
+import jax
+from jax import vmap, jit
+import jax.numpy as jnp
+import numpy as np
 
 
 
-def main():
-    pass
-
-
-
-if __name__ == "__main__":
-    # petting_zoo
+def petting_zoo_random_go():
+    # petting zooのgo環境でrandom gaentを終局まで動かす.
     petting_zoo_go_env = go.env()
     petting_zoo_go_env.reset()
-    time_sta = time.time()
     for agent in petting_zoo_go_env.agent_iter():
         observation, reward, termination, truncation, info = petting_zoo_go_env.last()
         action = None if termination or truncation else petting_zoo_go_env.action_space(agent).sample()  # this is where you would insert your policy
         petting_zoo_go_env.step(action)
-    time_end = time.time()
-    tim = time_end- time_sta
-    print("execution time in petting zoo is {}".format(tim))
 
-    # open spile
-    time_sta = time.time()
+
+def open_spile_random_go():
+    # open spileのgo環境でrandom gaentを終局まで動かす.
     game = pyspiel.load_game("go")
     state = game.new_initial_state()
     while not state.is_terminal():
@@ -45,5 +44,64 @@ if __name__ == "__main__":
             # We arbitrarily select the first available action as an example.
             action = legal_actions[0]
             state.apply_action(action)
+@jit
+def _init(key):
+    return init(key, size=19)
+
+
+@jit
+def _step(state, action):
+    return jit(step, static_argnums=(2,))(state, action, 19)
+
+
+init_vmap = vmap(_init)
+step_vmap = vmap(_step)
+
+
+def pgx_random_go(n_envs, key):
+    # pgxのgo環境でrandom agentを終局まで動かす.
+    for_init, rng = jax.random.split(key)
+    keys = jax.random.split(for_init, n_envs)
+    curr_player, state = init_vmap(keys)
+    print(curr_player.shape)
+    while state.terminated.sum() != n_envs:
+        rng = jax.random.split(rng)[0]
+        logits = jnp.log(state.legal_action_mask.astype(jnp.float16))
+        action = jax.random.categorical(rng, logits=logits)
+        curr_player, state, reward = step_vmap(state, action)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("n_processes", type=int)
+    args = parser.parse_args()
+    n_processes = args.n_processes
+
+    # petting_zoo
+    p = Pool(n_processes)
+    time_sta = time.time()
+    for i in range(n_processes):
+        p.apply(petting_zoo_random_go)
+    p.close()
+    time_end = time.time()
+    tim = time_end- time_sta
+    print("execution time in petting zoo is {}".format(tim))
+
+    # open spile
+    p = Pool(n_processes)
+    time_sta = time.time()
+    for i in range(n_processes):
+        p.apply(open_spile_random_go)
+    p.close()
+    time_end = time.time()
     tim = time_end- time_sta
     print("execution time in open spile is {}".format(tim))
+
+    # pgx
+    seed = np.random.randint(0, 1000)
+    key = jax.random.PRNGKey(seed)
+    time_sta = time.time()
+    pgx_random_go(n_processes, key)
+    time_end = time.time()
+    tim = time_end- time_sta
+    print("execution time in pgx is {}".format(tim))
