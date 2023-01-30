be9659efaa29e634f9059a9c843b8862c2b09c93
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-01-30 22:51:31 +0900

    rm map

--- pgx/go.py
+++ pgx/go.py
@@ -476,12 +476,10 @@ def _merge_ren(_state: GoState, _xy: int, _adj_xy: int):
 
     _adj_ren_id = _state.adj_ren_id.at[_my_color(_state)].get()
 
-    _oppo_adj_ren_id = jax.lax.map(
-        lambda _a: jnp.where(
-            _a[large_id], _a.at[large_id].set(False).at[small_id].set(True), _a
-        ),
-        _state.adj_ren_id.at[_opponent_color(_state)].get(),
-    )
+    _adj_r = _state.adj_ren_id[_opponent_color(_state)]
+    large_ids = jnp.tile(large_id, 5 * 5)
+    _oppo_adj_ren_id = jnp.where(_adj_r[large_ids], _adj_r.at[large_ids].set(False).at[small_id].set(True), _adj_r)
+
     _adj_ren_id = _adj_ren_id.at[small_id].set(
         jnp.logical_or(_adj_ren_id[small_id], _adj_ren_id[large_id])
     )
