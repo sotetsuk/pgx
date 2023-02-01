63d848f70116f6c480203952ea09bc7bc3538cd8
nissymori <kyoudai.630@gmail.com>
2023-02-02 03:37:49 +0900

    implement multiprocessing

--- workspace/compariosn/go_comparison.py
+++ workspace/compariosn/go_comparison.py
@@ -8,6 +8,8 @@ import numpy as np
 import time
 import argparse
 from multiprocessing import Pool
+from multiprocessing import Queue
+import multiprocessing
 from pgx.go import init, step, observe
 import jax
 from jax import vmap, jit
@@ -16,8 +18,9 @@ import numpy as np
 
 
 
-def petting_zoo_random_go():
+def petting_zoo_random_go(tup):
     # petting zooのgo環境でrandom gaentを終局まで動かす.
+    id, do_print = tup
     step_nums = 0
     petting_zoo_go_env = go.env()
     petting_zoo_go_env.reset()
@@ -26,22 +29,26 @@ def petting_zoo_random_go():
         action = None if termination or truncation else np.random.choice(np.where(observation["action_mask"]==1)[0])  # this is where you would insert your policy
         petting_zoo_go_env.step(action)
         step_nums += 1
-    return step_nums
+    if do_print:
+        print(id, step_nums)
+    return id, step_nums
 
 
-def open_spile_random_go():
+def open_spile_random_go(tup):
     # open spileのgo環境でrandom gaentを終局まで動かす.
+    id, do_print = tup
     game = pyspiel.load_game("go")
     state = game.new_initial_state()
     step_nums = 0
     while not state.is_terminal():
         legal_actions = state.legal_actions()
-
         # Sample a chance event outcome.
         action = np.random.choice(legal_actions)
         state.apply_action(action)
         step_nums += 1
-    return step_nums
+    if do_print:
+        print(id, step_nums)
+    return id, step_nums
 
 
 
@@ -55,29 +62,27 @@ if __name__ == "__main__":
     n_games = args.n_games
 
     # petting_zoo
+    # open spile
     p = Pool(n_processes)
+    process_list = []
     time_sta = time.time()
-    total_step_nums = 0
-    for i in range(n_games):
-        step_nums = p.apply(petting_zoo_random_go)
-        if args.print_per_game:
-            print(i, step_nums)
-        total_step_nums += step_nums   
-    p.close()
+    ex = p.map_async(petting_zoo_random_go, iterable=[(i, args.print_per_game) for i in range(n_games)])
+    result = ex.get()
     time_end = time.time()
     tim = time_end- time_sta
-    print("execution time in petting zoo is {}, avarage number of steps is {}".format(tim, total_step_nums//n_games))
+    p.close()
+    avarage_steps = sum(list(map(lambda x: x[1], ex.get())))//n_games
+    print("execution time in petting zoo is {}, avarage number of steps is {}".format(tim, avarage_steps))
+
 
     # open spile
     p = Pool(n_processes)
+    process_list = []
     time_sta = time.time()
-    total_step_nums = 0
-    for i in range(n_games):
-        step_nums = p.apply(open_spile_random_go)
-        if args.print_per_game:
-            print(i, step_nums)
-        total_step_nums += step_nums
-    p.close()
+    ex = p.map_async(open_spile_random_go, iterable=[(i, args.print_per_game) for i in range(n_games)])
+    result = ex.get()
     time_end = time.time()
     tim = time_end- time_sta
-    print("execution time in open spile is {}, avarage number of steps is {}".format(tim, total_step_nums//n_games))
+    p.close()
+    avarage_steps = sum(list(map(lambda x: x[1], ex.get())))//n_games
+    print("execution time in open_spile is {}, avarage number of steps is {}".format(tim, avarage_steps))
