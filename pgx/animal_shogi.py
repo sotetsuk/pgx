8f3358edbc5815be98026cf27c95b9cdfb3c01ee
youyou-ku <youyou5016ku@gmail.com>
2023-02-02 22:25:23 +0900

    resolve miss2

--- pgx/animal_shogi.py
+++ pgx/animal_shogi.py
@@ -637,13 +637,13 @@ def _filter_suicide_actions(
     moves = POINT_MOVES[king_sq, 4].reshape(12)
     for i in range(12):
         array = jax.lax.cond(
-            moves[i] & (effects[i] == 1),
+            moves[i] | (effects[i] != 0),
+            lambda: array,
             lambda: array.at[
                 _dlshogi_action(
                     _point_to_direction(king_sq, i, False, turn), i
                 )
             ].set(FALSE),
-            lambda: array,
         )
     return array
 
