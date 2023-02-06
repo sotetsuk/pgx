68f75d966d00a7c85afb546df3b6c2f547cdbc74
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-06 21:55:39 +0900

    .

---
+++ workspace/shogi_benchmark/benchmark.sh
@@ -0,0 +1,7 @@
+echo "| function name | # jaxpr lines | compile time |"
+echo "| :--- | ---: | ---: |"
+# for funcname in init step _dlaction_to_action _action_to_dlaction _piece_moves _legal_actions _is_mate
+for funcname in _dlaction_to_action _action_to_dlaction _piece_moves _legal_actions _is_mate
+do
+    python3 benchmark.py $funcname
+done
