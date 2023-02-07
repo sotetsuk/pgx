589113add1aa4681ea9eb97624e7ac706bf40deb
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-07 22:36:09 +0900

    .

--- tests/test_shogi.py
+++ tests/test_shogi.py
@@ -1,6 +1,6 @@
 import jax.numpy as jnp
 
-from pgx.shogi import State, init, _to_sfen
+from pgx.shogi import State, init, to_sfen
 
 
 def test_init():
@@ -9,7 +9,7 @@ def test_init():
 
 
 def test_to_sfen():
-    sfen = _to_sfen(init())
+    sfen = to_sfen(init())
     assert (sfen == "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1")
     pb = jnp.int8(
         [16, 0, 0, 15, 0, 1, 0, 0, 2, 0, 0, 0, 2, 15, 0, 0, 0, 6, 9, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 15, 0, 1, 0,
