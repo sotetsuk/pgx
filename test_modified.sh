74dfc9edd3fa49dcacaf637a2e1610e5a6e160c1
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-10 11:04:42 +0900

    Revert "."
    This reverts commit 5b9d8401ee386ca05a70104fce14fc53a9932159.

--- test_modified.sh
+++ test_modified.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-git fetch -a
+
 for game in animal_shogi backgammon chess contractbridgebidding go shogi suzume_jong tic_tac_toe; do
     if [[ -n $(git diff --name-only origin/main | grep py | grep ${game}) ]]; then
         python3 -m pytest --doctest-modules -v pgx/${game}.py tests/test_${game}.py || exit 1
