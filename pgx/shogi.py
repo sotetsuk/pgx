0960bc9a2ecd3913f4dcd8b7378906d575aea156
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-12 07:45:50 +0900

    .

--- pgx/shogi.py
+++ pgx/shogi.py
@@ -206,9 +206,8 @@ class Action:
 
     @staticmethod
     def from_dlshogi_action(state: State, action: jnp.ndarray):
-        action = jnp.int8(action)
-        # action = jnp.int32(action)  # NOTE: action (e.g., 2000) is bigger than int8
-        direction, to = action // 81, action % 81
+        action = jnp.int32(action)  # NOTE: action (e.g., 2000) is bigger than int8
+        direction, to = jnp.int8(action // 81), jnp.int8(action % 81)
         is_drop = direction >= 20
         # Compute <from> from <dir, to>
         #
