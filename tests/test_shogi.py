256a89f52bc80009abb99ca4433c317985bbeac4
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-13 00:39:09 +0900

    add test

--- tests/test_shogi.py
+++ tests/test_shogi.py
@@ -362,3 +362,17 @@ def test_dlshogi_action():
     legal_action_mask = _to_direction(legal_actions)
     assert legal_action_mask[20 * 81 + xy2i(1, 5)]
     assert not legal_action_mask[20 * 81 + xy2i(2, 5)]
+
+
+def test_step():
+    # init
+    s = _init()
+    visualize(s, "tests/assets/shogi/step_001.svg")
+    s = step(s, 268)
+    visualize(s, "tests/assets/shogi/step_002.svg")
+    s = step(s, 268)
+    visualize(s, "tests/assets/shogi/step_003.svg")
+    legal_actions = _legal_actions(s)
+    legal_action_mask = _to_direction(legal_actions)
+    print(_rotate(s.legal_action_mask.reshape(27, 81)[3].reshape(9, 9)))
+    assert not s.legal_action_mask[268]
