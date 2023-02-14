ca5cb0fe604189e3305c6a98edc45a699d6c7985
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-14 15:35:03 +0900

    [Shogi] Add env (#348)

--- pgx/__init__.py
+++ pgx/__init__.py
@@ -8,6 +8,10 @@ def make(env_id: EnvId):
         from pgx.tic_tac_toe import TicTacToe
 
         return TicTacToe()
+    elif env_id == "shogi/v0":
+        from pgx.shogi import Shogi
+
+        return Shogi()
     elif env_id == "minatar/asterix/v0":
         from pgx.minatar.asterix import MinAtarAsterix
 
