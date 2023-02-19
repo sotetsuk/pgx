5f0e47b46da02b1a7ad560c4ca5236d9eafc2aa3
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-19 20:51:09 +0900

    .

--- pgx/shogi.py
+++ pgx/shogi.py
@@ -373,16 +373,14 @@ def _step_move(state: State, action: Action) -> State:
     ####################################################################################
     # Update cached effects
     ####################################################################################
+    # fmt: off
     my_effects = state.effects[0]
     opp_effects = state.effects[1]
     # 移動元で塞がれていた利きを復元する（from_に効いていた大駒の利きを作り直す）
     queen_effect = jnp.tile(_queen_effect(state, action.from_), reps=(81, 1))
     my_effects |= _effect_filter_through(state, action.from_) & queen_effect
     # 相手も同様
-    opp_effects |= (
-        _effect_filter_through(_flip(state), _roatate_pos(action.from_))
-        & queen_effect[::-1]
-    )
+    opp_effects |= (_effect_filter_through(_flip(state), _roatate_pos(action.from_)) & queen_effect[::-1])
     # 移動元からの古い利きを消す
     my_effects = my_effects.at[action.from_, :].set(FALSE)
     # 移動先で相手の利きを消す（移動先で駒を取っていたら、取られた駒の利きを消す）
@@ -392,12 +390,11 @@ def _step_move(state: State, action: Action) -> State:
     # 移動先を通るような利きを塞ぐ
     my_effects &= ~_effect_filter_through(state, action.to)
     # 相手も同様
-    opp_effects &= ~_effect_filter_through(
-        _flip(state), _roatate_pos(action.to)
-    )
+    opp_effects &= ~_effect_filter_through(_flip(state), _roatate_pos(action.to))
     # set updated effects
     state = state.replace(effects=state.effects.at[0].set(my_effects))  # type: ignore
     state = state.replace(effects=state.effects.at[1].set(opp_effects))  # type: ignore
+    # fmt: on
 
     return state
 
@@ -412,19 +409,19 @@ def _step_drop(state: State, action: Action) -> State:
     ####################################################################################
     # Update cached effects
     ####################################################################################
+    # fmt: off
     my_effects = state.effects[0]
     opp_effects = state.effects[1]
-    # [OK] 新しい利きが増える
+    # 新しい利きが増える
     my_effects = my_effects.at[action.to, :].set(_effects(state, action.to))
-    # [OK] 打たれた点を経由する大駒の利きが消える
+    # 打たれた点を経由する大駒の利きが消える
     my_effects &= ~_effect_filter_through(state, action.to)
-    # [OK] 相手も同様
-    opp_effects &= ~_effect_filter_through(
-        _flip(state), _roatate_pos(action.to)
-    )
+    # 相手も同様
+    opp_effects &= ~_effect_filter_through(_flip(state), _roatate_pos(action.to))
     # set updated effects
     state = state.replace(effects=state.effects.at[0].set(my_effects))  # type: ignore
     state = state.replace(effects=state.effects.at[1].set(opp_effects))  # type: ignore
+    # fmt: on
 
     return state
 
