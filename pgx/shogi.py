bca8648e6bc089ed113799e246f85476de0678c4
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-20 16:18:24 +0900

    [Shogi] Fix from/to sfen (#376)

--- pgx/shogi.py
+++ pgx/shogi.py
@@ -135,11 +135,13 @@ class State(core.State):
         """Mainly for debugging purpose.
         terminated, reward, and curr_player are not changed"""
         state = State(turn=turn, piece_board=piece_board, hand=hand)  # type: ignore
-        # TODO: set effects before computing legal actions
+        # fmt: off
+        state = state.replace(effects=state.effects.at[0].set(_effects_all(state)))  # type: ignore
+        state = state.replace(effects=state.effects.at[1].set(_effects_all(_flip(state))))  # type: ignore
+        state = jax.lax.cond(turn % 2 == 1, lambda: _flip(state), lambda: state)
         legal_moves, legal_promotions, legal_drops = _legal_actions(state)
-        legal_action_mask = _to_direction(
-            legal_moves, legal_promotions, legal_drops
-        )
+        legal_action_mask = _to_direction(legal_moves, legal_promotions, legal_drops)
+        # fmt: on
         return state.replace(legal_action_mask=legal_action_mask, legal_moves=legal_moves)  # type: ignore
 
 
@@ -1068,6 +1070,8 @@ def _to_sfen(state: State):
     >>> _to_sfen(s)
     'lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1'
     """
+    if state.turn % 2 == 1:
+        state = _flip(state)
 
     pb = jnp.rot90(state.piece_board.reshape((9, 9)), k=3)
     sfen = ""
@@ -1159,24 +1163,3 @@ def _from_sfen(sfen):
         piece_board=jnp.rot90(piece_board.reshape((9, 9)), k=1).flatten(),
         hand=jnp.reshape(s_hand, (2, 7)),
     )
-
-
-def _from_cshogi(board):
-    """Convert cshogi (github.com/TadaoYamaoka/cshogi) board into Pgx state.
-
-    board.pieces_in_hand: 歩香桂銀[金]角飛 金のindexが違う
-    """
-    pb = jnp.zeros(81, dtype=jnp.int8)
-    hand = jnp.zeros((2, 7), dtype=jnp.int8)
-    # fmt: off
-    board_piece_dir = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 0, 0, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
-    # fmt: on
-    hand_piece_dir = [0, 1, 2, 3, 5, 6, 4]
-    pieces = board.pieces
-    pieces_in_hand = board.pieces_in_hand
-    for i in range(81):
-        pb = pb.at[i].set(board_piece_dir[pieces[i]])
-    for i in range(2):
-        for j in range(7):
-            hand = hand.at[i, j].set(pieces_in_hand[i][hand_piece_dir[j]])
-    return State._from_board(turn=board.turn, piece_board=pb, hand=hand)
