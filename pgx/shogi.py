105d68d825cc08e173adac3fb6ba55f27e20dd2e
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-21 19:47:50 +0900

    .

--- pgx/shogi.py
+++ pgx/shogi.py
@@ -664,7 +664,9 @@ def _legal_promotion(state: State, legal_moves: jnp.ndarray) -> jnp.ndarray:
         is_line1 | is_line2
     )
     promotion = jnp.where((promotion != 0) & is_stuck, TWO, promotion)
-    promotion = jnp.where((state.piece_board < GOLD).reshape(81, 1), promotion, ZERO)
+    promotion = jnp.where(
+        (state.piece_board < GOLD).reshape(81, 1), promotion, ZERO
+    )
     return promotion
 
 
