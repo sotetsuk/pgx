69a95e6911f2282d5bd5d26a2b2163829a42561f
nissymori <kyoudai.630@gmail.com>
2023-02-23 01:21:43 +0900

    ss

--- workspace/compariosn/petting_zoo_forloop.py
+++ workspace/compariosn/petting_zoo_forloop.py
@@ -1,34 +1,75 @@
-from vector_env import SyncVectorEnv
 import argparse
 import time
 import numpy as np
 import collections
-from tianshou.env import DummyVectorEnv
-from tianshou.env.pettingzoo_env import PettingZooEnv
-from pettingzoo.classic.tictactoe import tictactoe
-from pettingzoo.classic.go import go
+import supersuit as ss
+from pettingzoo.utils.env import AECEnv
+from pettingzoo.utils.conversions import aec_to_parallel
+import cloudpickle
 
-class AutoResetPettingZooEnv(PettingZooEnv):
-    def __init__(self, env):
-        super().__init__(env)
+def transpose(ll):
+    return [[ll[i][j] for i in range(len(ll))] for j in range(len(ll[0]))]
+
+def vec_env_args(env, num_envs):
+    def env_fn():
+        env_copy = cloudpickle.loads(cloudpickle.dumps(env))
+        return env_copy
+
+    return [env_fn] * num_envs
+class AutoResetPettingZooEnv(ss.vector.ConcatVecEnv):
+    """
+    Autoresetを実装
+    """
+    def __init__(self, vec_env_fn):
+        super().__init__(vec_env_fn)
     
-    def step(self, action):
-        obs, reward, term, trunc, info = super().step(action)
-        if term:
-            obs = super().reset()
-        return obs, reward, term, trunc, info
+
+    def reset(self, seed=None, options=None):
+        obs = super().reset()
+        return obs
+
+    def step(self, actions):
+        data = []
+        idx = 0
+        for i, venv in enumerate(self.vec_envs):
+            data.append(
+                venv.step(
+                    actions[i]
+                )
+            )
+            idx += venv.num_envs
+        observations, rewards, terminations, truncations, infos = transpose(data)
+        observations = self.concat_obs(observations)
+        rewards = np.concatenate(rewards, axis=0)
+        terminations = np.concatenate(terminations, axis=0)
+        truncations = np.concatenate(truncations, axis=0)
+        infos = sum(infos, [])
+        return observations, rewards, terminations, truncations, infos
 
 
 def make_env(env_name, n_envs):
+    from pettingzoo.classic.go import go
+    from pettingzoo.classic.tictactoe import tictactoe
+
     #from pettingzoo.classic import chess_v5
     def get_go_env():
-        return AutoResetPettingZooEnv(go.env())
+        env = go.env()
+        env.metadata["is_parallelizable"] = True
+        return aec_to_parallel(env)
+    
     def get_tictactoe_env():
-        return AutoResetPettingZooEnv(tictactoe.env())
+        return tictactoe.env()
     if env_name == "go":
-        return DummyVectorEnv([get_go_env for _ in range(n_envs)])
+        env = ss.pettingzoo_env_to_vec_env_v1(get_go_env())
+        envs = AutoResetPettingZooEnv(vec_env_args(env, n_envs))
+        envs.single_observation_space = envs.observation_space
+        envs.single_action_space = envs.action_space
+        envs.is_vector_env = True
+        return envs
     elif env_name == "tictactoe":
-        return DummyVectorEnv([get_tictactoe_env() for _ in range(n_envs)])
+        env = ss.pettingzoo_env_to_vec_env_v1(env)
+        envs = ss.concat_vec_envs_v1(env, n_envs, num_cpus=0)
+        return envs
     elif env_name == "chess":
         #return chess_v5.env()
         raise ValueError("Chess will be added later")
@@ -36,17 +77,16 @@ def make_env(env_name, n_envs):
         raise ValueError("no such environment in petting zoo")
 
 
-def random_play(env: DummyVectorEnv, n_steps_lim: int) -> int:
+def random_play(env, n_steps_lim: int) -> int: # TODO autoreset
     # petting zooのgo環境でrandom gaentを終局まで動かす.
     step_num = 0
     rng = np.random.default_rng()
     observation = env.reset()
-    terminated = np.zeros(len(env._env_fns))
+    player = 0
     while step_num < n_steps_lim:
-        assert len(env._env_fns) == len(observation)  # ensure parallerization
-        legal_action_mask = np.array([observation[i]["mask"] for i in range(len(observation))])
-        action = [rng.choice(np.where(legal_action_mask[i]==1)[0]) for i in range(len(legal_action_mask))]  # chose action randomly
-        observation, reward, terminated, _, _ = env.step(action)
+        legal_action_mask = np.array([mask for mask in observation["action_mask"]])
+        action = [[rng.choice(np.where(legal_action_mask[2*i]==1)[0]), 0] for i in range(len(legal_action_mask)//2)]  # chose action randomly
+        observation, rewards, terminations, truncations, infos = env.step(action)
         step_num += 1
     return step_num
 
