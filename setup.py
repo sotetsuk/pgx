06d893f9db604ee54b5b10d3d33582bb6280f145
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-25 02:36:53 +0900

    Release v0.0.19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pgx",
-    version="0.0.18",
+    version="0.0.19",
     long_description_content_type="text/markdown",
     description="",
     url="",
