1ae70ffbdbaabfc0582cad79f747aee6d5157a7f
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-25 00:32:26 +0900

    rm unused files

--- speed_benchmark/compare.sh
+++
@@ -1,6 +0,0 @@
-echo "| library | game | type | n_envs | steps | time | time per step |"
-echo "| :--- | ---: | ---: | ---: | ---: | ---: | ---: |"
-python compare_subproc.py open_spiel go 10 1000
-python compare_subproc.py petting_zoo go 10 1000
-python compare_for_loop.py open_spiel go 10 1000
-python compare_for_loop.py petting_zoo go 10 1000
