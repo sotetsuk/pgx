fc6d841642fc57ed73b1c76c078a7722707dd845
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-24 22:51:42 +0900

    .

--- speed_benchmark/petting_zoo_forloop.py
+++ speed_benchmark/petting_zoo_forloop.py
@@ -3,7 +3,6 @@ import time
 import json
 import numpy as np
 import collections
-from tianshou.env import DummyVectorEnv
 from tianshou.env.pettingzoo_env import PettingZooEnv
 
 
@@ -18,7 +17,7 @@ class AutoResetPettingZooEnv(PettingZooEnv):
         return obs, reward, term, trunc, info
 
 
-def make_env(env_name, n_envs):
+def make_env(env_name, n_envs, vec_env):
     
     def get_go_env():
         from pettingzoo.classic.go import go
@@ -31,24 +30,29 @@ def make_env(env_name, n_envs):
     def get_chess_env():
         from pettingzoo.classic.chess import chess
         return AutoResetPettingZooEnv(chess.env())
+
+    if vec_env == "for-loop": 
+        from tianshou.env import DummyVectorEnv as VecEnv
+    elif vec_env == "subproc":
+        from tianshou.env import SubprocVectorEnv as VecEnv
     
     if env_name == "go":
-        return DummyVectorEnv([get_go_env for _ in range(n_envs)])
+        env_fn = get_go_env
     elif env_name == "tic_tac_toe":
-        return DummyVectorEnv([get_tictactoe_env for _ in range(n_envs)])
+        env_fn = get_tictactoe_env
     elif env_name == "chess":
-        return DummyVectorEnv([get_chess_env for _ in range(n_envs)])
-    else:
-        raise ValueError(f"wrong argument: {env_name}")
+        env_fn = get_chess_env
+        
+    return VecEnv([env_fn for _ in range(n_envs)])
 
 
-def random_play(env: DummyVectorEnv, n_steps_lim: int, batch_size: int) -> int:
+def random_play(env, n_steps_lim: int, batch_size: int) -> int:
     # petting zooのgo環境でrandom gaentを終局まで動かす.
     step_num = 0
     rng = np.random.default_rng()
     observation = env.reset()
+    assert len(env._env_fns) == len(observation)  # ensure parallerization
     while step_num < n_steps_lim:
-        assert len(env._env_fns) == len(observation)  # ensure parallerization
         legal_action_mask = np.array([observation[i]["mask"] for i in range(batch_size)])
         action = [rng.choice(np.where(legal_action_mask[i])[0]) for i in range(batch_size)]  # chose action randomly
         observation, reward, terminated, _, _ = env.step(action)
