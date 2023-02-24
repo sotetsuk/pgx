8d91d8ab3fd6a1258c0d5c2872e8be0243018aa8
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-25 04:06:04 +0900

    .

---
+++ speed_benchmark/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+N=10240
+
+
+# CUDA_VISIBLE_DEVICES=0 python3 -O run_pgx.py
+
+# for game in "tic_tac_toe" "backgammon" "go"; do
+# for bs in 2 4 8 16 32 64 128 256 512 1024; do
+#   python3 -O run_open_spiel_forloop.py $game $bs $N
+# done
+# done
+
+for game in "tic_tac_toe" "backgammon" "go"; do
+for bs in 2 4 8 16 32 64 128 256 512 1024; do
+  python3 -O run_open_spiel_subproc.py $game $bs $N
+done
+done
+
+# for game in "tic_tac_toe" "go"; do
+# for bs in 2 4 8 16 32 64 128 256 512 1024; do
+#   python3 -O run_petting_zoo.py $game for-loop $bs $N 2>/dev/null
+# done
+# done
+
+# for game in "tic_tac_toe" "go"; do
+# for bs in 2 4 8 16 32 64 128 256 512 1024; do
+#   python3 -O run_petting_zoo.py $game subproc $bs $N 2>/dev/null
+# done
+# done
