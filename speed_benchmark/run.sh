65177e4e1b96d962ab3f83ceb9a006ad98ca6163
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-25 04:20:33 +0900

    .

--- speed_benchmark/run.sh
+++ speed_benchmark/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 
-N=10240
+N=102400
 
 
-# CUDA_VISIBLE_DEVICES=0 python3 -O run_pgx.py
+CUDA_VISIBLE_DEVICES=0 python3 -O run_pgx.py $N
 
-# for game in "tic_tac_toe" "backgammon" "go"; do
-# for bs in 2 4 8 16 32 64 128 256 512 1024; do
-#   python3 -O run_open_spiel_forloop.py $game $bs $N
-# done
-# done
+for game in "tic_tac_toe" "backgammon" "go"; do
+for bs in 2 4 8 16 32 64 128 256 512 1024; do
+  python3 -O run_open_spiel_forloop.py $game $bs $N
+done
+done
 
 for game in "tic_tac_toe" "backgammon" "go"; do
 for bs in 2 4 8 16 32 64 128 256 512 1024; do
@@ -18,14 +18,14 @@ for bs in 2 4 8 16 32 64 128 256 512 1024; do
 done
 done
 
-# for game in "tic_tac_toe" "go"; do
-# for bs in 2 4 8 16 32 64 128 256 512 1024; do
-#   python3 -O run_petting_zoo.py $game for-loop $bs $N 2>/dev/null
-# done
-# done
+for game in "tic_tac_toe" "go"; do
+for bs in 2 4 8 16 32 64 128 256 512 1024; do
+  python3 -O run_petting_zoo.py $game for-loop $bs $N 2>/dev/null
+done
+done
 
-# for game in "tic_tac_toe" "go"; do
-# for bs in 2 4 8 16 32 64 128 256 512 1024; do
-#   python3 -O run_petting_zoo.py $game subproc $bs $N 2>/dev/null
-# done
-# done
+for game in "tic_tac_toe" "go"; do
+for bs in 2 4 8 16 32 64 128 256 512 1024; do
+  python3 -O run_petting_zoo.py $game subproc $bs $N 2>/dev/null
+done
+done
