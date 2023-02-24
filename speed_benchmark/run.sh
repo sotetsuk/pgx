8b01d22e9acdac7d1bf512a37a73aaa478bff2ce
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-25 04:22:12 +0900

    .

--- speed_benchmark/run.sh
+++ speed_benchmark/run.sh
@@ -3,29 +3,29 @@
 
 N=102400
 
-
-CUDA_VISIBLE_DEVICES=0 python3 -O run_pgx.py $N
+ 
+CUDA_VISIBLE_DEVICES=0 python3 -O run_pgx.py $N > results.json
 
 for game in "tic_tac_toe" "backgammon" "go"; do
 for bs in 2 4 8 16 32 64 128 256 512 1024; do
-  python3 -O run_open_spiel_forloop.py $game $bs $N
+  python3 -O run_open_spiel_forloop.py $game $bs $N >> results.json
 done
 done
 
 for game in "tic_tac_toe" "backgammon" "go"; do
 for bs in 2 4 8 16 32 64 128 256 512 1024; do
-  python3 -O run_open_spiel_subproc.py $game $bs $N
+  python3 -O run_open_spiel_subproc.py $game $bs $N >> results.json
 done
 done
 
 for game in "tic_tac_toe" "go"; do
 for bs in 2 4 8 16 32 64 128 256 512 1024; do
-  python3 -O run_petting_zoo.py $game for-loop $bs $N 2>/dev/null
+  python3 -O run_petting_zoo.py $game for-loop $bs $N >> results.json 2>/dev/null
 done
 done
 
 for game in "tic_tac_toe" "go"; do
 for bs in 2 4 8 16 32 64 128 256 512 1024; do
-  python3 -O run_petting_zoo.py $game subproc $bs $N 2>/dev/null
+  python3 -O run_petting_zoo.py $game subproc $bs $N >> results.json 2>/dev/null
 done
 done
