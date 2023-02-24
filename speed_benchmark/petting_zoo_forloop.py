8d988fbe9613e3766617c75250731aa274e69044
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-24 22:31:48 +0900

    tidy

--- speed_benchmark/petting_zoo_forloop.py
+++ speed_benchmark/petting_zoo_forloop.py
@@ -1,10 +1,11 @@
 import argparse
 import time
+import json
 import numpy as np
 import collections
 from tianshou.env import DummyVectorEnv
 from tianshou.env.pettingzoo_env import PettingZooEnv
-from pettingzoo.classic.tictactoe import tictactoe
+
 
 class AutoResetPettingZooEnv(PettingZooEnv):
     def __init__(self, env):
@@ -18,21 +19,27 @@ class AutoResetPettingZooEnv(PettingZooEnv):
 
 
 def make_env(env_name, n_envs):
-    from pettingzoo.classic.go import go
-    #from pettingzoo.classic import chess_v5
+    
     def get_go_env():
+        from pettingzoo.classic.go import go
         return AutoResetPettingZooEnv(go.env())
+
     def get_tictactoe_env():
+        from pettingzoo.classic.tictactoe import tictactoe
         return AutoResetPettingZooEnv(tictactoe.env())
+    
+    def get_chess_env():
+        from pettingzoo.classic.chess import chess
+        return AutoResetPettingZooEnv(chess.env())
+    
     if env_name == "go":
         return DummyVectorEnv([get_go_env for _ in range(n_envs)])
-    elif env_name == "tictactoe":
+    elif env_name == "tic_tac_toe":
         return DummyVectorEnv([get_tictactoe_env for _ in range(n_envs)])
     elif env_name == "chess":
-        #return chess_v5.env()
-        raise ValueError("Chess will be added later")
+        return DummyVectorEnv([get_chess_env for _ in range(n_envs)])
     else:
-        raise ValueError("no such environment in petting zoo")
+        raise ValueError(f"wrong argument: {env_name}")
 
 
 def random_play(env: DummyVectorEnv, n_steps_lim: int, batch_size: int) -> int:
@@ -40,11 +47,10 @@ def random_play(env: DummyVectorEnv, n_steps_lim: int, batch_size: int) -> int:
     step_num = 0
     rng = np.random.default_rng()
     observation = env.reset()
-    terminated = np.zeros(len(env._env_fns))
     while step_num < n_steps_lim:
         assert len(env._env_fns) == len(observation)  # ensure parallerization
-        legal_action_mask = np.array([observation[i]["mask"] for i in range(len(observation))])
-        action = [rng.choice(np.where(legal_action_mask[i]==1)[0]) for i in range(len(legal_action_mask))]  # chose action randomly
+        legal_action_mask = np.array([observation[i]["mask"] for i in range(batch_size)])
+        action = [rng.choice(np.where(legal_action_mask[i])[0]) for i in range(batch_size)]  # chose action randomly
         observation, reward, terminated, _, _ = env.step(action)
         step_num += batch_size
     return step_num
@@ -55,10 +61,12 @@ if __name__ == "__main__":
     parser.add_argument("env_name")
     parser.add_argument("batch_size", type=int)
     parser.add_argument("n_steps_lim", type=int)
+    parser.add_argument("--seed", default=0, type=int)
     args = parser.parse_args()
     assert args.n_steps_lim % args.batch_size == 0
     env = make_env(args.env_name, args.batch_size)
     time_sta = time.time()
-    step_num = random_play(env, args.n_steps_lim, args.batch_size)
+    steps_num = random_play(env, args.n_steps_lim, args.batch_size)
     time_end = time.time()
-    print((step_num)/(time_end-time_sta))
+    sec = time_end - time_sta
+    print(json.dumps({"game": args.env_name, "library": "open_spiel", "total_steps": steps_num, "total_sec": sec, "steps/sec": steps_num/sec}))
