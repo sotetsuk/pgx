0da08549eee5a734ace15bd00b0610299a41b30a
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-26 03:16:43 +0900

    .

--- speed_benchmark/run_open_spiel_forloop.py
+++ speed_benchmark/run_open_spiel_forloop.py
@@ -4,111 +4,65 @@ import collections
 import time
 import numpy as np
 import pyspiel
-from open_spiel.python.rl_environment import Environment, ChanceEventSampler
 
 
-# SyncVectorEnv is copied from 
-# https://github.com/deepmind/open_spiel/blob/master/open_spiel/python/vector_env.py
-# 
-# Copyright 2022 DeepMind Technologies Limited
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-class SyncVectorEnv(object):
-  """A vectorized RL Environment.
-  This environment is synchronized - games do not execute in parallel. Speedups
-  are realized by calling models on many game states simultaneously.
-  """
+class OpenSpielEnv:
 
-  def __init__(self, envs):
-    if not isinstance(envs, list):
-      raise ValueError(
-          "Need to call this with a list of rl_environment.Environment objects")
-    self.envs = envs
+    def __init__(self, game_name):
+        self.game = pyspiel.load_game(game_name)
+        self.state = self.game.new_initial_state()
 
-  def __len__(self):
-    return len(self.envs)
+    def skip_chance(self):
+        if self.state.is_chance_node():
+            # print("came to chance node")
+            outcomes_with_probs = self.state.chance_outcomes() 
+            action_list, prob_list = zip(*outcomes_with_probs)
+            action = np.random.choice(action_list, p=prob_list)
+            self.state.apply_action(action)
 
-  def observation_spec(self):
-    return self.envs[0].observation_spec()
+    def reset(self):
+        self.state = self.game.new_initial_state()
+        self.skip_chance()
+        obs = self.state.observation_tensor(self.state.current_player())
+        legal_actions = self.state.legal_actions()
+        return obs, legal_actions
 
-  @property
-  def num_players(self):
-    return self.envs[0].num_players
+    def step(self, action):
+        self.state.apply_action(action)
+        self.skip_chance()
+        done = self.state.is_terminal()
 
-  def step(self, step_outputs, reset_if_done=False):
-    """Apply one step.
-    Args:
-      step_outputs: the step outputs
-      reset_if_done: if True, automatically reset the environment
-          when the epsiode ends
-    Returns:
-      time_steps: the time steps,
-      reward: the reward
-      done: done flag
-      unreset_time_steps: unreset time steps
-    """
-    time_steps = [
-        self.envs[i].step([step_outputs[i].action])
-        for i in range(len(self.envs))
-    ]
-    reward = [step.rewards for step in time_steps]
-    done = [step.last() for step in time_steps]
-    unreset_time_steps = time_steps  # Copy these because you may want to look
-                                     # at the unreset versions to extract
-                                     # information from them
+        if done:  # auto reset
+            self.state = self.game.new_initial_state()
 
-    if reset_if_done:
-      time_steps = self.reset(envs_to_reset=done)
-
-    return time_steps, reward, done, unreset_time_steps
-
-  def reset(self, envs_to_reset=None):
-    if envs_to_reset is None:
-      envs_to_reset = [True for _ in range(len(self.envs))]
-
-    time_steps = [
-        self.envs[i].reset()
-        if envs_to_reset[i] else self.envs[i].get_time_step()
-        for i in range(len(self.envs))
-    ]
-    return time_steps
+        obs = self.state.observation_tensor(self.state.current_player())
+        legal_actions = self.state.legal_actions()
+        return obs, legal_actions
 
 
-def make_single_env(env_name: str, seed: int):
-    def gen_env():
-        game = pyspiel.load_game(env_name)
-        return Environment(game,  chance_event_sampler=ChanceEventSampler(seed=seed))
-    return gen_env()
+class DummyVecOpenSpielEnv:
+
+    def __init__(self, game, batch_size):
+        self.batch_size = batch_size
+        self.envs = [OpenSpielEnv(game) for _ in range(self.batch_size)]
+
+    def reset(self):
+        observations, legal_actions = zip(*[env.reset() for env in self.envs])
+        return np.array(observations), legal_actions
+
+    def step(self, action):
+        observations, legal_actions = zip(*[env.step(a) for env, a in zip(self.envs, action)])
+        return np.array(observations), legal_actions
 
 
-def make_env(env_name: str, n_envs: int, seed: int) -> SyncVectorEnv:
-    return SyncVectorEnv([make_single_env(env_name, seed + i) for i in range(n_envs)])
-
-
-def random_play(env: SyncVectorEnv, n_steps_lim: int, batch_size: int):
-    # random play for  open spiel
-    StepOutput = collections.namedtuple("step_output", ["action"])
-    time_step = env.reset()
-    assert len(env.envs) == len(time_step)  # ensure parallerization
-    rng = np.random.default_rng()
+def random_play(env, n_steps_lim, batch_size):
+    assert n_steps_lim % batch_size == 0
+    obs, legal_actions = env.reset()
     step_num = 0
-    while step_num < n_steps_lim:
-        # See https://github.com/deepmind/open_spiel/blob/master/open_spiel/python/examples/rl_example.py
-        obs = np.stack([ts.observations["info_state"][ts.observations["current_player"]] for ts in time_step])
+    for _ in range(n_steps_lim // batch_size):
         # print(obs.shape)
-        actions = [rng.choice(ts.observations["legal_actions"][ts.observations["current_player"]]) for ts in time_step]
-        step_outputs = [StepOutput(action=action) for action in actions]
-        time_step, reward, done, unreset_time_steps = env.step(step_outputs, reset_if_done=True)
+        action = [np.random.choice(a) for a in legal_actions]
+        obs, legal_actions = env.step(action)
         step_num += batch_size
     return step_num
 
@@ -121,7 +75,7 @@ if __name__ == "__main__":
     parser.add_argument("--seed", default=0, type=int)
     args = parser.parse_args()
     assert args.n_steps_lim % args.batch_size == 0
-    env = make_env(args.env_name, args.batch_size, args.seed)
+    env = DummyVecOpenSpielEnv(args.env_name, args.batch_size)
     time_sta = time.time()
     steps_num = random_play(env, args.n_steps_lim, args.batch_size)
     time_end = time.time()
