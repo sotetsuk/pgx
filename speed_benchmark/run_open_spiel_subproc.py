feb267863139774cd131be102d086b97b35facca
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-26 03:34:43 +0900

    .

--- speed_benchmark/run_open_spiel_subproc.py
+++ speed_benchmark/run_open_spiel_subproc.py
@@ -3,23 +3,54 @@ from tianshou.env import SubprocVectorEnv
 import numpy as np
 import time
 import argparse
-import warnings
 from abc import ABC
 from typing import Any, Dict, List, Tuple
 
 import pyspiel
 from pettingzoo.utils.env import AECEnv
-from open_spiel.python.rl_environment import Environment, ChanceEventSampler
 
+
+class OpenSpiel:
+
+    def __init__(self, game_name):
+        self.game = pyspiel.load_game(game_name)
+        self.state = self.game.new_initial_state()
+
+    def skip_chance(self):
+        if self.state.is_chance_node():
+            # print("came to chance node")
+            outcomes_with_probs = self.state.chance_outcomes()
+            action_list, prob_list = zip(*outcomes_with_probs)
+            action = np.random.choice(action_list, p=prob_list)
+            self.state.apply_action(action)
+
+    def reset(self):
+        self.state = self.game.new_initial_state()
+        self.skip_chance()
+        obs = self.state.observation_tensor(self.state.current_player())
+        legal_actions = self.state.legal_actions()
+        return obs, legal_actions
+
+    def step(self, action):
+        self.state.apply_action(action)
+        self.skip_chance()
+        done = self.state.is_terminal()
+
+        if done:  # auto reset
+            self.state = self.game.new_initial_state()
+
+        obs = self.state.observation_tensor(self.state.current_player())
+        legal_actions = self.state.legal_actions()
+        return obs, legal_actions
 
 
 # OpenSpielEnv is modified from TianShou repository (see #384 for changes):
 # This wrapper enables to use TianShou's SubprocVectorEnv for OpenSpiel
-# 
+#
 # https://github.com/thu-ml/tianshou/blob/master/tianshou/env/pettingzoo_env.py
-# 
+#
 # Distributed under MIT LICENSE:
-# 
+#
 # https://github.com/thu-ml/tianshou/blob/master/LICENSE
 class OpenSpielEnv(AECEnv, ABC):
     """The interface for petting zoo environments.
@@ -39,46 +70,32 @@ class OpenSpielEnv(AECEnv, ABC):
     Further usage can be found at :ref:`marl_example`.
     """
 
-    def __init__(self, env: Environment):
+    def __init__(self, env):
         super().__init__()
         self.env = env
         self.reset()
 
-
     def reset(self, *args: Any, **kwargs: Any) -> Tuple[dict, dict]:
-        time_step = self.env.reset()
+        obs, legal_actions = self.env.reset()
 
-        obs = time_step.observations  # open_spielのEnvironmentの出力
         observation_dict = {
-            "agent_id": obs["current_player"],
-            "obs": obs["info_state"][obs["current_player"]],
-            "mask": obs["legal_actions"][obs["current_player"]]
+            "agent_id": 0,
+            "obs": np.array(obs),  # wrap by np.array is several times faster
+            "mask": np.array(legal_actions)
         }  # align to tianshou petting zoo format
 
         # return empty dict for significant speed up
-        return observation_dict, {}  # {"info": obs["info_state"]} 
-
+        return observation_dict, {}
 
     def step(self, action: Any, reset_if_done=True) -> Tuple[Dict, List[int], bool, bool, Dict]:
-
-        time_step = self.env.step([action])
-        term = time_step.last()
-        if term:  # AutoReset
-            time_step = self.env.reset()
-
-        reward = time_step.rewards if not term else [0., 0.]
-        curr_player = time_step.observations['current_player']
-
-
+        obs, legal_actions = self.env.step(action)
         obs = {
-            'agent_id': curr_player,
-            'obs': time_step.observations['info_state'][curr_player],
-            'mask': time_step.observations["legal_actions"][curr_player]
+            'agent_id': 0,
+            'obs': np.array(obs),
+            'mask': np.array(legal_actions)
         }
 
-        # return empty dict for significant speed up
-        return obs, reward, term, False, {}  # {"info": spiel_observation["info_state"]}
-
+        return obs, 0, False, False, {}
 
     def close(self) -> None:
         self.env.close()
@@ -93,26 +110,19 @@ class OpenSpielEnv(AECEnv, ABC):
         return self.env.render()
 
 
-def make_single_env(env_name: str, seed: int):
-    def gen_env():
-        game = pyspiel.load_game(env_name)
-        return Environment(game,  chance_event_sampler=ChanceEventSampler(seed=seed))
-    return gen_env()
-
 def make_env(env_name: str, n_envs: int,  seed: int):
-    return SubprocVectorEnv([lambda: OpenSpielEnv(make_single_env(env_name, seed)) for _ in range(n_envs)])
+    return SubprocVectorEnv([lambda: OpenSpielEnv(OpenSpiel(env_name)) for _ in range(n_envs)])
 
 
 def random_play(env: SubprocVectorEnv, n_steps_lim: int, batch_size: int):
+    assert n_steps_lim % batch_size == 0
     step_num = 0
-    rng = np.random.default_rng()
-    observation, info = env.reset()
-    while step_num < n_steps_lim:
-        legal_action_mask = [observation[i]["mask"] for i in range(batch_size)]
-        observation = np.stack([observation[i]["obs"] for i in range(batch_size)])
-        # print(observation.shape)
-        action = [rng.choice(legal_action_mask[i]) for i in range(batch_size)]  # chose action randomly
-        observation, reward, terminated, _, info = env.step(action)
+    observation, legal_actions = env.reset()
+    for _ in range(n_steps_lim // batch_size):
+        legal_actions = [observation[i]["mask"] for i in range(batch_size)]
+        observation = np.array([observation[i]["obs"] for i in range(batch_size)])
+        action = [np.random.choice(legal_actions[i]) for i in range(batch_size)]
+        observation, _, _, _, _ = env.step(action)
         step_num += batch_size
     return step_num
 
