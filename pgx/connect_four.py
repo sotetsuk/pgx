92c98f406be79f2b3cfd6404996e382c0a0eb6da
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-02-26 18:43:01 +0900

    [ConnectFour, Othello] Add boards (#429)

---
+++ pgx/connect_four.py
@@ -0,0 +1,26 @@
+import jax.numpy as jnp
+
+import pgx.core as core
+from pgx.flax.struct import dataclass
+
+FALSE = jnp.bool_(False)
+TRUE = jnp.bool_(True)
+
+
+@dataclass
+class State(core.State):
+    curr_player: jnp.ndarray = jnp.int8(0)
+    observation: jnp.ndarray = jnp.zeros(27, dtype=jnp.bool_)
+    reward: jnp.ndarray = jnp.float32([0.0, 0.0])
+    terminated: jnp.ndarray = FALSE
+    legal_action_mask: jnp.ndarray = jnp.ones(9, dtype=jnp.bool_)
+    # ---
+    turn: jnp.ndarray = jnp.int8(0)
+    # 6x7 board
+    # [[ 0,  1,  2,  3,  4,  5,  6],
+    #  [ 7,  8,  9, 10, 11, 12, 13],
+    #  [14, 15, 16, 17, 18, 19, 20],
+    #  [21, 22, 23, 24, 25, 26, 27],
+    #  [28, 29, 30, 31, 32, 33, 34],
+    #  [35, 36, 37, 38, 39, 40, 41]]
+    board: jnp.ndarray = -jnp.ones(42, jnp.int8)  # -1 (empty), 0, 1
