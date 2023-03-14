5c4caef87dc15e74802bdb3868eb7bf6afee37f6
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-15 04:39:47 +0900

    .

--- pgx/minatar/asterix.py
+++ pgx/minatar/asterix.py
@@ -83,7 +83,9 @@ class MinAtarAsterix(pgx.Env):
         )
         return state.replace(rng=rng)  # type: ignore
 
-    def _observe(self, state: pgx.State, player_id: jnp.ndarray) -> jnp.ndarray:
+    def _observe(
+        self, state: pgx.State, player_id: jnp.ndarray
+    ) -> jnp.ndarray:
         assert isinstance(state, State)
         return _to_obs(state)
 
