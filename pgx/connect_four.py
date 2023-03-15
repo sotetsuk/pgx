8612eb9f53680c6de940e24a89539db3533c0e03
OkanoShinri <n.salt2000@gmail.com>
2023-03-15 20:00:52 +0900

    add env

--- pgx/connect_four.py
+++ pgx/connect_four.py
@@ -16,7 +16,7 @@ class State(pgx.State):
     reward: jnp.ndarray = jnp.float32([0.0, 0.0])
     terminated: jnp.ndarray = FALSE
     truncated: jnp.ndarray = FALSE
-    legal_action_mask: jnp.ndarray = jnp.ones(9, dtype=jnp.bool_)
+    legal_action_mask: jnp.ndarray = jnp.ones(7, dtype=jnp.bool_)
     _rng_key: jax.random.KeyArray = jax.random.PRNGKey(0)
     # ---
     turn: jnp.ndarray = jnp.int8(0)
@@ -28,3 +28,48 @@ class State(pgx.State):
     #  [28, 29, 30, 31, 32, 33, 34],
     #  [35, 36, 37, 38, 39, 40, 41]]
     board: jnp.ndarray = -jnp.ones(42, jnp.int8)  # -1 (empty), 0, 1
+
+
+class ConnectFour(pgx.Env):
+    def __init__(
+        self,
+        *,
+        auto_reset: bool = False,
+        max_truncation_steps: int = -1,
+    ):
+        super().__init__(
+            auto_reset=auto_reset, max_truncation_steps=max_truncation_steps
+        )
+
+    def _init(self, key: jax.random.KeyArray) -> State:
+        return init(key)
+
+    def _step(self, state: pgx.State, action: jnp.ndarray) -> State:
+        assert isinstance(state, State)
+        return step(state, action)
+
+    def _observe(
+        self, state: pgx.State, player_id: jnp.ndarray
+    ) -> jnp.ndarray:
+        assert isinstance(state, State)
+        return observe(state, player_id)
+
+    @property
+    def version(self) -> str:
+        return "beta"
+
+    @property
+    def num_players(self) -> int:
+        return 2
+
+
+def init(rng: jax.random.KeyArray) -> State:
+    ...
+
+
+def step(state: State, action: jnp.ndarray) -> State:
+    ...
+
+
+def observe(state: State, player_id: jnp.ndarray) -> jnp.ndarray:
+    ...
