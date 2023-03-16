e479758923f2d8e4c612d00b3493ca85a5e41769
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-16 14:25:18 +0900

    working

--- main.py
+++ main.py
@@ -10,39 +10,15 @@ import numpy as np
 from gymnasium import spaces
 from pettingzoo import AECEnv
 from IPython.display import display_svg  # type:ignore
-from pettingzoo.utils.agent_selector import agent_selector
+from pettingzoo.utils import agent_selector, wrappers
+from pettingzoo.test import api_test
 
-env = pgx.make("tic_tac_toe")
-env.init = jax.jit(env.init)
-env.step = jax.jit(env.step)
-
-# %%
-
-s = env.init(jax.random.PRNGKey(0))
-for i in range(5):
-    s = env.step(s, i)
-    display_svg(s._repr_html_(), raw=True)
-# %%
-
-
-class ToPettingZoo(AECEnv):
-    metadata = {
-        "render_modes": ["svg"],
-    }
-
-    def __init__(self, env: pgx.Env, render_mode=None):
-        super().__init__()
-        self.pgx_env: pgx.Env = env
-        env.init = jax.jit(env.init)
-        env.step = jax.jit(env.step)
-        self._state = env.init(jax.random.PRNGKey(0))
-
-        # TODO: fix me
-        self.agents = ["player_0", "player_1"]  # TODO: Tic-tac-toe dependent
-        self.possible_agents = self.agents[:]
-
-        self.action_spaces = {i: spaces.Discrete(9) for i in self.agents}  # TODO: Tic-tac-toe dependent
-        self.observation_spaces = {  # TODO: Tic-tac-toe dependent
+# %% 
+def get_agents_spaces(env_id: pgx.EnvId):
+    if env_id == "tic_tac_toe":
+        agents = ["player_0", "player_1"]
+        action_spaces = {i: spaces.Discrete(9) for i in agents}
+        observation_spaces = {
             i: spaces.Dict(
                 {
                     "observation": spaces.Box(
@@ -51,13 +27,51 @@ class ToPettingZoo(AECEnv):
                     "action_mask": spaces.Box(low=0, high=1, shape=(9,), dtype=np.int8),
                 }
             )
-            for i in self.agents
+            for i in agents
         }
+        return agents, action_spaces, observation_spaces
+    elif env_id == "go-19x19":
+        agents = ["player_0", "player_1"]
+        size = 19
+        action_spaces = {i: spaces.Discrete(size * size + 1) for i in agents}
+        observation_spaces = {
+            i: spaces.Dict(
+                {
+                    "observation": spaces.Box(
+                        low=0, high=1, shape=(size, size, 17), dtype=np.int8
+                    ),
+                    "action_mask": spaces.Box(low=0, high=1, shape=(size * size + 1,), dtype=np.int8),
+                }
+            )
+            for i in agents
+        }
+        return agents, action_spaces, observation_spaces
+    else:
+        assert False
+ 
+
+# %%
+
+class PettingZooEnv(AECEnv):
+    metadata = {
+        "render_modes": ["svg"],
+    }
+
+    def __init__(self, env_id: pgx.EnvId, render_mode=None):
+        super().__init__()
+        self.pgx_env: pgx.Env = pgx.make(env_id)
+        self.pgx_env.init = jax.jit(self.pgx_env.init)
+        self.pgx_env.step = jax.jit(self.pgx_env.step)
+        self._state = self.pgx_env.init(jax.random.PRNGKey(0))
+
+        self.agents, self.action_spaces, self.observation_spaces = get_agents_spaces(env_id)
+        self.possible_agents = self.agents[:]
 
         self.rewards = {i: 0 for i in self.agents}
         self.terminations = {i: False for i in self.agents}
         self.truncations = {i: False for i in self.agents}
-        self.infos = {i: {"legal_moves": list(range(0, 9))} for i in self.agents}
+        # self.infos = {i: {"legal_moves": list(range(0, 9))} for i in self.agents}  
+        self.infos = {a: {} for a in self.agents}
 
         self._agent_selector = agent_selector(self.agents)
         self.agent_selection = self._agent_selector.reset()
@@ -65,7 +79,10 @@ class ToPettingZoo(AECEnv):
 
     def observe(self, agent):
         # TODO: use agent
-        return {"observation": self._state.observation, "action_mask": self._state.legal_action_mask}
+        return {
+            "observation": np.array(self._state.observation),
+            "action_mask": np.array(self._state.legal_action_mask)
+        }
 
     def observation_space(self, agent):
         return self.observation_spaces[agent]
@@ -80,14 +97,14 @@ class ToPettingZoo(AECEnv):
         ):
             return self._was_dead_step(action)  # type: ignore
 
-        self._state = self.pgx_env.step(self._state, jnp.int32(action)) 
+        self._state = self.pgx_env.step(self._state, jnp.int32(action))
 
         next_agent = self._agent_selector.next()  # TODO: fix me
 
         if self._state.terminated:
             for i in range(self.pgx_env.num_players):
                 self.rewards[f"player_{i}"] = float(self._state.reward[i])
-            self.terminations =  {a: True for a in self.agents}
+            self.terminations = {a: True for a in self.agents}
 
         # Switch selection to next agents
         self._cumulative_rewards[self.agent_selection] = 0
@@ -124,21 +141,17 @@ class ToPettingZoo(AECEnv):
         pass
 
 # %%
+env = PettingZooEnv("tic_tac_toe", render_mode=None)
+env = wrappers.TerminateIllegalWrapper(env, illegal_reward=-1)
+env = wrappers.AssertOutOfBoundsWrapper(env)
+env = wrappers.OrderEnforcingWrapper(env)
+api_test(env, num_cycles=1000, verbose_progress=True)
 
-env = pgx.make("tic_tac_toe")
-pz = ToPettingZoo(env, render_mode="svg")
-
-pz.reset()
-obs, *_ = pz.last()
-# %%
-obs
-# %%
-pz.step(0)
-obs, *_ = pz.last()
-obs
 # %%
 
-pz.step()
-obs, *_ = pz.last()
-obs
+env = PettingZooEnv("tic_tac_toe", render_mode="svg")
+# %%
+env.reset()
+# %%
+env.step(5)
 # %%
