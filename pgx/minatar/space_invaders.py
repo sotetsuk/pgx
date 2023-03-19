eb3010375259aa099e4dda23e841ac7b2f9fe2f6
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-20 08:39:22 +0900

    .

--- pgx/minatar/space_invaders.py
+++ pgx/minatar/space_invaders.py
@@ -8,7 +8,7 @@ The authors of original MinAtar implementation are:
 The original MinAtar implementation is distributed under GNU General Public License v3.0
     * https://github.com/kenjyoung/MinAtar/blob/master/License.txt
 """
-from typing import Tuple, Optional, Literal
+from typing import Literal, Optional, Tuple
 
 import jax
 import jax.lax as lax
