6f6b593ec66aa101bb888ed894a4778893683cc7
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-20 17:25:18 +0900

    add test

--- tests/test_seaquest.py
+++ tests/test_seaquest.py
@@ -134,3 +134,63 @@ def test_api():
     import pgx
     env = pgx.make("minatar/seaquest")
     pgx.api_test(env)
+
+
+def test_buggy_sample():
+    state = seaquest.State(
+        oxygen=jnp.int32(187),
+        diver_count=jnp.int32(0),
+        sub_x=jnp.int32(0),
+        sub_y=jnp.int32(3),
+        sub_or=jnp.bool_(False),
+        f_bullets= -jnp.ones(
+            (5, 3), dtype=jnp.int32
+        ),  #.at[0, :].set(jnp.int32([6, 6, 0])),
+        e_bullets=-jnp.ones(
+            (25, 3), dtype=jnp.int32
+        ),
+        e_fish = (-jnp.ones(
+            (25, 4), dtype=jnp.int32
+        )).at[:2, :].set(
+            [[3, 8, 0, 2],
+             [0, 4, 1, 4]]
+        ),
+        e_subs = (-jnp.ones(
+            (25, 5), dtype=jnp.int32
+        )).at[:2, :].set(
+            jnp.int32(
+                [[9, 1, 1, 0, 6],
+                 [6, 6, 0, 3, 1]]
+            )
+        ),
+        divers = (-jnp.ones(
+            (5, 4), dtype=jnp.int32
+        )).at[:2, :].set(
+            [[3, 3, 0, 2],
+             [1, 7, 1, 2]]
+        ),
+        e_spawn_speed=jnp.int32(19),
+        e_spawn_timer=jnp.int32(18),
+        d_spawn_timer=jnp.int32(21),
+        move_speed=jnp.int32(5),
+        ramp_index=jnp.int32(1),
+        shot_timer=jnp.int32(0),
+        surface=jnp.bool_(False),
+        terminal=jnp.bool_(False),
+        last_action=jnp.int32(4)
+    )
+    state = _step_det(state, 0,
+                      enemy_lr=True,
+                      is_sub=False,
+                      enemy_y=4,
+                      diver_lr=True,
+                      diver_y=7
+                      )
+    # print(state.f_bullets)
+    print("e_bullets")
+    print(state.e_bullets)
+    # print(state.e_fish)
+    print("e_subs")
+    print(state.e_subs)
+    assert (state.e_bullets[0] == jnp.int32([-1, -1, -1])).all()
+    assert (state.e_subs[0] == jnp.int32([6, 6, 0, 2, 0])).all()
