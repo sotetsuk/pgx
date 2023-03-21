ecaead758edd88210056645c1dcbd118aac20b9c
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-22 06:10:00 +0900

    .

--- tests/test_shogi.py
+++ tests/test_shogi.py
@@ -565,7 +565,7 @@ def test_buggy_samples():
     visualize(state, "tests/assets/shogi/buggy_samples_007.svg")
     dlshogi_action = 202
     a = Action.from_dlshogi_action(state, dlshogi_action)
-    assert a.from_ == xy2i(5, 5)
+    assert a.from_ == xy2i(6, 6)
 
     state = _from_sfen("+PsGg1p2+P/+B1+Pgp+N1sp/1+N5l1/P3kP1pL/3P1r3/B2KP3L/4L1SP+s/+r2+p2pgP/2P2+n+p2 b")
     visualize(state, "tests/assets/shogi/buggy_samples_008.svg")
