eaa8cf0787b4d6e86e8fba4c792c29eb2a73e1bb
salt <72956592+OkanoShinri@users.noreply.github.com>
2023-03-22 03:00:21 +0900

    [2048] Add `2048` Env (#600)

--- pgx/_2048.py
+++ pgx/_2048.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import jax
 import jax.numpy as jnp
 
@@ -20,13 +21,14 @@ from pgx._flax.struct import dataclass
 
 FALSE = jnp.bool_(False)
 TRUE = jnp.bool_(True)
+ZERO = jnp.int8(0)
 
 
 @dataclass
 class State(core.State):
     current_player: jnp.ndarray = jnp.int8(0)
     observation: jnp.ndarray = jnp.zeros(16, dtype=jnp.bool_)
-    reward: jnp.ndarray = jnp.float32(0.0)
+    reward: jnp.ndarray = jnp.float32([0.0])
     terminated: jnp.ndarray = FALSE
     truncated: jnp.ndarray = FALSE
     legal_action_mask: jnp.ndarray = jnp.ones(4, dtype=jnp.bool_)
@@ -86,12 +88,148 @@ class Play2048(core.Env):
 
 
 def _init(rng: jax.random.KeyArray) -> State:
-    ...
+    rng1, rng2 = jax.random.split(rng)
+    board = _add_random_num(jnp.zeros((4, 4), jnp.int8), rng1)
+    board = _add_random_num(board, rng2)
+    return State(board=board.ravel())  # type:ignore
 
 
-def _step(state, action):
-    ...
+def _step(state: State, action):
+    """action: 0(left), 1(up), 2(right), 3(down)"""
+    board_2d = state.board.reshape((4, 4))
+    board_2d = jax.lax.switch(
+        action,
+        [
+            lambda: board_2d,
+            lambda: jnp.rot90(board_2d, 1),
+            lambda: jnp.rot90(board_2d, 2),
+            lambda: jnp.rot90(board_2d, 3),
+        ],
+    )
+    board_2d, reward = jax.vmap(_slide_and_merge)(board_2d)
+    board_2d = jax.lax.switch(
+        action,
+        [
+            lambda: board_2d,
+            lambda: jnp.rot90(board_2d, -1),
+            lambda: jnp.rot90(board_2d, -2),
+            lambda: jnp.rot90(board_2d, -3),
+        ],
+    )
+
+    _rng_key, sub_key = jax.random.split(state._rng_key)
+    board_2d = _add_random_num(board_2d, sub_key)
+
+    legal_action = jax.vmap(_can_slide_left)(
+        jnp.array(
+            [
+                board_2d,
+                jnp.rot90(board_2d, 1),
+                jnp.rot90(board_2d, 2),
+                jnp.rot90(board_2d, 3),
+            ]
+        )
+    )
+
+    return state.replace(  # type:ignore
+        _rng_key=_rng_key,
+        board=board_2d.ravel(),
+        reward=jnp.float32([reward.sum()]),
+        legal_action_mask=legal_action.ravel(),
+        terminated=~legal_action.any(),
+    )
 
 
 def _observe(state, player_id) -> jnp.ndarray:
     ...
+
+
+def _add_random_num(board_2d, key):
+    """Add 2 or 4 to the empty space on the board.
+    2 appears 90% of the time, and 4 appears 10% of the time.
+    cf. https://github.com/gabrielecirulli/2048/blob/master/js/game_manager.js#L71
+    """
+    key, sub_key = jax.random.split(key)
+    pos = jax.random.choice(key, jnp.arange(16), p=(board_2d.ravel() == 0))
+    set_num = jax.random.choice(
+        sub_key, jnp.int8([1, 2]), p=jnp.array([0.9, 0.1])
+    )
+    board_2d = board_2d.at[pos // 4, pos % 4].set(set_num)
+    return board_2d
+
+
+def _slide_and_merge(line):
+    """[2 2 2 2] -> [4 4 0 0]"""
+    line = _slide_left(line)
+    line, reward = _merge(line)
+    line = _slide_left(line)
+    return line, reward
+
+
+def _merge(line):
+    """[2 2 2 2] -> [4 0 4 0]"""
+    line, reward = jax.lax.cond(
+        (line[0] != 0) & (line[0] == line[1]),
+        lambda: (
+            line.at[0].set(line[0] + 1).at[1].set(ZERO),
+            2 ** (line[0] + 1),
+        ),
+        lambda: (line, ZERO),
+    )
+    line, reward = jax.lax.cond(
+        (line[1] != 0) & (line[1] == line[2]),
+        lambda: (
+            line.at[1].set(line[1] + 1).at[2].set(ZERO),
+            reward + 2 ** (line[1] + 1),
+        ),
+        lambda: (line, reward),
+    )
+    line, reward = jax.lax.cond(
+        (line[2] != 0) & (line[2] == line[3]),
+        lambda: (
+            line.at[2].set(line[2] + 1).at[3].set(ZERO),
+            reward + 2 ** (line[2] + 1),
+        ),
+        lambda: (line, reward),
+    )
+    return line, reward
+
+
+def _slide_left(line):
+    """[0 2 0 2] -> [2 2 0 0]"""
+    line = jax.lax.cond(
+        (line[2] == 0),
+        lambda: line.at[2:].set(jnp.roll(line[2:], -1)),
+        lambda: line,
+    )
+    line = jax.lax.cond(
+        (line[1] == 0),
+        lambda: line.at[1:].set(jnp.roll(line[1:], -1)),
+        lambda: line,
+    )
+    line = jax.lax.cond(
+        (line[0] == 0),
+        lambda: jnp.roll(line, -1),
+        lambda: line,
+    )
+    return line
+
+
+def _can_slide_left(board_2d):
+    def _can_slide(line):
+        """Judge if it can be moved to the left."""
+        can_slide = (line[:3] == 0).any()
+        can_slide |= (
+            (line[0] == line[1]) | (line[1] == line[2]) | (line[2] == line[3])
+        )
+        can_slide &= ~(line == 0).all()
+        return can_slide
+
+    can_slide = jax.vmap(_can_slide)(board_2d).any()
+    return can_slide
+
+
+# only for debug
+def show(state):
+    board = jnp.array([0 if i == 0 else 2**i for i in state.board])
+    print(board.reshape((4, 4)))
---
+++ tests/test_2048.py
@@ -0,0 +1,114 @@
+import jax
+import jax.numpy as jnp
+from pgx._2048 import Play2048, _slide_and_merge, State
+
+env = Play2048()
+init = jax.jit(env.init)
+step = jax.jit(env.step)
+observe = jax.jit(env.observe)
+slide_and_merge = jax.jit(_slide_and_merge)
+
+
+def test_init():
+    key = jax.random.PRNGKey(0)
+    state = init(key=key)
+    assert jnp.count_nonzero(state.board == 1) == 2
+
+
+def test_slide_and_merge():
+    line = jnp.int8([0, 2, 0, 2])
+    assert (slide_and_merge(line)[0] == jnp.int8([3, 0, 0, 0])).all()
+
+    line = jnp.int8([0, 2, 0, 1])
+    assert (slide_and_merge(line)[0] == jnp.int8([2, 1, 0, 0])).all()
+
+    line = jnp.int8([2, 2, 2, 2])
+    assert (slide_and_merge(line)[0] == jnp.int8([3, 3, 0, 0])).all()
+
+    line = jnp.int8([2, 0, 0, 2])
+    assert (slide_and_merge(line)[0] == jnp.int8([3, 0, 0, 0])).all()
+
+    line = jnp.int8([1, 4, 4, 5])
+    assert (slide_and_merge(line)[0] == jnp.int8([1, 5, 5, 0])).all()
+
+    board = jnp.int8([0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2])
+    board_2d = board.reshape((4, 4))
+    board_2d = jax.vmap(_slide_and_merge)(board_2d)[0]
+    board_1d = board_2d.ravel()
+    assert (
+        board_1d == jnp.int8([3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0])
+    ).all()
+
+
+def test_step():
+    key = jax.random.PRNGKey(0)
+    state = init(key)
+    """
+    [[0 0 0 0]
+     [0 0 2 0]
+     [0 0 0 0]
+     [0 0 2 0]]
+    """
+    assert (
+        state.board
+        == jnp.int8([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0])
+    ).all()
+
+    state = step(state, 3)  # down
+    """
+    [[0 0 0 0]
+     [0 0 0 0]
+     [0 0 0 2]
+     [0 0 4 0]]
+    """
+    assert (
+        state.board
+        == jnp.int8([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 2, 0])
+    ).all()
+
+
+def test_legal_action():
+    board = jnp.int8([0, 1, 2, 3, 2, 3, 4, 5, 3, 4, 5, 6, 4, 5, 6, 0])
+    state = State(board=board)
+    state = step(state, 0)
+    """
+    [[ 2  4  8  2]
+     [ 4  8 16 32]
+     [ 8 16 32 64]
+     [16 32 64  0]]
+    """
+    assert (state.legal_action_mask == jnp.bool_([0, 0, 1, 1])).all()
+    assert not state.terminated
+
+
+def test_terminated():
+    board = jnp.int8([1, 2, 3, 4, 2, 3, 4, 5, 3, 4, 5, 6, 4, 5, 6, 0])
+    state = State(board=board)
+    state = step(state, 0)
+    """
+    [[ 2  4  8 16]
+     [ 4  8 16 32]
+     [ 8 16 32 64]
+     [16 32 64  2]]
+    """
+    assert state.terminated
+
+
+def test_random_play():
+    key = jax.random.PRNGKey(0)
+    done = jnp.bool_(False)
+    key, sub_key = jax.random.split(key)
+    state = init(sub_key)
+    while not done:
+        legal_actions = jnp.where(state.legal_action_mask)[0]
+        key, sub_key = jax.random.split(key)
+        action = jax.random.choice(sub_key, legal_actions)
+        state = step(state, jnp.int16(action))
+        done = state.terminated
+
+
+# def test_api():
+#    import pgx
+#
+#    env = pgx.make("2048")
+#    pgx.api_test(env, 10)
