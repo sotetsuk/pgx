85ebadcf83baebe0bdf39bbe3be19e7380ccf2a2
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-22 14:48:33 +0900

    .

--- tests/test_shogi.py
+++ tests/test_shogi.py
@@ -571,6 +571,9 @@ def test_buggy_samples():
     visualize(state, "tests/assets/shogi/buggy_samples_008.svg")
     dlshogi_action = 225
     a = Action.from_dlshogi_action(state, dlshogi_action)
+    legal_moves = _effects_all(state)
+    print(_rotate(legal_moves[:, xy2i(8, 1)].reshape(9,9)))
+    assert legal_moves[xy2i(9, 2), xy2i(8, 1)]
     assert a.from_ == xy2i(9, 2)
     assert a.piece == HORSE
     expected_state = _from_sfen("+P+BGg1p2+P/2+Pgp+N1sp/1+N5l1/P3kP1pL/3P1r3/B2KP3L/4L1SP+s/+r2+p2pgP/2P2+n+p2 w Snp 1")
