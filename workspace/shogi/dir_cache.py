f9de5273be5ba02b0da1802958b09aff88994be2
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-23 00:56:09 +0900

    [Shogi] Refactor convert to direction (#630)

--- workspace/shogi/dir_cache.py
+++ workspace/shogi/dir_cache.py
@@ -20,45 +20,51 @@ from flax.serialization import to_bytes
 #  8 Up2 left
 #  9 Up2 right
 
-LEGAL_FROM_MASK = jnp.zeros((10, 81, 81), dtype=jnp.bool_)
+
+LEGAL_FROM_IDX = -jnp.ones((10, 81, 8), dtype=jnp.int32)
+
 
 for dir_ in tqdm(range(10)):
-    for from_ in tqdm(range(81), leave=False):
-        for to in range(81):
-            ok = False
-            x0, y0 = from_ // 9, from_ % 9
-            x1, y1 = to // 9, to % 9
-            dx = x1 - x0
-            dy = y1 - y0
-            if dir_ == 0:  # Up
-                ok = (dx == 0) and (dy < 0)
-            elif dir_ == 1:  # Up left
-                ok = (dx > 0) and (dy < 0) and (abs(dx) == abs(dy))
-            elif dir_ == 2:  # Up right
-                ok = (dx < 0) and (dy < 0) and (dx == dy)
-            elif dir_ == 3:  # Left
-                ok = (dx > 0) and (dy == 0)
-            elif dir_ == 4:  # Right
-                ok = (dx < 0) and (dy == 0)
-            elif dir_ == 5:  # Down
-                ok = (dx == 0) and (dy > 0)
-            elif dir_ == 6:  # Down left
-                ok = (dx > 0) and (dy > 0) and (dx == dy)
-            elif dir_ == 7:  # Down right
-                ok = (dx < 0) and (dy > 0) and (abs(dx) == abs(dy))
-            elif dir_ == 8:  # Up2 left
-                ok = (dx == 1) and (dy == -2)
-            elif dir_ == 9:  # Up2 right
-                ok = (dx == -1) and (dy == -2)
-            LEGAL_FROM_MASK = LEGAL_FROM_MASK.at[dir_, to, from_].set(ok)
+    for to in range(81):
+        x, y = to // 9, to % 9
+        if dir_ == 0:  # Up
+            dx, dy = 0, +1
+        elif dir_ == 1:  # Up left
+            dx, dy = -1, +1
+        elif dir_ == 2:  # Up right
+            dx, dy = +1, +1
+        elif dir_ == 3:  # Left
+            dx, dy = -1, 0
+        elif dir_ == 4:  # Right
+            dx, dy = +1, 0
+        elif dir_ == 5:  # Down
+            dx, dy = 0, -1
+        elif dir_ == 6:  # Down left
+            dx, dy = -1, -1
+        elif dir_ == 7:  # Down right
+            dx, dy = +1, -1
+        elif dir_ == 8:  # Up2 left
+            dx, dy = -1, +2
+        elif dir_ == 9:  # Up2 right
+            dx, dy = +1, +2
+        for i in range(8):
+            x += dx
+            y += dy
+            if x < 0 or 8 < x or y < 0 or 8 < y:
+                break
+            LEGAL_FROM_IDX = LEGAL_FROM_IDX.at[dir_, to, i].set(x * 9 + y)
+            if dir_ == 8 or dir_ == 9:
+                break
+
+# print(jnp.rot90(jnp.arange(81).reshape(9, 9), k=3))
 
 
 # check
 # to = 40
 # for dir_ in tqdm(range(10)):
 #     print(dir_)
-#     print(jnp.rot90(LEGAL_FROM_MASK[dir_, to].reshape(9, 9), k=3))
+#     print(LEGAL_FROM_IDX[dir_, to, :])
 
 
-print(f"LEGAL_FROM_MASK_BYTES = {to_bytes(LEGAL_FROM_MASK)}")
+print(f"BYTES = {to_bytes(LEGAL_FROM_IDX)}")
 
