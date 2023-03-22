dbbe1160055329eac6088649d24011ccbae9ea54
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-22 17:09:04 +0900

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pgx",
-    version="0.1.5",
+    version="0.1.6",
     long_description_content_type="text/markdown",
     description="",
     url="",
