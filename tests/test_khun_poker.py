51cfde7f40eea89f24f93f0712a234509b449533
OkanoShinri <n.salt2000@gmail.com>
2023-03-24 00:34:40 +0900

    add test

--- tests/test_khun_poker.py
+++ tests/test_khun_poker.py
@@ -20,35 +20,57 @@ def test_step():
     # cards = [2, 0]
     state = init(key)
     state = step(state, CHECK)
+    assert not state.terminated
     state = step(state, CHECK)
     assert state.terminated
     assert (state.reward == jnp.float32([1, -1])).all()
 
     state = init(key)
     state = step(state, CHECK)
+    assert not state.terminated
     state = step(state, BET)
+    assert not state.terminated
     state = step(state, FOLD)
     assert state.terminated
     assert (state.reward == jnp.float32([-1, 1])).all()
 
     state = init(key)
     state = step(state, CHECK)
+    assert not state.terminated
     state = step(state, BET)
+    assert not state.terminated
     state = step(state, CALL)
     assert state.terminated
     assert (state.reward == jnp.float32([2, -2])).all()
 
     state = init(key)
     state = step(state, BET)
+    assert not state.terminated
     state = step(state, FOLD)
     assert state.terminated
     assert (state.reward == jnp.float32([1, -1])).all()
 
     state = init(key)
     state = step(state, BET)
+    assert not state.terminated
     state = step(state, CALL)
     assert state.terminated
     assert (state.reward == jnp.float32([2, -2])).all()
+
+
+def test_random_play():
+    N = 100
+    key = jax.random.PRNGKey(0)
+    for _ in range(N):
+        done = jnp.bool_(False)
+        key, sub_key = jax.random.split(key)
+        state = init(sub_key)
+        while not done:
+            legal_actions = jnp.where(state.legal_action_mask)[0]
+            key, sub_key = jax.random.split(key)
+            action = jax.random.choice(sub_key, legal_actions)
+            state = step(state, action)
+            done = state.terminated
 
 
 # def test_api():
