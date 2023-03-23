0c7b04d1e6f8a71e597c3f6db5a722d30a2384ae
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-24 05:05:00 +0900

    .

--- pgx/_shogi.py
+++ pgx/_shogi.py
@@ -526,8 +526,7 @@ def _observe(state: State, player_id: jnp.ndarray) -> jnp.ndarray:
     my_hand_feat = hand_feat(state.hand[0])
     opp_hand_feat = hand_feat(state.hand[1])
 
-    checked = FALSE
-    checked = jnp.tile(FALSE, reps=(1, 9, 9))
+    checked = jnp.tile(is_checked(state.piece_board), reps=(1, 9, 9))
 
     feat1 = [
         my_piece_feat.reshape(14, 9, 9),
