1f40622b27abcdbb634009bf766557a99bca0263
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-25 21:22:53 +0900

    .

--- pgx/_shogi_utils.py
+++ pgx/_shogi_utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import jax
 import jax.numpy as jnp
 
 from pgx._cache import load_shogi_is_on_the_way  # type: ignore
@@ -40,6 +41,13 @@ BETWEEN = load_shogi_is_on_the_way()  # bool (5, 81, 81, 81)
 # Used for computing dlshogi action
 LEGAL_FROM_IDX = load_shogi_legal_from_idx()  # (10, 81, 8)
 
+@jax.jit
+@jax.vmap
+def can_move_any_ix(from_):
+    return jnp.nonzero(CAN_MOVE[:, from_, :].any(axis=0), size=34, fill_value=-1)[0]
+
+
+CAN_MOVE_ANY = can_move_any_ix(jnp.arange(81))  # (81, 34)
 
 def _to_sfen(state):
     """Convert state into sfen expression.
