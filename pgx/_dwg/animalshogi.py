b9edf6d9e550dafca400a4f0cfbf2b3154c6493a
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-31 02:46:05 +0900

    rm old implementation and fix visualization

--- pgx/_dwg/animalshogi.py
+++ pgx/_dwg/animalshogi.py
@@ -75,72 +75,104 @@ def _make_animalshogi_dwg(dwg, state: AnimalShogiState, config: dict):
     # pieces
     p1_pieces_g = dwg.g()
     p2_pieces_g = dwg.g()
-    for i, piece_pos, piece_type in zip(
-        range(10),
-        state.board[1:11],
-        ["P", "R", "B", "K", "G", "P", "R", "B", "K", "G"],
-    ):
-        for xy, is_set in enumerate(piece_pos):
-            if is_set == 1:
-                if i < 5:
-                    pieces_g = p1_pieces_g
-                    x = 2 - xy // BOARD_HEIGHT  # AnimalShogiStateは右上原点
-                    y = xy % BOARD_HEIGHT
-                    fill_color = color_set.p1_color
-                    stroke = color_set.p1_outline
-                else:
-                    pieces_g = p2_pieces_g
-                    x = xy // BOARD_HEIGHT
-                    y = 3 - xy % BOARD_HEIGHT
-                    fill_color = color_set.p2_color
-                    stroke = color_set.p2_outline
 
-                pieces_g.add(
-                    dwg.rect(
-                        insert=(
-                            (x + 0.1) * GRID_SIZE,
-                            (y + 0.1) * GRID_SIZE,
-                        ),
-                        size=(
-                            0.8 * GRID_SIZE,
-                            0.8 * GRID_SIZE,
-                        ),
-                        rx="3px",
-                        ry="3px",
-                        stroke=stroke,
-                        fill=fill_color,
-                    )
+    # -1: EMPTY
+    #  0: PAWN
+    #  1: ROOK
+    #  2: BISHOP
+    #  3: KING
+    #  4: GOLD
+    #  5: OPP_PAWN
+    #  6: OPP_ROOK
+    #  7: OPP_BISHOP
+    #  8: OPP_KING
+    #  9: OPP_GOLD
+    for xy in range(12):
+        piece_type = "EMPTY"
+        n = state.board[xy]
+        if n in (0, 5):
+            piece_type = "P"
+        if n in (1, 6):
+            piece_type = "R"
+        if n in (2, 7):
+            piece_type = "B"
+        if n in (3, 8):
+            piece_type = "K"
+        if n in (4, 9):
+            piece_type = "G"
+
+        if piece_type == "EMPTY":
+            continue
+
+        if state.turn == 0:
+            is_black = 0 <= n < 5
+        else:
+            is_black = 5 <= n
+
+        if is_black < 5:
+            pieces_g = p1_pieces_g
+            x = 2 - xy // BOARD_HEIGHT  # AnimalShogiStateは右上原点
+            y = xy % BOARD_HEIGHT
+            fill_color = color_set.p1_color
+            stroke = color_set.p1_outline
+        else:
+            pieces_g = p2_pieces_g
+            x = xy // BOARD_HEIGHT
+            y = 3 - xy % BOARD_HEIGHT
+            fill_color = color_set.p2_color
+            stroke = color_set.p2_outline
+
+        pieces_g.add(
+            dwg.rect(
+                insert=(
+                    (x + 0.1) * GRID_SIZE,
+                    (y + 0.1) * GRID_SIZE,
+                ),
+                size=(
+                    0.8 * GRID_SIZE,
+                    0.8 * GRID_SIZE,
+                ),
+                rx="3px",
+                ry="3px",
+                stroke=stroke,
+                fill=fill_color,
+            )
+        )
+        pieces_g.add(
+            dwg.text(
+                text=piece_type,
+                insert=(
+                    (x + 0.27) * GRID_SIZE,
+                    (y + 0.72) * GRID_SIZE,
+                ),
+                fill=stroke,
+                font_size="46px",
+                font_family="Courier",
+            )
+        )
+        # 移動可能方向
+        for _x, _y in MOVE[piece_type]:
+            pieces_g.add(
+                dwg.circle(
+                    center=(
+                        (x + 0.5 + _x * 0.35) * GRID_SIZE,
+                        (y + 0.5 + _y * 0.35) * GRID_SIZE,
+                    ),
+                    r=GRID_SIZE * 0.01,
+                    stroke=stroke,
+                    fill=stroke,
                 )
-                pieces_g.add(
-                    dwg.text(
-                        text=piece_type,
-                        insert=(
-                            (x + 0.27) * GRID_SIZE,
-                            (y + 0.72) * GRID_SIZE,
-                        ),
-                        fill=stroke,
-                        font_size="46px",
-                        font_family="Courier",
-                    )
-                )
-                # 移動可能方向
-                for _x, _y in MOVE[piece_type]:
-                    pieces_g.add(
-                        dwg.circle(
-                            center=(
-                                (x + 0.5 + _x * 0.35) * GRID_SIZE,
-                                (y + 0.5 + _y * 0.35) * GRID_SIZE,
-                            ),
-                            r=GRID_SIZE * 0.01,
-                            stroke=stroke,
-                            fill=stroke,
-                        )
-                    )
-    # hand
+            )
+
+    # # hand
     for i, piece_num, piece_type in zip(
-        range(6), state.hand, ["P", "R", "B", "P", "R", "B"]
+        range(6), state.hand.flatten(), ["P", "R", "B", "P", "R", "B"]
     ):
-        _g = p1_pieces_g if i < 3 else p2_pieces_g
+        if state.turn == 0:
+            is_black = i < 3
+        else:
+            is_black = 3 <= i
+        _g = p1_pieces_g if is_black else p2_pieces_g
         _g.add(
             dwg.text(
                 text=f"{piece_type}:{piece_num}",
