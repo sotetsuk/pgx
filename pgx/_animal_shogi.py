9bd84e00d17365106616371cfea6bb532c049ebe
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-03-31 14:02:53 +0900

    fmt

--- pgx/_animal_shogi.py
+++ pgx/_animal_shogi.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from functools import partial
-from typing import Tuple
-
 import jax
 import jax.numpy as jnp
 
