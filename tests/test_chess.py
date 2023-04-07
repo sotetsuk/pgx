31184bd5924ee2cb2d7d39169a62a89179928e1f
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-07 16:58:22 +0900

    .

--- tests/test_chess.py
+++ tests/test_chess.py
@@ -200,5 +200,14 @@ def test_legal_action_mask():
     state = State._from_fen("7k/8/8/8/p7/8/P7/K7 b - - 0 1")
     state.save_svg("tests/assets/chess/legal_action_mask_004.svg")
     state = step(state, jnp.int32(4104))  # BKing: h8 -> h7
-    print(jnp.nonzero(state.legal_action_mask))
+    state.save_svg("tests/assets/chess/legal_action_mask_005.svg")
     assert state.legal_action_mask.sum() == 3
+
+    state = State._from_fen("7k/8/8/8/8/P7/8/K7 b - - 0 1")
+    state.save_svg("tests/assets/chess/legal_action_mask_006.svg")
+    state = step(state, jnp.int32(4104))  # h8 -> h7
+    state.save_svg("tests/assets/chess/legal_action_mask_007.svg")
+    print(CAN_MOVE[1, 2])
+    print(jnp.nonzero(state.legal_action_mask))
+    assert state.legal_action_mask.sum() == 4
+
