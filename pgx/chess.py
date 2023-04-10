e5bd30d0114e1de7155af24781c3172385c10e00
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-10 17:45:28 +0900

    .

--- pgx/chess.py
+++ pgx/chess.py
@@ -331,6 +331,7 @@ def _legal_action_mask(state):
     actions = legal_actions(jnp.arange(64)).flatten()  # include -1
     # +1 is to avoid setting True to the last element
     mask = jnp.zeros(64 * 73 + 1, dtype=jnp.bool_)
+    # TODO: promotion
     return mask.at[actions].set(TRUE)[:-1]
 
 
