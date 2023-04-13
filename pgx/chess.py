e37a9f3e655ad616f2b7f4745ef4242f1b919053
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-13 17:57:07 +0900

    .

--- pgx/chess.py
+++ pgx/chess.py
@@ -427,7 +427,9 @@ def _legal_action_mask(state):
         @jax.vmap
         def legal_labels(from_):
             ok = (
-                (from_ >= 0) & (from_ < 64) & (to >= 0)
+                (from_ >= 0)
+                & (from_ < 64)
+                & (to >= 0)
                 & (state.board[from_] == PAWN)
                 & (state.board[to - 1] == -PAWN)
             )
