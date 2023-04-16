c26b8b72d4d8f162d9b9e3140de76a8b174d5438
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-16 10:15:33 +0900

    [Tidy] Fix format CI (#748)

--- pgx/__init__.py
+++ pgx/__init__.py
@@ -1,5 +1,9 @@
 from pgx._test import api_test
-from pgx._visualizer import set_visualization_config, save_svg, save_svg_animation
+from pgx._visualizer import (
+    save_svg,
+    save_svg_animation,
+    set_visualization_config,
+)
 from pgx.core import Env, EnvId, State, available_games, make
 
 __all__ = [
