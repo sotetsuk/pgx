8b50e21d36c5de3a67c69135b05d839e32b963a2
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-20 11:49:06 +0900

    .

--- pgx/_test.py
+++ pgx/_test.py
@@ -121,7 +121,7 @@ def _validate_taking_action_after_terminal(state: State, step_fn):
     state = step_fn(state, action)
     assert (state.reward == 0).all()
     for field in fields(state):
-        if field.name in ["reward", "steps"]:
+        if field.name in ["reward", "steps", "_info"]:
             continue
         assert (
             getattr(state, field.name) == getattr(prev_state, field.name)
