f8d700c491d25811d016c5261e181f002d364570
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-22 14:16:36 +0900

    rm shogi init legal action mask

--- pgx/_src/shogi_utils.py
+++ pgx/_src/shogi_utils.py
@@ -15,7 +15,6 @@
 import jax
 import jax.numpy as jnp
 
-from pgx._src.cache import load_shogi_init_legal_action_mask  # type: ignore
 from pgx._src.cache import load_shogi_is_on_the_way  # type: ignore
 from pgx._src.cache import load_shogi_legal_from_idx  # type: ignore
 from pgx._src.cache import load_shogi_raw_effect_boards  # type: ignore
@@ -80,7 +79,14 @@ BETWEEN_IX = jax.jit(
 
 CAN_MOVE_ANY = can_move_any_ix(jnp.arange(81))  # (81, 36)
 
-INIT_LEGAL_ACTION_MASK = load_shogi_init_legal_action_mask()
+INIT_LEGAL_ACTION_MASK = jnp.zeros(81 * 27, dtype=jnp.bool_)
+# fmt: off
+ixs = [5, 7, 14, 23, 25, 32, 34, 41, 43, 50, 52, 59, 61, 68, 77, 79, 115, 124, 133, 142, 187, 196, 205, 214, 268, 277, 286, 295, 304, 331]
+# fmt: on
+for ix in ixs:
+    INIT_LEGAL_ACTION_MASK = INIT_LEGAL_ACTION_MASK.at[ix].set(True)
+assert INIT_LEGAL_ACTION_MASK.shape == (81 * 27,)
+assert INIT_LEGAL_ACTION_MASK.sum() == 30
 
 
 def _around(c):
