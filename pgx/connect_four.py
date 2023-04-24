4632390afdeff0d221b0ffe6da8370b6204d0f7b
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-24 14:40:37 +0900

    connect four

--- pgx/connect_four.py
+++ pgx/connect_four.py
@@ -33,7 +33,7 @@ class State(v1.State):
     _rng_key: jax.random.KeyArray = jax.random.PRNGKey(0)
     _step_count: jnp.ndarray = jnp.int32(0)
     # --- Connect Four specific ---
-    turn: jnp.ndarray = jnp.int8(0)
+    _turn: jnp.ndarray = jnp.int8(0)
     # 6x7 board
     # [[ 0,  1,  2,  3,  4,  5,  6],
     #  [ 7,  8,  9, 10, 11, 12, 13],
@@ -41,8 +41,8 @@ class State(v1.State):
     #  [21, 22, 23, 24, 25, 26, 27],
     #  [28, 29, 30, 31, 32, 33, 34],
     #  [35, 36, 37, 38, 39, 40, 41]]
-    board: jnp.ndarray = -jnp.ones(42, jnp.int8)  # -1 (empty), 0, 1
-    blank_row: jnp.ndarray = jnp.full(7, 5)
+    _board: jnp.ndarray = -jnp.ones(42, jnp.int8)  # -1 (empty), 0, 1
+    _blank_row: jnp.ndarray = jnp.full(7, 5)
 
     @property
     def env_id(self) -> v1.EnvId:
@@ -114,11 +114,11 @@ def _init(rng: jax.random.KeyArray) -> State:
 
 
 def _step(state: State, action: jnp.ndarray) -> State:
-    board = state.board
-    row = state.blank_row[action]
-    blank_row = state.blank_row.at[action].set(row - 1)
-    board = board.at[_to_idx(row, action)].set(state.turn)
-    won = _win_check(board, state.turn)
+    board = state._board
+    row = state._blank_row[action]
+    blank_row = state._blank_row.at[action].set(row - 1)
+    board = board.at[_to_idx(row, action)].set(state._turn)
+    won = _win_check(board, state._turn)
     reward = jax.lax.cond(
         won,
         lambda: jnp.float32([-1, -1]).at[state.current_player].set(1),
@@ -127,9 +127,9 @@ def _step(state: State, action: jnp.ndarray) -> State:
     return state.replace(  # type: ignore
         current_player=1 - state.current_player,
         legal_action_mask=blank_row >= 0,
-        turn=1 - state.turn,
-        board=board,
-        blank_row=blank_row,
+        _turn=1 - state._turn,
+        _board=board,
+        _blank_row=blank_row,
         terminated=won | jnp.all(blank_row == -1),
         reward=reward,
     )
@@ -144,7 +144,7 @@ def _win_check(board, turn) -> jnp.ndarray:
 
 
 def _observe(state: State, player_id: jnp.ndarray) -> jnp.ndarray:
-    turns = jnp.int8([state.turn, 1 - state.turn])
+    turns = jnp.int8([state._turn, 1 - state._turn])
     turns = jax.lax.cond(
         player_id == state.current_player,
         lambda: turns,
@@ -152,6 +152,6 @@ def _observe(state: State, player_id: jnp.ndarray) -> jnp.ndarray:
     )
 
     def make(turn):
-        return state.board.reshape(6, 7) == turn
+        return state._board.reshape(6, 7) == turn
 
     return jnp.stack(jax.vmap(make)(turns), -1)
