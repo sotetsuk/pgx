a04d1782910b1e048ec500dcd67a30a81d2fe0f2
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-25 21:06:19 +0900

    fix terminal

--- pgx/minatar/breakout.py
+++ pgx/minatar/breakout.py
@@ -91,10 +91,9 @@ class MinAtarBreakout(v1.Env):
 
     def _step(self, state: v1.State, action) -> State:
         assert isinstance(state, State)
-        state = _step(
+        return _step(
             state, action, sticky_action_prob=self.sticky_action_prob
         )
-        return state.replace(terminated=state._terminal)  # type: ignore
 
     def _observe(self, state: v1.State, player_id: jnp.ndarray) -> jnp.ndarray:
         assert isinstance(state, State)
@@ -206,6 +205,7 @@ def _step_det_at_non_terminal(state: State, action: jnp.ndarray):
         _terminal=terminal,
         _last_action=action,
         reward=r[jnp.newaxis],
+        terminated=terminal,
     )
     return state
 
