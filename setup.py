7c1d29e57f386e5890af9ff2d19bf0ca1710380b
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-27 15:44:58 +0900

    fix assets

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email="sotetsu.koyamada@gmail.com",
     keywords="",
     packages=find_packages(),
-    package_data={"": ["LICENSE", "*.svg"]},
+    package_data={"": ["LICENSE", "*.svg", "_src/assets/*.npy"]},
     include_package_data=True,
     install_requires=[
         "jax>=0.3.25",  # JAX version on Colab (TPU)
