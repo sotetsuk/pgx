8e05ce153a8f918e767f50e9cab787f54792181c
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-27 13:29:43 +0900

    fix script

--- fig/generate_chess_svg_animation.py
+++ fig/generate_chess_svg_animation.py
@@ -1,26 +1,25 @@
+import sys
 import jax
-
 import pgx
 from pgx.experimental.utils import act_randomly
 
-env = pgx.make("chess")
-init = jax.jit(jax.vmap(env.init))
-step = jax.jit(jax.vmap(env.step))
+env_id: pgx.EnvId = sys.argv[1]
+env = pgx.make(env_id, auto_reset=True)
+init = jax.jit(env.init)
+step = jax.jit(env.step)
 
-batch_size = 4
 rng = jax.random.PRNGKey(9999)
 
 states = []
 rng, subkey = jax.random.split(rng)
-keys = jax.random.split(subkey, batch_size)
-state = init(keys)
+state = init(subkey)
 states.append(state)
-while not state.terminated.all():
+# while not state.terminated.all():
+for _ in range(18):
     rng, subkey = jax.random.split(rng)
     action = act_randomly(subkey, state)
     state = step(state, action)
     states.append(state)
 
-pgx.save_svg_animation(states, "chess.svg", frame_duration_seconds=0.1)
-pgx.save_svg(states[0], "chess_initial.svg")
-pgx.save_svg(states[-1], "chess_final.svg")
+pgx.save_svg_animation(states, f"{env_id}_light.svg", frame_duration_seconds=0.3)
+pgx.save_svg_animation(states, f"{env_id}_dark.svg", frame_duration_seconds=0.3, color_theme="dark")
