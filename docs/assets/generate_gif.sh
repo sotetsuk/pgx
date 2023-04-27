e7e29d182572104c4b7b43f3959f6e90e9575e5f
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-04-27 13:47:32 +0900

    .

--- docs/assets/generate_gif.sh
+++ docs/assets/generate_gif.sh
@@ -8,3 +8,11 @@ cd tmp
 inkscape --export-type=png *.svg
 convert *.png "$1"_light.gif
 mv "$1"_light.gif ../
+
+# dark theme
+rm -rf tmp
+python3 generate_gif.py $1 dark
+cd tmp
+inkscape --export-type=png *.svg
+convert *.png "$1"_dark.gif
+mv "$1"_dark.gif ../
