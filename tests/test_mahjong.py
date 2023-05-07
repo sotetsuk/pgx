ce5cd3ffcba7eb1f96b6a86f7d466dd2fe8bf60f
OkanoShinri <n.salt2000@gmail.com>
2023-05-07 17:54:47 +0900

    passing tests

--- tests/test_mahjong.py
+++ tests/test_mahjong.py
@@ -2,7 +2,7 @@ from pgx._mahjong._hand import Hand
 from pgx._mahjong._yaku import Yaku
 from pgx._mahjong._shanten import Shanten
 import jax.numpy as jnp
-
+from jax import jit
 
 def test_ron():
     # fmt:off
@@ -14,8 +14,8 @@ def test_ron():
     ])
     # fmt:on
 
-    assert Hand.can_ron(hand, 0)
-    assert Hand._can_tsumo(Hand.add(hand, 0))
+    assert jit(Hand.can_ron)(hand, 0)
+    #assert jit(Hand._can_tsumo(Hand.add(hand, 0)))
     assert ~Hand.can_ron(hand, 1)
     assert ~Hand._can_tsumo(Hand.add(hand, 1))
 
