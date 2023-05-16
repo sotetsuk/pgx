77d399883d0a16adcadc6ad17eff810eb4448555
youyou-ku <youyou5016ku@gmail.com>
2023-05-16 20:37:19 +0900

    del ep, castling

--- pgx/_gardner_chess.py
+++ pgx/_gardner_chess.py
@@ -31,8 +31,6 @@ class State(v1.State):
     # --- Chess specific ---
     _turn: jnp.ndarray = jnp.int8(0)
     _board: jnp.ndarray = INIT_BOARD  # 左上からFENと同じ形式で埋めていく
-    _can_castle_queen_side: jnp.ndarray = jnp.ones(2, dtype=jnp.bool_)
-    _en_passant: jnp.ndarray = jnp.int8(-1)  # En passant target. Flips.
     # # of moves since the last piece capture or pawn move
     _halfmove_count: jnp.ndarray = jnp.int32(0)
     _fullmove_count: jnp.ndarray = jnp.int32(1)  # increase every black move
@@ -75,22 +73,20 @@ def _rotate(board):
 def _from_fen(fen: str):
     """Restore state from FEN
 
-    >>> state = _from_fen("rnbqk/ppppp/5/PPPPP/RNBQK w Qq - 0 1")
+    >>> state = _from_fen("rnbqk/ppppp/5/PPPPP/RNBQK w - - 0 1")
     >>> _rotate(state._board.reshape(5, 5))
     Array([[-4, -2, -3, -5, -6],
            [-1, -1, -1, -1, -1],
            [ 0,  0,  0,  0,  0],
            [ 1,  1,  1,  1,  1],
            [ 4,  2,  3,  5,  6]], dtype=int8)
-    >>> state = _from_fen("rnbqk/Ppppp/5/1PPPP/RNBQK b Qq a3 0 1")
+    >>> state = _from_fen("bbkqr/Ppppp/5/1PPP1/RNBQK b - - 0 1")
     >>> _rotate(state._board.reshape(5, 5))
-    Array([[-4, -2, -3, -5, -6],
+    Array([[-3, -3, -6, -5, -4],
            [ 1, -1, -1, -1, -1],
            [ 0,  0,  0,  0,  0],
-           [ 0,  1,  1,  1,  1],
+           [ 0,  1,  1,  1,  0],
            [ 4,  2,  3,  5,  6]], dtype=int8)
-    >>> state._en_passant
-    Array(2, dtype=int8)
     """
     board, turn, castling, en_passant, halfmove_cnt, fullmove_cnt = fen.split()
     arr = []
@@ -104,30 +100,10 @@ def _from_fen(fen: str):
                 if str.islower(c):
                     ix *= -1
                 arr.append(ix)
-    can_castle_queen_side = jnp.zeros(2, dtype=jnp.bool_)
-    if "Q" in castling:
-        can_castle_queen_side = can_castle_queen_side.at[0].set(TRUE)
-    if "q" in castling:
-        can_castle_queen_side = can_castle_queen_side.at[1].set(TRUE)
-    if turn == "b":
-        can_castle_queen_side = can_castle_queen_side[::-1]
     mat = jnp.int8(arr).reshape(5, 5)
-    if turn == "b":
-        mat = -jnp.flip(mat, axis=0)
-    ep = (
-        jnp.int8(-1)
-        if en_passant == "-"
-        else jnp.int8(
-            "abcde".index(en_passant[0]) * 5 + int(en_passant[1]) - 1
-        )
-    )
-    if turn == "b" and ep >= 0:
-        ep = _flip_pos(ep)
     state = State(  # type: ignore
         _board=jnp.rot90(mat, k=3).flatten(),
         _turn=jnp.int8(0) if turn == "w" else jnp.int8(1),
-        _can_castle_queen_side=can_castle_queen_side,
-        _en_passant=ep,
         _halfmove_count=jnp.int32(halfmove_cnt),
         _fullmove_count=jnp.int32(fullmove_cnt),
     )
@@ -154,11 +130,11 @@ def _to_fen(state: State):
     - アンパッサン可能な位置。ポーンが2マス動いた場合はそのポーンが通過した位置を記録
     - 最後にポーンの移動および駒取りが発生してからの手数と通常の手数
 
-    >>> s = State(_en_passant=jnp.int8(12))
+    >>> s = State()
     >>> _to_fen(s)
-    'rnbqk/ppppp/5/PPPPP/RNBQK w Qq c3 0 1'
-    >>> _to_fen(_from_fen("rnbqk/ppppp/P4/1PPPP/RNBQK b Qq d3 0 1"))
-    'rnbqk/ppppp/P4/1PPPP/RNBQK b Qq d3 0 1'
+    'rnbqk/ppppp/5/PPPPP/RNBQK w - - 0 1'
+    >>> _to_fen(_from_fen("bbkqr/Ppppp/5/1PPP1/RNBQK b - - 0 1"))
+    'bbkqr/Ppppp/5/1PPP1/RNBQK b - - 0 1'
     """
     pb = jnp.rot90(state._board.reshape(5, 5), k=1)
     if state._turn == 1:
@@ -187,29 +163,8 @@ def _to_fen(state: State):
             fen += " "
     # 手番
     fen += "w " if state._turn == 0 else "b "
-    # キャスリング
-    can_castle_queen_side = state._can_castle_queen_side
-    if state._turn == 1:
-        can_castle_queen_side = can_castle_queen_side[::-1]
-    if not (can_castle_queen_side.any()):
-        fen += "-"
-    else:
-        if can_castle_queen_side[0]:
-            fen += "Q"
-        if can_castle_queen_side[1]:
-            fen += "q"
-    fen += " "
-    # アンパッサン
-    en_passant = state._en_passant
-    if state._turn == 1:
-        en_passant = _flip_pos(en_passant)
-    ep = int(en_passant.item())
-    if ep == -1:
-        fen += "-"
-    else:
-        fen += "abcde"[ep // 5]
-        fen += str(ep % 5 + 1)
-    fen += " "
+    # キャスリング、アンパッサン(なし)
+    fen += "- - "
     fen += str(state._halfmove_count.item())
     fen += " "
     fen += str(state._fullmove_count.item())
