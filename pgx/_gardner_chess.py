8f3fa27de33d5b4a4fc3accea473b06df798696e
youyou-ku <youyou5016ku@gmail.com>
2023-05-17 21:07:49 +0900

    fix

--- pgx/_gardner_chess.py
+++ pgx/_gardner_chess.py
@@ -109,7 +109,7 @@ class Action:
     def _to_label(self):
         plane = PLANE_MAP[self.from_, self.to]
         # plane = jax.lax.select(self.underpromotion >= 0, ..., plane)
-        return jnp.int32(self.from_) * 73 + jnp.int32(plane)
+        return jnp.int32(self.from_) * 49 + jnp.int32(plane)
 
 
 def _flip_pos(x):
