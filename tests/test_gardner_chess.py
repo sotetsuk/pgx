7679c1ced030eddf8a9f51155ba9e286f9966f7d
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-05-17 18:38:44 +0900

    .

--- tests/test_gardner_chess.py
+++ tests/test_gardner_chess.py
@@ -8,14 +8,14 @@ pgx.set_visualization_config(color_theme="dark")
 
 def p(s: str, b=False):
     """
-    >>> p("e3")
+    >>> p("e1")
     20
-    >>> p("e3", b=True)
+    >>> p("e1", b=True)
     24
     """
     x = "abcde".index(s[0])
     offset = int(s[1]) - 1 if not b else 5 - int(s[1])
-    return x * 8 + offset
+    return x * 5 + offset
 
 
 def test_action():
