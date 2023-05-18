247c548e064751468e8cbd767dae2c72e78013db
nissymori <kyoudai.630@gmail.com>
2023-05-18 18:20:37 +0900

    inig

--- pgx/backgammon.py
+++ pgx/backgammon.py
@@ -125,6 +125,7 @@ def _step(state: State, action: jnp.ndarray) -> State:
 def _observe(state: State, player_id: jnp.ndarray) -> jnp.ndarray:
     """
     手番のplayerに対する観測を返す.
+    [boad, playable_dice, playable_num]
     """
     board: jnp.ndarray = state._board
     zero_one_dice_vec: jnp.ndarray = _to_zero_one_dice_vec(
