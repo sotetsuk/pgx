b6e868ea3da3c25ebac461e7ab6f2a6550925b43
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-05-26 03:50:13 +0900

    .

--- pgx/shogi.py
+++ pgx/shogi.py
@@ -647,4 +647,4 @@ def _observe(state: State, player_id: jnp.ndarray) -> jnp.ndarray:
     ]
     feat2 = my_hand_feat + opp_hand_feat + [checked]
     feat = jnp.vstack(feat1 + feat2)
-    return feat.transpose((1, 2, 0))
+    return jnp.rot90(feat.transpose((1, 2, 0)), k=3)
