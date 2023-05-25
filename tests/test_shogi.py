12e84d8047e7de0a09b328cacf6f578dea0eda1c
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-05-26 02:39:37 +0900

    [Shogi] Fix max termination steps (#902)

--- tests/test_shogi.py
+++ tests/test_shogi.py
@@ -403,6 +403,5 @@ def test_sfen():
 
 def test_api():
     import pgx
-    # env = pgx.make("shogi")
-    env = Shogi(max_termination_steps=50)
-    pgx.v1_api_test(env, 5)
+    env = pgx.make("shogi")
+    pgx.v1_api_test(env, 1)
