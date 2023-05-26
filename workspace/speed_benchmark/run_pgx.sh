45839eda0acba1f0ee95d82a784ccb946cb8899c
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-05-26 19:31:59 +0900

    .

---
+++ workspace/speed_benchmark/run_pgx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+NUM_BATCH_STEPS=${1}
+PY="python3"
+GAMES=$(python3 -c "import pgx; print(' '.join(pgx.available_envs()))")
+
+BATCH_SIZE=4095
+
+# all gpus
+for game in $GAMES; do
+  $PY -O run_pgx.py $game $bs $NUM_BATCH_STEPS
+done
+
+BATCH_SIZE=512
+
+# 1 gpu
+for game in $GAMES; do
+  CUDA_VISIBLE_DEVICES=0 $PY -O run_pgx.py $game $bs $NUM_BATCH_STEPS
+done
