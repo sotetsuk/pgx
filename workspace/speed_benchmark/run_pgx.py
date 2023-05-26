77a255e1e3da50036ea71fbbe6d70e9e7708063e
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-05-26 15:32:55 +0900

    .

--- workspace/speed_benchmark/run_pgx.py
+++ workspace/speed_benchmark/run_pgx.py
@@ -2,34 +2,18 @@ import time
 import sys
 import json
 import jax
-import jax.numpy as jnp
 import pgx
 from pgx.experimental.utils import act_randomly
+import jax.numpy as jnp
 
 TRUE = jnp.bool_(True)
 FALSE = jnp.bool_(False)
 
 
+num_devices = jax.device_count()
+
 
 def auto_reset(step_fn, init_fn):
-    """Auto reset wrapper.
-    There are several concerns before staging this wrapper:
-    1. Final state (observation)
-    When auto restting happened, the termianl (or truncated) state/observation is replaced by initial state/observation,
-    This is not problematic if it's termination.
-    However, when truncation happened, value of truncated state/observation might be used by agent.
-    So we have to preserve the final state (observation) somewhere.
-    For example, in Gymnasium,
-    https://github.com/Farama-Foundation/Gymnasium/blob/main/gymnasium/wrappers/autoreset.py#L59
-    However, currently, truncation does *NOT* actually happens because
-    all of Pgx environments (games) are finite-horizon and terminates in reasonable # of steps.
-    (NOTE: Chess, Shogi, and Go have `max_termination_steps` option following AlphaZero approach)
-    So, curren implementation is enough (so far), and I feel implementing `final_state/observation` is useless and not necessary.
-    2. Performance:
-    Might harm the performance as it always generates new state.
-    Memory usage might be doubled. Need to check.
-    """
-
     def wrapped_step_fn(state: pgx.State, action):
         state = jax.lax.cond(
             state.terminated,
@@ -58,14 +42,16 @@ def auto_reset(step_fn, init_fn):
 
 
 def benchmark(env_id: pgx.EnvId, batch_size, num_batch_steps):
+    num_steps = batch_size * num_batch_steps
 
     env = pgx.make(env_id)
     assert env is not None
 
-    @jax.jit
+    batch_size_per_dev = batch_size // num_devices
+
     def run(key):
         key, subkey = jax.random.split(key)
-        keys = jax.random.split(subkey, batch_size)
+        keys = jax.random.split(subkey, batch_size_per_dev)
         state = jax.vmap(env.init)(keys)
 
         def fn(i, x):
@@ -77,39 +63,52 @@ def benchmark(env_id: pgx.EnvId, batch_size, num_batch_steps):
             return key, s
 
         _, state = jax.lax.fori_loop(
-            0, num_batch_steps, fn, (key, state)
+            0, num_batch_steps, fn, (rng_key, state)
         )
         return state
 
-    # warmup
+    rng_key = jax.random.PRNGKey(0)
 
-    key = jax.random.PRNGKey(0)
-    key, subkey = jax.random.split(key)
-    run(subkey)
+    if num_devices > 1:
+        run = jax.pmap(run)
+        # warmup start
+        rng_key, subkey = jax.random.split(rng_key)
+        pmap_keys = jax.random.split(subkey, num_devices)
+        s = run(pmap_keys)
+        # warmup end
+        ts = time.time()
+        rng_key, subkey = jax.random.split(rng_key)
+        pmap_keys = jax.random.split(subkey, num_devices)
+        s = run(pmap_keys)
+        te = time.time()
+    else:
+        run = jax.jit(run)
+        # warmup start
+        key = jax.random.PRNGKey(0)
+        key, subkey = jax.random.split(key)
+        s = run(subkey)
+        # warmup end
+        ts = time.time()
+        key, subkey = jax.random.split(key)
+        s = run(subkey)
+        te = time.time()
 
-    ts = time.time()
-    key, subkey = jax.random.split(key)
-    run(subkey)
-    te = time.time()
-
-    return te - ts
+    return num_steps, te - ts
 
 
 games = {
     "tic_tac_toe": "tic_tac_toe",
-    "backgammon": "backgammon",
     "connect_four": "connect_four",
-    "chess": "chess",
     "go": "go_19x19",
+    "chess": "chess",
 }
 
 
-num_batch_steps = int(sys.argv[1])
-bs_list = [2 ** i for i in range(1, 16)]
-d = {}
-for game, env_id in games.items():
-    for bs in bs_list:
-        num_steps = bs * num_batch_steps
-        sec = benchmark(env_id, bs, num_batch_steps)
-        print(json.dumps({"game": game, "library": "pgx (A100 x 1)",
-              "total_steps": num_steps, "total_sec": sec, "steps/sec": num_steps / sec, "batch_size": bs}))
+game = sys.argv[1]
+bs = int(sys.argv[2])
+num_batch_steps = int(sys.argv[3])
+env_id = games[game]
+
+
+num_steps, sec = benchmark(env_id, bs, bs * num_batch_steps)
+print(json.dumps({"game": game, "library": f"pgx/{num_devices}dev", "total_steps": num_steps, "total_sec": sec, "steps/sec": num_steps / sec, "batch_size": bs}))
