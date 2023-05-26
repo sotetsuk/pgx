0f66d67723b3acf309bef6af4b1e1b55577f1b94
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-05-26 16:24:40 +0900

    .

--- workspace/speed_benchmark/install.sh
+++ workspace/speed_benchmark/install.sh
@@ -1,1 +1,1 @@
-python3 -m pip install pettingzoo open_spiel pygame cloudpickle chess tianshou==0.4.11 cshogi pygame chess
+python3 -m pip install pettingzoo open_spiel pygame cloudpickle chess tianshou==0.4.11 cshogi
