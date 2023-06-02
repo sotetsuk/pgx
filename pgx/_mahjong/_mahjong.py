7a373c308b6998e6767c386e7e8d6a68bcf4a869
OkanoShinri <n.salt2000@gmail.com>
2023-06-02 21:40:46 +0900

    self_kan

--- pgx/_mahjong/_mahjong.py
+++ pgx/_mahjong/_mahjong.py
@@ -16,11 +16,9 @@ import jax
 import jax.numpy as jnp
 
 import pgx.v1 as v1
-
-# from pgx._mahjong._action import Action
+from pgx._mahjong._action import Action
 from pgx._mahjong._hand import Hand
-
-# from pgx._mahjong._meld import Meld
+from pgx._mahjong._meld import Meld
 from pgx._src.struct import dataclass
 
 FALSE = jnp.bool_(False)
@@ -38,13 +36,13 @@ class State(v1.State):
     _rng_key: jax.random.KeyArray = jax.random.PRNGKey(0)
     _step_count: jnp.ndarray = jnp.int32(0)
     # --- Mahjong specific ---
-    deck: jnp.ndarray = jnp.zeros(136)
+    deck: jnp.ndarray = jnp.zeros(136, dtype=jnp.int8)
 
     # 次に引く牌のindex
     next_deck_ix: jnp.ndarray = jnp.int8(135)
 
     # 各プレイヤーの手牌. 長さ34で、数字は持っている牌の数
-    hand: jnp.ndarray = jnp.zeros((4, 34))
+    hand: jnp.ndarray = jnp.zeros((4, 34), dtype=jnp.int8)
 
     # 直前に捨てられてron,pon,chi の対象になっている牌. 存在しなければ-1
     target: jnp.ndarray = jnp.int8(0)
@@ -59,10 +57,15 @@ class State(v1.State):
     riichi: jnp.ndarray = FALSE
 
     # 各playerの副露回数
-    n_meld: jnp.ndarray = jnp.zeros(4)
+    n_meld: jnp.ndarray = jnp.zeros(4, dtype=jnp.int8)
 
     # melds[i][j]: player i のj回目の副露(j=1,2,3,4). 存在しなければ0
     melds: jnp.ndarray = jnp.zeros((4, 4))
+
+    is_menzen: jnp.ndarray = FALSE
+
+    # pon[i][j]: player i がjをポンを所有している場合, src << 2 | index. or 0
+    pon: jnp.ndarray = jnp.zeros((4, 4), dtype=jnp.int8)
 
     @property
     def env_id(self) -> v1.EnvId:
@@ -125,10 +128,14 @@ def _step(state: State, action: jnp.ndarray) -> State:
     #   - ron, tsumo
     # - 勝利条件確認
 
-    state = _discard(state, action)
-
-    # ポンとかチーとかない場合はdrawへ
-    state = _draw(state)
+    discard = action < 34
+    ankan = 34 <= action < 68
+    state = jax.lax.cond(
+        discard, lambda: _discard(state, action), lambda: state
+    )
+    state = jax.lax.cond(
+        ~discard & ankan, lambda: _selfkan(state, action - 34), lambda: state
+    )
 
     return state
 
@@ -150,9 +157,65 @@ def _discard(state: State, tile: jnp.ndarray):
     hand = state.hand.at[state.current_player].set(
         Hand.sub(state.hand[state.current_player], tile)
     )
-    return state.replace(  # type:ignore
+    state = state.replace(  # type:ignore
         target=jnp.int8(tile), last_draw=-1, hand=hand
     )
+
+    # ポンとかチーとかない場合はdrawへ
+    state = _draw(state)
+
+    return state
+
+
+def _append_meld(state: State, meld, player):
+    melds = state.melds.at[(player, state.n_meld[player])].set(meld)
+    n_meld = state.n_meld.at[player].set(state.n_meld[player] + 1)
+    return state.replace(melds=melds, n_meld=n_meld)
+
+
+def _selfkan(state: State, action):
+    target = action - 34
+    pon = state.pon[(state.current_player, target)]
+    state = jax.lax.cond(
+        pon == 0,
+        lambda: _ankan(state, target),
+        lambda: _kakan(state, target, pon >> 2, pon & 0b11),
+    )
+
+    # 嶺上牌
+    rinshan_tile = state.deck[state.next_deck_ix]
+    next_deck_ix = state.next_deck_ix - 1
+    hand = state.hand.at[state.current_player].set(
+        Hand.add(state.hand[state.current_player], rinshan_tile)
+    )
+    return state.replace(
+        next_deck_ix=next_deck_ix, last_draw=rinshan_tile, hand=hand
+    )
+
+
+def _ankan(state: State, target):
+    curr_player = state.current_player
+    meld = Meld.init(target + 34, target, src=0)
+    state = _append_meld(state, meld, curr_player)
+    hand = state.hand.at[curr_player].set(
+        Hand.ankan(state.hand[curr_player], target)
+    )
+    # TODO: 国士無双ロンの受付
+
+    return state.replace(hand=hand)
+
+
+def _kakan(state: State, target, pon_src, pon_idx):
+    melds = state.melds.at[(state.current_player, pon_idx)].set(
+        Meld.init(target + 34, target, pon_src)
+    )
+    hand = state.hand.at[state.current_player].set(
+        Hand.kakan(state.hand[state.current_player], target)
+    )
+    pon = state.pon.at[(state.current_player, target)].set(0)
+    # TODO: 槍槓の受付
+
+    return state.replace(melds=melds, hand=hand, pon=pon)
 
 
 def _observe(state: State, player_id: jnp.ndarray) -> jnp.ndarray:
