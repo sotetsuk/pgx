7cd51596c9c36491183cd26d074105548e0bd1cc
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-06-20 14:02:49 +0900

    .

--- pgx/experimental/wrappers.py
+++ pgx/experimental/wrappers.py
@@ -12,14 +12,17 @@ class Wrapper(pgx.Env):
     def __init__(self, env: pgx.Env):
         self.env: pgx.Env = env
 
-    def init(self, key: jax.random.KeyArray) -> pgx.State:
-        return self.env.init(key)
+    def _init(self, key: jax.random.KeyArray) -> pgx.State:
+        """Implement game-specific init function here."""
+        return self.env._init(key)
 
-    def step(self, state: pgx.State, action: jnp.ndarray) -> pgx.State:
-        return self.env.step(state, action)
+    def _step(self, state, action) -> pgx.State:
+        """Implement game-specific step function here."""
+        return self.env._step(state, action)
 
-    def observe(self, state: pgx.State, player_id: jnp.ndarray) -> jnp.ndarray:
-        return self.env.observe(state, player_id)
+    def _observe(self, state: pgx.State, player_id: jnp.ndarray) -> jnp.ndarray:
+        """Implement game-specific observe function here."""
+        return self.env._observe(state, player_id)
 
     @property
     def id(self) -> pgx.EnvId:
