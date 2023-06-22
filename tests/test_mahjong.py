01c7c0448853b4da3e856088d067ba18123c2e4b
OkanoShinri <n.salt2000@gmail.com>
2023-06-23 01:02:37 +0900

    add test

--- tests/test_mahjong.py
+++ tests/test_mahjong.py
@@ -1,7 +1,8 @@
 from pgx._mahjong._hand import Hand
 from pgx._mahjong._yaku import Yaku
+from pgx._mahjong._action import Action
 from pgx._mahjong._shanten import Shanten
-from pgx._mahjong._mahjong import Mahjong
+from pgx._mahjong._mahjong2 import Mahjong
 import jax.numpy as jnp
 from jax import jit
 import jax
@@ -11,7 +12,7 @@ init = jit(env.init)
 step = jit(env.step)
 
 
-def test_ron():
+def test_hand():
     # fmt:off
     hand = jnp.int8([
         0, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -50,8 +51,6 @@ def test_ron():
     assert jit(Hand.can_ron)(hand, 0)
     assert ~jit(Hand.can_ron)(hand, 1)
 
-
-def test_riichi():
     # fmt:off
     hand = jnp.int8([
         1, 1, 1, 1, 1, 1, 1, 1, 0,
@@ -85,8 +84,6 @@ def test_riichi():
 
     assert jit(Hand.can_riichi)(hand)
 
-
-def test_can_chi():
     from pgx._mahjong._action import Action
 
     # fmt:off
@@ -193,36 +190,71 @@ def test_discard():
     key = jax.random.PRNGKey(0)
     state = init(key=key)
     assert state.current_player == 0
+    assert state.target == -1
     assert state.deck[state.next_deck_ix] == 8
     # fmt:off
-    assert (state.hand==jnp.int8([
-        [0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 2, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
-        [1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 2, 1, 0, 2, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0],
-        [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0]])).all()
+    assert (state.hand==jnp.int8(
+        [[0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 2, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+         [1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 2, 1, 0, 2, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+         [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0]]
+    )).all()
     # fmt:on
 
-    state = step(state, 4)
+    state = step(state, 8)
     assert state.current_player == 1
+    assert state.target == 8
     assert state.deck[state.next_deck_ix] == 31
     # fmt:off
-    assert (state.hand==jnp.int8([
-        [[0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 2, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+    assert (state.hand==jnp.int8(
+        [[0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 2, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
          [1, 0, 0, 1, 1, 1, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0, 0],
          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 2, 1, 0, 2, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0],
-         [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0]]])).all()
+         [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0]]
+    )).all()
     # fmt:on
 
-    state = step(state, 0)
+    state = step(state, Action.TSUMOGIRI)
     assert state.current_player == 2
+    assert state.target == 8
     assert state.deck[state.next_deck_ix] == 16
     # fmt:off
-    assert (state.hand==jnp.int8([
-        [[0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 2, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
-         [0, 0, 0, 1, 1, 1, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0, 0],
+    assert (state.hand==jnp.int8(
+        [[0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 2, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+         [1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0, 0],
          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 2, 1, 0, 2, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 2, 1, 0],
-         [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0]]])).all()
+         [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0]]
+    )).all()
     # fmt:on
+
+
+def test_chi():
+    key = jax.random.PRNGKey(2)
+    state = init(key=key)
+    """
+    current_player 1
+    [[0 1 0 1 0 1 0 0 0 1 1 1 0 1 0 0 0 1 1 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0]
+     [2 0 1 1 1 0 1 0 0 1 0 1 3 0 0 1 0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0]
+     [0 0 0 0 2 0 1 1 2 0 0 0 0 0 1 0 0 1 0 0 1 1 0 0 0 1 1 1 0 0 0 0 0 0]
+     [0 0 1 1 0 1 0 0 0 0 0 0 0 0 0 0 0 2 0 2 1 0 0 0 1 0 0 0 0 1 1 0 0 2]]
+    """
+    state = env.step(state, 6)
+    assert state.current_player == 2
+    assert state.legal_action_mask[Action.CHI_L]
+
+    state1 = env.step(state, Action.CHI_L)
+    assert state1.current_player == 2
+    # fmt:off
+    assert (state1.hand==jnp.int8([
+        [[0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0], 
+         [2, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 3, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 2, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 2]]])).all()
+    # fmt:on
+    assert state1.melds[2, 0] == 74
+
+    state2 = env.step(state, Action.PASS)
+    assert state2.current_player == 2
 
 
 def test_ankan():
@@ -260,5 +292,8 @@ def test_random_play():
         a = act_randomly(subkey, state)
         state = step(state, a)
 
-        assert state.hand[state.current_player].sum() in [13, 14]
-        assert (state.hand >= 0).all()
+        assert state.hand[state.current_player].sum() + jnp.count_nonzero(
+            state.melds[state.current_player]
+        ) * 3 in [13, 14]
+        assert (0 <= state.hand).all()
+        assert (state.hand <= 4).all()
