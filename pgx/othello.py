0f39500e99a89b26eb88b5539f226b3ca5141de2
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-10-04 13:34:40 +0900

    othello

--- pgx/othello.py
+++ pgx/othello.py
@@ -57,7 +57,8 @@ class Othello(v1.Env):
     def _init(self, key: jax.random.KeyArray) -> State:
         return _init(key)
 
-    def _step(self, state: v1.State, action: jnp.ndarray) -> State:
+    def _step(self, state: v1.State, action: jnp.ndarray, key) -> State:
+        del key
         assert isinstance(state, State)
         return _step(state, action)
 
