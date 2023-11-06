b098f27482b872e50f8b050425ca2ffe1ccc499f
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-11-06 10:55:07 +0900

    .

--- pgx/experimental/utils.py
+++ pgx/experimental/utils.py
@@ -5,5 +5,5 @@ from pgx.v1 import State
 
 
 def act_randomly(rng: jax.random.KeyArray, state: State) -> jnp.ndarray:
-    logits = jnp.log(state.legal_action_mask.astype(jnp.float16))
+    logits = jnp.log(state.legal_action_mask.astype(jnp.float32))
     return jax.random.categorical(rng, logits=logits, axis=1)
