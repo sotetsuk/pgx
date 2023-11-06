f970260bfc99f0733976605af208ef346183f70c
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-11-06 11:03:15 +0900

    fmt

--- pgx/experimental/utils.py
+++ pgx/experimental/utils.py
@@ -2,6 +2,8 @@ import jax
 import jax.numpy as jnp
 
 
-def act_randomly(rng: jax.random.KeyArray, legal_action_mask: jnp.ndarray) -> jnp.ndarray:
+def act_randomly(
+    rng: jax.random.KeyArray, legal_action_mask: jnp.ndarray
+) -> jnp.ndarray:
     logits = jnp.log(legal_action_mask.astype(jnp.float32))
     return jax.random.categorical(rng, logits=logits, axis=1)
