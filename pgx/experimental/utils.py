7198bf66a5d6484985e155f448c5a39bd7317040
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-11-08 23:13:02 +0900

    [experimental] Add error message to `act_randomly` (#1101)

--- pgx/experimental/utils.py
+++ pgx/experimental/utils.py
@@ -2,8 +2,15 @@ import jax
 import jax.numpy as jnp
 
 from pgx._src.types import Array, PRNGKey
+from pgx.core import State
 
 
 def act_randomly(rng: PRNGKey, legal_action_mask: Array) -> Array:
+    assert not isinstance(legal_action_mask, State), (
+        "Pgx v2.0.0 changes pgx.experimental.act_randomly to reveive legal_action_mask instead of state:\n\n"
+        "  * <  v2.0.0: act_randomly(rng, state)\n"
+        "  * >= v2.0.0: act_randomly(rng, state.legal_action_mask)\n\n"
+        "Note that codes under pgx.experimental are subject to change without notice."
+    )
     logits = jnp.log(legal_action_mask.astype(jnp.float32))
     return jax.random.categorical(rng, logits=logits, axis=1)
