6e9c2d9feda1fd1e66fc1e84d8b932f75bf8853d
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-11-16 20:54:41 +0900

    extract config

---
+++ examples/alphazero/config.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+import pgx
+
+
+class Config(BaseModel):
+    env_id: pgx.EnvId = "go_9x9"
+    seed: int = 0
+    max_num_iters: int = 400
+    # network params
+    num_channels: int = 128
+    num_layers: int = 6
+    resnet_v2: bool = True
+    # selfplay params
+    selfplay_batch_size: int = 1024
+    num_simulations: int = 32
+    max_num_steps: int = 256
+    # training params
+    training_batch_size: int = 4096
+    learning_rate: float = 0.001
+    # eval params
+    eval_interval: int = 5
+
+    class Config:
+        extra = "forbid"
--- examples/alphazero/train.py
+++ examples/alphazero/train.py
@@ -28,34 +28,12 @@ import pgx
 import wandb
 from omegaconf import OmegaConf
 from pgx.experimental import auto_reset
-from pydantic import BaseModel
 
+from config import Config
 from network import AZNet
 
 devices = jax.local_devices()
 num_devices = len(devices)
-
-
-class Config(BaseModel):
-    env_id: pgx.EnvId = "go_9x9"
-    seed: int = 0
-    max_num_iters: int = 400
-    # network params
-    num_channels: int = 128
-    num_layers: int = 6
-    resnet_v2: bool = True
-    # selfplay params
-    selfplay_batch_size: int = 1024
-    num_simulations: int = 32
-    max_num_steps: int = 256
-    # training params
-    training_batch_size: int = 4096
-    learning_rate: float = 0.001
-    # eval params
-    eval_interval: int = 5
-
-    class Config:
-        extra = "forbid"
 
 
 conf_dict = OmegaConf.from_cli()
