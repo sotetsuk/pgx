ac59a192d7618cd08d79cd354ea03bfbe3d51313
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-01-08 17:17:11 +0900

    fix

--- pgx/_src/games/tic_tac_toe.py
+++ pgx/_src/games/tic_tac_toe.py
@@ -1,4 +1,4 @@
-# Copyright 2023 GameStatehe Pgx Authors. All Rights Reserved.
+# Copyright 2023 The Pgx Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -8,7 +8,7 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WIGameStateHOUGameState WARRANGameStateIES OR CONDIGameStateIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
