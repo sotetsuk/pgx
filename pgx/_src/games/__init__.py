ba5f771945c609cee59f238d5b2ee6bca8470250
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-01-08 15:27:50 +0900

    [Protocol] Add `TwoPlayerPerfectInfoGame` (#1144)

---
+++ pgx/_src/games/__init__.py
@@ -0,0 +1,28 @@
+from typing import Optional, Protocol, TypeVar, runtime_checkable
+
+from jax import Array
+
+# NOTE: These protocols are WIP and subject to change.
+
+T = TypeVar("T")  # GameState
+
+
+@runtime_checkable
+class TwoPlayerPerfectInfoGame(Protocol[T]):
+    def init(self) -> T:
+        ...
+
+    def step(self, state: T, action: Array) -> T:
+        ...
+
+    def observe(self, state: T, color: Optional[Array] = None) -> Array:
+        ...
+
+    def legal_action_mask(self, state: T) -> Array:
+        ...
+
+    def is_terminal(self, state: T) -> Array:
+        ...
+
+    def returns(self, state: T) -> Array:
+        ...
