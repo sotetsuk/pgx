5e83d63ba1aa32a48c93898444e03f800762276b
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-01-08 17:16:16 +0900

    fmt

--- pgx/_src/games/tic_tac_toe.py
+++ pgx/_src/games/tic_tac_toe.py
@@ -12,11 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional, NamedTuple
+from typing import NamedTuple, Optional
 
 import jax
 import jax.numpy as jnp
-
 from jax import Array
 
 
