5d670c9d05956d3db1fc73b32be15a01acf5aa45
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-01-08 21:18:53 +0900

    .

--- pgx/connect_four.py
+++ pgx/connect_four.py
@@ -25,20 +25,6 @@ TRUE = jnp.bool_(True)
 
 
 @dataclass
-class GameState:
-    _turn: Array = jnp.int32(0)
-    # 6x7 board
-    # [[ 0,  1,  2,  3,  4,  5,  6],
-    #  [ 7,  8,  9, 10, 11, 12, 13],
-    #  [14, 15, 16, 17, 18, 19, 20],
-    #  [21, 22, 23, 24, 25, 26, 27],
-    #  [28, 29, 30, 31, 32, 33, 34],
-    #  [35, 36, 37, 38, 39, 40, 41]]
-    _board: Array = -jnp.ones(42, jnp.int32)  # -1 (empty), 0, 1
-    winner: Array = jnp.int32(-1)
-
-
-@dataclass
 class State(core.State):
     current_player: Array = jnp.int32(0)
     observation: Array = jnp.zeros((6, 7, 2), dtype=jnp.bool_)
