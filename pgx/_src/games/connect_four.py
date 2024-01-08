d196a71465d63511e91b2e8f5cc95a9cf48eac7b
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-01-08 21:21:57 +0900

    .

--- pgx/_src/games/connect_four.py
+++ pgx/_src/games/connect_four.py
@@ -12,17 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional
+from typing import Optional, NamedTuple
 
 import jax
 import jax.numpy as jnp
 from jax import Array
 
-from pgx._src.struct import dataclass
 
-
-@dataclass
-class GameState:
+class GameState(NamedTuple):
     color: Array = jnp.int32(0)
     # 6x7 board
     # [[ 0,  1,  2,  3,  4,  5,  6],
@@ -45,18 +42,17 @@ class Game:
         board2d = board2d.at[5 - num_filled, action].set(state.color)
         won = ((board2d.flatten()[IDX] == state.color).all(axis=1)).any()
         winner = jax.lax.select(won, state.color, -1)
-        return state.replace(  # type: ignore
+        return state._replace(  # type: ignore
             color=1 - state.color,
             board=board2d.flatten(),
             winner=winner,
         )
 
     def observe(self, state: GameState, color: Optional[Array] = None) -> Array:
-        turns = jax.lax.select(color == 0, jnp.int32([0, 1]), jnp.int32([1, 0]))
-
         def make(turn):
             return state.board.reshape(6, 7) == turn
 
+        turns = jax.lax.select(color == 0, jnp.int32([0, 1]), jnp.int32([1, 0]))
         return jnp.stack(jax.vmap(make)(turns), -1)
 
     def legal_action_mask(self, state: GameState) -> Array:
