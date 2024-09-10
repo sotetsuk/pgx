a468769600d187e25c085d3b68ca22b2ac4bd209
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-09-10 18:43:55 +0900

    extract

--- pgx/_src/games/go.py
+++ pgx/_src/games/go.py
@@ -162,16 +162,16 @@ def _apply_action(state: GameState, action, size) -> GameState:
     is_atari = (idx_sum[chain_ix] ** 2) == idx_squared_sum[chain_ix] * num_pseudo[chain_ix]
     single_liberty = (idx_squared_sum[chain_ix] // idx_sum[chain_ix]) - 1
     is_killed = (neighbours != -1) & (chain_id * oppo_color > 0) & is_atari & (single_liberty == xy)
-    surrounded_stones = state.chain_id_board[:, None] == chain_id
+    surrounded_stones = (state.chain_id_board[:, None] == chain_id) & (is_killed[None, :])
+    chain_id_board = jnp.where(surrounded_stones.any(axis=-1), 0, state.chain_id_board)
+    state = state._replace(chain_id_board=chain_id_board)
 
     def _remove_stones(i, s) -> GameState:
         num_captured_stones = jnp.count_nonzero(surrounded_stones[:, i])
-        chain_id_board = jnp.where(surrounded_stones[:, i], 0, s.chain_id_board)
         ko = jax.lax.select(ko_may_occur & (num_captured_stones == 1), neighbours[i], s.ko)
         return jax.lax.cond(
             is_killed[i],
             lambda: s._replace(
-                chain_id_board=chain_id_board,
                 num_captured_stones=s.num_captured_stones.at[s.color].add(num_captured_stones),
                 ko=ko,
             ),
