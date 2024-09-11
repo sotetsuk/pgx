538dc65e08cdefab34cdce7f38b765a7e18a10a9
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-09-11 22:51:50 +0900

    tidy

--- pgx/chess.py
+++ pgx/chess.py
@@ -23,66 +23,13 @@ from pgx._src.games.chess_utils import INIT_LEGAL_ACTION_MASK
 from pgx._src.struct import dataclass
 from pgx._src.types import Array, PRNGKey
 
-INIT_ZOBRIST_HASH = jnp.uint32([1172276016, 1112364556])
-MAX_TERMINATION_STEPS = 512  # from AZ paper
-
-TRUE = jnp.bool_(True)
-FALSE = jnp.bool_(False)
-
-EMPTY = jnp.int32(0)
-PAWN = jnp.int32(1)
-KNIGHT = jnp.int32(2)
-BISHOP = jnp.int32(3)
-ROOK = jnp.int32(4)
-QUEEN = jnp.int32(5)
-KING = jnp.int32(6)
-# OPP_PAWN = -1
-# OPP_KNIGHT = -2
-# OPP_BISHOP = -3
-# OPP_ROOK = -4
-# OPP_QUEEN = -5
-# OPP_KING = -6
-
-
-# board index (white view)
-# 8  7 15 23 31 39 47 55 63
-# 7  6 14 22 30 38 46 54 62
-# 6  5 13 21 29 37 45 53 61
-# 5  4 12 20 28 36 44 52 60
-# 4  3 11 19 27 35 43 51 59
-# 3  2 10 18 26 34 42 50 58
-# 2  1  9 17 25 33 41 49 57
-# 1  0  8 16 24 32 40 48 56
-#    a  b  c  d  e  f  g  h
-# board index (flipped black view)
-# 8  0  8 16 24 32 40 48 56
-# 7  1  9 17 25 33 41 49 57
-# 6  2 10 18 26 34 42 50 58
-# 5  3 11 19 27 35 43 51 59
-# 4  4 12 20 28 36 44 52 60
-# 3  5 13 21 29 37 45 53 61
-# 2  6 14 22 30 38 46 54 62
-# 1  7 15 23 31 39 47 55 63
-#    a  b  c  d  e  f  g  h
-# fmt: off
-INIT_BOARD = jnp.int32([
-    4, 1, 0, 0, 0, 0, -1, -4,
-    2, 1, 0, 0, 0, 0, -1, -2,
-    3, 1, 0, 0, 0, 0, -1, -3,
-    5, 1, 0, 0, 0, 0, -1, -5,
-    6, 1, 0, 0, 0, 0, -1, -6,
-    3, 1, 0, 0, 0, 0, -1, -3,
-    2, 1, 0, 0, 0, 0, -1, -2,
-    4, 1, 0, 0, 0, 0, -1, -4
-])
-# fmt: on
 
 @dataclass
 class State(core.State):
     current_player: Array = jnp.int32(0)
     rewards: Array = jnp.float32([0.0, 0.0])
-    terminated: Array = FALSE
-    truncated: Array = FALSE
+    terminated: Array = jnp.bool_(False)
+    truncated: Array = jnp.bool_(False)
     legal_action_mask: Array = INIT_LEGAL_ACTION_MASK  # 64 * 73 = 4672
     observation: Array = jnp.zeros((8, 8, 119), dtype=jnp.float32)
     _step_count: Array = jnp.int32(0)
