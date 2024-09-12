b3a72d7608d8d85b23cdabf8b06a23a71ce3343c
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-09-12 17:53:40 +0900

    .

--- pgx/_src/games/chess.py
+++ pgx/_src/games/chess.py
@@ -420,8 +420,10 @@ def _legal_action_mask(state: GameState) -> Array:
             ok &= ((between_ixs < 0) | (state.board[between_ixs] == EMPTY)).all()
             # filter pawn move
             pawn_should = (a.to % 8) >= (a.from_ % 8)  # should move forward
-            pawn_should &= (a.to // 8 != a.from_ // 8) | (state.board[a.to] >= 0)  # cannot move up if occupied by opponent
-            pawn_should &= (a.to // 8 == a.from_ // 8) | (state.board[a.to] < 0)  # cannot move diagnally without capturing
+            pawn_should &= (
+                ((a.to // 8 == a.from_ // 8) & (state.board[a.to] == 0)) |   # move up
+                ((a.to // 8 != a.from_ // 8) & (state.board[a.to] < 0))      # capture
+            )
             ok &= (piece != PAWN) | pawn_should
             return jax.lax.select(ok, a._to_label(), -1)
 
