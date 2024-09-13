14abcf3b5430639261bbea03b8da762e7c4587c7
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-09-13 11:54:35 +0900

    .

--- pgx/_src/games/chess_utils.py
+++ pgx/_src/games/chess_utils.py
@@ -105,18 +105,13 @@ for from_ in range(64):
         CAN_MOVE[5, from_, a] = True
 # KING
 for from_ in range(64):
-    r0, c0 = from_ % 8, from_ // 8
     legal_dst = []
     for to in range(64):
-        r1, c1 = to % 8, to // 8
-        if from_ == to:
-            continue
-        if (np.abs(r1 - r0) <= 1) and (np.abs(c1 - c0) <= 1):
+        r0, c0, r1, c1 = from_ % 8, from_ // 8, to % 8, to // 8
+        if from_ != to and np.abs(r1 - r0) <= 1 and np.abs(c1 - c0) <= 1:
             legal_dst.append(to)
-    assert len(legal_dst) <= 8
-    LEGAL_DEST[6, from_, : len(legal_dst)] = legal_dst
-    for a in legal_dst:
-        CAN_MOVE[6, from_, a] = True
+    LEGAL_DEST[6, from_, :len(legal_dst)] = legal_dst
+    CAN_MOVE[6, from_, legal_dst] = True
 
 assert (LEGAL_DEST[0, :, :] == -1).all()
 
