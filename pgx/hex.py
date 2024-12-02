16f3a8f5d15673f5bad901fc7a44db74f3c7e387
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-12-02 11:28:06 +0900

    tidy

--- pgx/hex.py
+++ pgx/hex.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import NamedTuple
 from functools import partial
+from typing import NamedTuple
 
 import jax
 import jax.numpy as jnp
